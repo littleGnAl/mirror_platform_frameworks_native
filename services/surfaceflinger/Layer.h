abbced20468070a95869379ea8dd9384be09de8f
nao.x.ejiri <nao.x.ejiri@sonymobile.com>
2014-04-21 20:16:20 +0900

    Add flag to hide window layers on external display
    When MHL or WFD was connected, window layers with internal only flag
    will not be output to external display such as TV.
    
    Test: I18a6d4febe9bb23f1b008e68c0db2337cbb9cd12
    
    Change-Id: I307f1431902706bf9cac35a60ca2397bf813afff

--- services/surfaceflinger/Layer.h
+++ services/surfaceflinger/Layer.h
@@ -687,6 +687,10 @@ public:
     PixelFormat getPixelFormat() const { return mFormat; }
 
 private:
+    // Used only to check if layer is internal to skip it on external display
+    bool isInternalOnly(const sp<const DisplayDevice>& hw,
+            uint32_t flags = (ISurfaceComposerClient::eInternalOnly |
+                    ISurfaceComposerClient::eInternalOnlySS)) const;
 
     // -----------------------------------------------------------------------
 
@@ -793,6 +797,8 @@ private:
 
     // This layer can be a cursor on some displays.
     bool mPotentialCursor;
+    // do not output to external
+    uint32_t mInternalOnly;
 
     // Local copy of the queued contents of the incoming BufferQueue
     mutable Mutex mQueueItemLock;
