ad4310902d8cdf2b3d4f91a2e1059abc86f623a0
Shuo Gao <shuo.gao@intel.com>
2014-04-22 00:53:11 +0800

    Fix a bug in atrace async feature
    There is bug in atrace for --async-stop and --async-dump,
    this patch fix this bug to enable asynchronous log collection.
    
    Change-Id: I7db0d81d2f29317c43f92b0d869b51ed4b707efa
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Mingwei Shi <mingwei.shi@intel.com>

--- cmds/atrace/atrace.cpp
+++ cmds/atrace/atrace.cpp
@@ -819,7 +819,7 @@ int main(int argc, char **argv)
                     g_traceOverwrite = true;
                 } else if (!strcmp(long_options[option_index].name, "async_stop")) {
                     async = true;
-                    traceStop = false;
+                    traceStart = false;
                 } else if (!strcmp(long_options[option_index].name, "async_dump")) {
                     async = true;
                     traceStart = false;
@@ -845,8 +845,10 @@ int main(int argc, char **argv)
     }
 
     bool ok = true;
-    ok &= setUpTrace();
-    ok &= startTrace();
+    if (traceStart) {
+        ok &= setUpTrace();
+        ok &= startTrace();
+    }
 
     if (ok && traceStart) {
         printf("capturing trace...");
