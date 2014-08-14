Jackeagle/caf_frameworks_native

SurfaceFlinger: Add NULL check for buffer handling
Add buffer handling NULL check in dequeueBuffer and
verify whether the output data from binder is not NULL
in queueBuffer and connect api's to avoid SF crash

CRs-Fixed: 573088
CRs-Fixed: 572315

Change-Id: I41cebbc0cbcbbb0fd5ecb38db7ec7b0c91cdffe9

libs/gui/Surface.cpp
@@ -209,6 +209,9 @@ int Surface::dequeueBuffer(android_native_buffer_t** buffer,
             ALOGE("dequeueBuffer: IGraphicBufferProducer::requestBuffer failed: %d",
                     result);
             return result;
+        } else if (gbuf == 0) {
+            ALOGE("dequeueBuffer: Buffer is null return");
+            return INVALID_OPERATION;
         }
     }
 

littleGnAl/mirror_platform_frameworks_native

libs/gui/Surface.cpp
@@ -205,6 +205,9 @@ int Surface::dequeueBuffer(android_native_buffer_t** buffer, int* fenceFd) {
         if (result != NO_ERROR) {
             ALOGE("dequeueBuffer: IGraphicBufferProducer::requestBuffer failed: %d", result);
             return result;
+        } else if (gbuf == 0) {
+            LOG_ALWAYS_FATAL("dequeueBuffer: Buffer is null return");
+            return INVALID_OPERATION;
         }
     }
 