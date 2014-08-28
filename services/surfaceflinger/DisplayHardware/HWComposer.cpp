e6373c890685fa6a0b1a71890cb324312b9a7f86
Michael I. Gold <gold@nvidia.com>
2014-08-28 16:22:51 -0700

    hwcomposer: query for virtual display support
    Query the hwcomposer for virtual display support instead of assuming
    they are supposed based on the version number.
    
    Change-Id: Iebf61e6d4daebe74c5849fc2c4105210996ff644

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -156,6 +156,12 @@ HWComposer::HWComposer(
         if (hwcHasApiVersion(mHwc, HWC_DEVICE_API_VERSION_1_3)) {
             // 1.3 adds support for virtual displays
             mNumDisplays = MAX_HWC_DISPLAYS;
+            // ... but some implementations may not support them
+            int types = 0;
+            status_t err = mHwc->query(mHwc, HWC_DISPLAY_TYPES_SUPPORTED, &types);
+            if (err == NO_ERROR && !(types & HWC_DISPLAY_VIRTUAL_BIT)) {
+                mNumDisplays = NUM_BUILTIN_DISPLAYS;
+            }
         } else if (hwcHasApiVersion(mHwc, HWC_DEVICE_API_VERSION_1_1)) {
             // 1.1 adds support for multiple displays
             mNumDisplays = NUM_BUILTIN_DISPLAYS;
