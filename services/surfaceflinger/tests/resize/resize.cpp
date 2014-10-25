1c60c95cdcb256a8c7679e1d1d67b4159cab3ffc
Qiang Chen <qcwj.lk@gmail.com>
2014-10-25 16:31:08 +0800

    Fix resize test app build errors
    Surface lock method should pass in dirty bounds as parameters.
    Added libbinder into linking libs list, corrected header file
    include.
    
    Change-Id: I7601d88cf1ff1035bdc4c6fd6812bea32ccb41b0
    Signed-off-by: Qiang Chen <qcwj.lk@gmail.com>

--- services/surfaceflinger/tests/resize/resize.cpp
+++ services/surfaceflinger/tests/resize/resize.cpp
@@ -24,10 +24,9 @@
 
 #include <gui/Surface.h>
 #include <gui/SurfaceComposerClient.h>
+#include <android/native_window.h>
 
 using namespace android;
-
-namespace android {
 
 int main(int argc, char** argv)
 {
@@ -53,7 +52,7 @@ int main(int argc, char** argv)
     android_memset16((uint16_t*)outBuffer.bits, 0xF800, bpr*outBuffer.height);
     surface->unlockAndPost();
 
-    surface->lock(&outBuffer);
+    surface->lock(&outBuffer, NULL);
     android_memset16((uint16_t*)outBuffer.bits, 0x07E0, bpr*outBuffer.height);
     surface->unlockAndPost();
 
