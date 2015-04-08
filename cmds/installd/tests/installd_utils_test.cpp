6d1fc9c913f5426d666805c7a568f64c5391e233
Jeff Sharkey <jsharkey@android.com>
2015-04-07 18:14:05 -0700

    Plumb through volume UUID when building paths.
    Since app data paths can live on expanded storage devices, accept the
    target volume UUID when building paths.  The null UUID indicates the
    default internal storage.
    
    To improve readability, start using std::string in several places,
    which throws when allocations fail.  For now, perform last-second
    sanity checks on incoming path arguments, but we'll eventually want
    to check arguments as they come through installd.cpp, instead of
    crashing the entire daemon.
    
    Bug: 19993667
    Change-Id: Ib9343575ffb62bf3981e19375de8f3822fc31e28

--- cmds/installd/tests/installd_utils_test.cpp
+++ cmds/installd/tests/installd_utils_test.cpp
@@ -321,6 +321,7 @@ TEST_F(UtilsTest, CreatePkgPath_LongPkgNameSuccess) {
 
     const char *prefix = TEST_DATA_DIR PRIMARY_USER_PREFIX;
     size_t offset = strlen(prefix);
+
     EXPECT_STREQ(pkgname, path + offset)
              << "Package path should be a really long string of a's";
 }
@@ -369,20 +370,6 @@ TEST_F(UtilsTest, CreatePkgPath_SecondaryUser) {
 
     EXPECT_STREQ(TEST_DATA_DIR SECONDARY_USER_PREFIX "1/com.example.package", path)
             << "Package path should be in /data/user/";
-}
-
-TEST_F(UtilsTest, CreatePkgPathInDir_ProtectedDir) {
-    char path[PKG_PATH_MAX];
-
-    dir_rec_t dir;
-    dir.path = (char*) "/data/app-private/";
-    dir.len = strlen(dir.path);
-
-    EXPECT_EQ(0, create_pkg_path_in_dir(path, &dir, "com.example.package", ".apk"))
-            << "Should successfully create package path.";
-
-    EXPECT_STREQ("/data/app-private/com.example.package.apk", path)
-            << "Package path should be in /data/app-private/";
 }
 
 TEST_F(UtilsTest, CreatePersonaPath_Primary) {
