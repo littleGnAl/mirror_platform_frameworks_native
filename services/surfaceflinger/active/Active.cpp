3807f9fdc2c045a0c18911431d9566eafe06ab46
Dan Stoza <stoza@google.com>
2015-04-14 13:50:15 -0700

    WIP: Flatland 2.0
    Change-Id: I7658490d26f3ea68d01702abbb5852d961193224

---
+++ services/surfaceflinger/active/Active.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gui/SurfaceComposerClient.h>
+
+#include <array>
+#include <iostream>
+
+namespace android {
+
+enum class Format
+{
+    Unknown = 0,
+    RGBA = 1,
+};
+
+class Window
+{
+  private:
+    uint32_t mTop;
+    uint32_t mLeft;
+    uint32_t mWidth;
+    uint32_t mHeight;
+    Format mFormat;
+    std::array<float, 4> mColor; // [0] = R, [1] = G, [2] = B, [3] = A
+    float mPlaneAlpha;
+};
+
+int initialize()
+{
+    std::cout << "Initializing SurfaceFlinger... ";
+
+    sp<IBinder> builtInDisplay = SurfaceComposerClient::getBuiltInDisplay(0);
+    if (builtInDisplay == NULL) {
+        std::cout << "failed to get built-in display\n";
+        return -1;
+    }
+
+    std::cout << "done\n";
+
+    return 0;
+}
+
+} // namespace android
+
+int main(int /* argc */, const char** /* argv[] */)
+{
+    return android::initialize();
+}
