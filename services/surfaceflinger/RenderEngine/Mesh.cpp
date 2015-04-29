159bb81e25a627528f79e455bdabaca68d9380a3
Dan Stoza <stoza@google.com>
2015-04-29 13:30:31 -0700

    SurfaceFlinger: Fix integer overflow in Mesh ctor
    Performs range checking on the inputs to Mesh::Mesh() before allocating
    the storage array.
    
    Bug: 20674682
    Change-Id: I4fc918a8c312d967dd6d9f91a098b2e0a7081027

--- services/surfaceflinger/RenderEngine/Mesh.cpp
+++ services/surfaceflinger/RenderEngine/Mesh.cpp
@@ -16,14 +16,37 @@
 
 #include "Mesh.h"
 
+#include <utils/Log.h>
+
 namespace android {
 
 Mesh::Mesh(Primitive primitive, size_t vertexCount, size_t vertexSize, size_t texCoordSize)
     : mVertexCount(vertexCount), mVertexSize(vertexSize), mTexCoordsSize(texCoordSize),
       mPrimitive(primitive)
 {
-    mVertices = new float[(vertexSize + texCoordSize) * vertexCount];
-    mStride = mVertexSize + mTexCoordsSize;
+    if (vertexCount == 0) {
+        mVertices = new float[1];
+        mVertices = 0.0f;
+        mStride = 0;
+        return;
+    }
+
+    size_t stride = vertexSize + texCoordSize;
+    size_t remainder = (stride * vertexCount) / vertexCount;
+    if ((stride < vertexSize) || (remainder != stride)) {
+        ALOGE("Overflow in Mesh(..., %zu, %zu, %zu)", vertexCount, vertexSize,
+                texCoordSize);
+        mVertices = new float[1];
+        mVertices[0] = 0.0f;
+        mVertexCount = 0;
+        mVertexSize = 0;
+        mTexCoordsSize = 0;
+        mStride = 0;
+        return;
+    }
+
+    mVertices = new float[stride * vertexCount];
+    mStride = stride;
 }
 
 Mesh::~Mesh() {
