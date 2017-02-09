00aa9a8a1be6054087784dcf34ff51e66f110c80
Ganesh Mahendran <opensource.ganesh@gmail.com>
2017-02-09 18:04:18 +0800

    servicemanager: Subtract two page in binder vm size
    Kernel requires a guard page in mmap, subtract two pages
    to avoid kernel vm fragmentation.
    
    Change-Id: I9854cb8b0df9b24986b81bb0df6e34a63c36339b
    Signed-off-by: Ganesh Mahendran <opensource.ganesh@gmail.com>

--- cmds/servicemanager/binder.h
+++ cmds/servicemanager/binder.h
@@ -5,6 +5,7 @@
 #define _BINDER_H_
 
 #include <sys/ioctl.h>
+#include <unistd.h>
 #include <linux/android/binder.h>
 
 struct binder_state;
@@ -31,6 +32,8 @@ struct binder_death {
 #define BINDER_SERVICE_MANAGER  0U
 
 #define SVC_MGR_NAME "android.os.IServiceManager"
+
+#define BINDER_VM_SIZE ((1*1024*1024) - (sysconf(_SC_PAGE_SIZE) *2))
 
 enum {
     /* Must match definitions in IBinder.h and IServiceManager.h */
