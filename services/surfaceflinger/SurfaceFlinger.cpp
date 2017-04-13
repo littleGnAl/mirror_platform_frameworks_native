4d14e084cb7f43b94f3a432430244b03a4cfcbb8
sky zhou <ykdsea@gmail.com>
2017-04-13 14:28:32 +0800

    For device composition, clear client target as hwc required.
    [Problem:]
    Netflix have two layers as following:
    layer A has no buffer in bufferqueue.
    layer B has sideband handle, and no buffer in bufferqueue.
    And layer B in on top of layer A.
    
    layer A is set to COMPOSITION_SOLID_COLOR, HWC on my platform changed it to COMPOSITION_CLIENT.
    layer B is set to COMPOSITION_SIDEBAND, HWC changed it to COMPOSITION_DEVICE.
    
    For layer B has no buffer in bufferqueue, the Layer::isOpaque() always return false.
    So the surfaceflinger will never punch hole for layer B, which made we can't see the
    video display.
    
    [Solution:]
    SurfaceFlinger maybe can't decide if need to clear, just do as HWC asked.
    
    Test: Manual - play netflix on Amlogic S905x, which can display video correctlly.
    
    Change-Id: Iff225f1d4c9c693fb2b084766dbea58ceaaed796

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2146,8 +2146,7 @@ bool SurfaceFlinger::doComposeSurfaces(
                     case HWC2::Composition::Sideband:
                     case HWC2::Composition::SolidColor: {
                         const Layer::State& state(layer->getDrawingState());
-                        if (layer->getClearClientTarget(hwcId) && !firstLayer &&
-                                layer->isOpaque(state) && (state.alpha == 1.0f)
+                        if (layer->getClearClientTarget(hwcId) && !firstLayer
                                 && hasClientComposition) {
                             // never clear the very first layer since we're
                             // guaranteed the FB is already cleared
@@ -2156,6 +2155,7 @@ bool SurfaceFlinger::doComposeSurfaces(
                         break;
                     }
                     case HWC2::Composition::Client: {
+                        firstLayer = true;
                         layer->draw(displayDevice, clip);
                         break;
                     }
@@ -2165,7 +2165,6 @@ bool SurfaceFlinger::doComposeSurfaces(
             } else {
                 ALOGV("  Skipping for empty clip");
             }
-            firstLayer = false;
         }
     } else {
         // we're not using h/w composer
