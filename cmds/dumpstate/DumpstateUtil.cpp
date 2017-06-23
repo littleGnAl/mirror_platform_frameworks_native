7d4a420e3b0332f32fdebe7d0efcb387023c4a39
Jiyong Park <jiyong@google.com>
2017-06-23 21:23:16 +0900

    dumpstate/Android.mk is converted to Android.bp
    Converted to soong. And libdumpstateutil is marked as
    vendor_available:true.
    
    Bug: 37342627
    Test: BOARD_VNDK_VERSION=current m libdumpstateutil.vendor -j
    Change-Id: Id1594d9aa2a7035aaad901d4c3881bd6128c431e
    Merged-In: Id1594d9aa2a7035aaad901d4c3881bd6128c431e

--- cmds/dumpstate/DumpstateUtil.cpp
+++ cmds/dumpstate/DumpstateUtil.cpp
@@ -30,8 +30,7 @@
 #include <android-base/properties.h>
 #include <android-base/stringprintf.h>
 #include <android-base/strings.h>
-#include <android-base/unique_fd.h>
-#include <cutils/log.h>
+#include <log/log.h>
 
 #include "DumpstateInternal.h"
 
