be6e52d99ecaac0364e8f0c58840690f234ce87c
Sherry Yang <sherryy@google.com>
2017-07-24 14:12:27 -0700

    Add test to check buffer hdr not in user space
    Add a test to check that the start of the first
    transaction buffer is page aligned and that the
    gap between two consecutive buffers is less than
    or equal to the number of bytes it takes to align
    the first buffer.
    
    Test: run binderLibTest
    Bug: 36007193
    Change-Id: Ibe8b7437a1d532741b0e002a838264f990848f4e
    Signed-off-by: Sherry Yang <sherryy@google.com>

--- libs/binder/tests/binderLibTest.cpp
+++ libs/binder/tests/binderLibTest.cpp
@@ -302,12 +302,33 @@ class BinderLibTestCallBack : public BBinder, public BinderLibTestEvent
                 m_result = data.readInt32();
                 triggerEvent();
                 return NO_ERROR;
+            case BINDER_LIB_TEST_CALL_BACK_VERIFY_BUF: {
+                sp<IBinder> server;
+                int ret;
+                const uint8_t *buf = data.data();
+                size_t size = data.dataSize();
+                if (prev_end)
+                    EXPECT_TRUE((size_t)(buf - prev_end) <= sizeof(void *));
+                else
+                    EXPECT_TRUE(IS_ALIGNED((unsigned long)buf, PAGE_SIZE));
+
+                prev_end = buf + size + data.objectsCount()*sizeof(binder_size_t);
+
+                if (size > 0) {
+                    server = static_cast<BinderLibTestEnv *>(binder_env)->getServer();
+                    ret = server->transact(BINDER_LIB_TEST_INDIRECT_TRANSACTION,
+                                             data, reply);
+                    EXPECT_EQ(NO_ERROR, ret);
+                }
+                return NO_ERROR;
+            }
             default:
                 return UNKNOWN_TRANSACTION;
             }
         }
 
         status_t m_result;
+        const uint8_t *prev_end = NULL;
 };
 
 class TestDeathRecipient : public IBinder::DeathRecipient, public BinderLibTestEvent
@@ -726,6 +747,27 @@ TEST_F(BinderLibTest, FreedBinder) {
          */
         strong->handle = oldHandle;
     }
+}
+
+TEST_F(BinderLibTest, CheckNoHeaderMappedInUser) {
+    status_t ret;
+    Parcel data, reply;
+    sp<BinderLibTestCallBack> callBack = new BinderLibTestCallBack();
+    int buffer_num = 3;
+    for (int i = 0; i < buffer_num; i++) {
+        BinderLibTestBundle datai;
+        datai.appendFrom(&data, 0, data.dataSize());
+
+        data.freeData();
+        data.writeInt32(1);
+        data.writeStrongBinder(callBack);
+        data.writeInt32(BINDER_LIB_TEST_CALL_BACK_VERIFY_BUF);
+
+        datai.appendTo(&data);
+    }
+
+    ret = m_server->transact(BINDER_LIB_TEST_INDIRECT_TRANSACTION, data, &reply);
+    EXPECT_EQ(NO_ERROR, ret);
 }
 
 class BinderLibTestService : public BBinder
