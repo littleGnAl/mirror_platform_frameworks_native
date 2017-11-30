6c2be7df02d96db6ef456e7c548fd5b0e06a2bef
Vishnu Nair <vishnun@google.com>
2017-11-30 15:18:30 -0800

    Fix dumpsys timeout regression
    Bug: 69974956
    Test: adb bugreport ~/tmp.zip
    Change-Id: I833937de55c780773d946ef36a5105e544940bfa
    Merged-In: I833937de55c780773d946ef36a5105e544940bfa

--- cmds/dumpstate/dumpstate.cpp
+++ cmds/dumpstate/dumpstate.cpp
@@ -1207,7 +1207,7 @@ static void dumpstate() {
     printf("========================================================\n");
 
     RunDumpsys("DUMPSYS", {"--skip", "meminfo", "cpuinfo"}, CommandOptions::WithTimeout(90).Build(),
-               10);
+               SEC_TO_MSEC(10));
 
     printf("========================================================\n");
     printf("== Checkins\n");
@@ -1279,8 +1279,10 @@ static void DumpstateTelephonyOnly() {
     printf("== Android Framework Services\n");
     printf("========================================================\n");
 
-    RunDumpsys("DUMPSYS", {"connectivity"}, CommandOptions::WithTimeout(90).Build(), 10);
-    RunDumpsys("DUMPSYS", {"carrier_config"}, CommandOptions::WithTimeout(90).Build(), 10);
+    RunDumpsys("DUMPSYS", {"connectivity"}, CommandOptions::WithTimeout(90).Build(),
+               SEC_TO_MSEC(10));
+    RunDumpsys("DUMPSYS", {"carrier_config"}, CommandOptions::WithTimeout(90).Build(),
+               SEC_TO_MSEC(10));
 
     printf("========================================================\n");
     printf("== Running Application Services\n");
