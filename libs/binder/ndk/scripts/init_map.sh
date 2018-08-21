4797da7d3b20478bcd4e1d4e617cca554efde0fb
Steven Moreland <smoreland@google.com>
2018-08-20 19:47:00 -0700

    WIP: NDK Library for libbinder
    // Things that I explicitly want input on (beyond the code/API itself)
    FIXME: what to name 'AStatus.h' ? 'ABinderProcess.h' ? (and some of the
         functions here)
    FIXME: move AIBinder_registerAsService/AIBinder_getService to an
        IServiceManager API ?? In fact, in the APEX world, we want these
        APIs, but in the NDK world, it seems like there is a separate
        connection mechanism. This I'll probably have to link up through
        IServiceManager or something, but I haven't looked at it.
    FIXME: is there a reason why IBinder remoteBinder/localBinder are
        non-const? I would like to change AIBinder_isRemote to take a const
        AIBinder*, but I can't for this reason.
    FIXME: de-dupe transaction codes/statuses with libbinder (seems like
        this is impossible because of include_dirs/LOCAL_INCLUDE_DIRS).
    FIXME: I like requiring AIBinder_associateClass for future proffing,
        but we could get away with just passing in an interfaceDescriptor
        here. Any preference?
    
    // Thing that I might move to follow-up items or still need to do.
    TODO(b/112664205): make aidl-gen produce interfaces that use these (and
        think about interoperation with existing interfaces).
    FIXME: sometimes getService takes 1s since there is a race for it
        getting registered
    FIXME: add generic class which allows for easy implementation of a
        class like IFoo in this test.
    FIXME: Parcel for other types
    FIXME: Parcel for FD should set ACCEPT_FDS automatically
    
    Bug: 111445392
    Test: ndk/runtests.sh
    
    Change-Id: Ifbca8f0fdf70a3213fe0d94320fc31eeb62408c4

---
+++ libs/binder/ndk/scripts/init_map.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Simple helper for ease of development until this API is frozen.
+
+echo "LIBBINDER_NDK {"
+echo "  global:"
+{
+    grep -oP "AParcel_[a-zA-Z0-9_]+(?=\()" include_ndk/binder/AParcel.h | awk '{ print "    "$0";"; }';
+    grep -oP "AIBinder_[a-zA-Z0-9_]+(?=\()" include_ndk/binder/AIBinder.h | awk '{ print "    "$0";"; }';
+    grep -oP "AWeak_[a-zA-Z0-9_]+(?=\()" include_ndk/binder/AIBinder.h | awk '{ print "    "$0";"; }';
+} | sort | uniq
+echo "  local:"
+echo "    *;"
+echo "};"
