70423ad352f8d823533aceebc0a653eab0f1d3d1
Steven Moreland <smoreland@google.com>
2018-08-28 01:23:02 -0700

    libbinder_ndk: Add C++ wrapper code.
    This library wraps libbinder_ndk with types similar to those found in
    libbinder. This makes implementations more familiar and reduces the
    amount of auto-generated code that has to be repeated in AIDL.
    
    In order to simplify the distribution of the this code, it is
    header-only in the NDK headers. This allows NDK users to use AIDL
    against the NDK with no additional dependencies.
    
    Bug: 112664205
    Test: atest android.binder.cts
    
    Change-Id: I6e6ef646226d474d538639f740e340c4e37fa7f7

---
+++ libs/binder/ndk/include_ndk/android/binder_auto_utils.h
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <android/binder_ibinder.h>
+#include <android/binder_parcel.h>
+#include <android/binder_status.h>
+
+#ifdef __cplusplus
+
+namespace android {
+
+// Represents one strong pointer to an AIBinder object.
+class AutoAIBinder {
+public:
+    // Takes ownership of one strong refcount of binder.
+    explicit AutoAIBinder(AIBinder* binder = nullptr) : mBinder(binder) {}
+
+    AutoAIBinder(const AutoAIBinder& other) { *this = other; }
+    ~AutoAIBinder() { set(nullptr); }
+
+    AutoAIBinder& operator=(const AutoAIBinder& other) {
+        AIBinder_incStrong(other.mBinder);
+        set(other.mBinder);
+        return *this;
+    }
+
+    // Takes ownership of one strong refcount of binder
+    void set(AIBinder* binder) {
+        if (mBinder != nullptr) AIBinder_decStrong(mBinder);
+        mBinder = binder;
+    }
+
+    // This allows the value in this class to be set from beneath it. If you call this method and
+    // then change the value of T*, you must take ownership of the value you are replacing.
+    //
+    // Recommended use is like this:
+    //   AutoAIBinder a; // will be nullptr
+    //   SomeInitFunction(a.getR()); // value is initialized
+    //
+    AIBinder** getR() { return &mBinder; }
+    AIBinder* get() { return mBinder; }
+    AIBinder* get() const { return mBinder; }
+
+    AIBinder* release() {
+        AIBinder* ret = mBinder;
+        mBinder = nullptr;
+        return ret;
+    }
+
+private:
+    AIBinder* mBinder = nullptr;
+};
+
+template <typename T, void (*Destroy)(T*)>
+class AutoA {
+public:
+    // Takes ownership of one strong refcount of T.
+    explicit AutoA(T* t = nullptr) : mT(t) {}
+    ~AutoA() { set(nullptr); }
+
+    void set(T* t) {
+        Destroy(mT);
+        mT = t;
+    }
+
+    T* get() { return mT; }
+    const T* get() const { return mT; }
+
+    T* release() {
+        T* ret = mT;
+        mT = nullptr;
+        return ret;
+    }
+
+    // This allows the value in this class to be set from beneath it. If you call this method and
+    // then change the value of T*, you must take ownership of the value you are replacing.
+    //
+    // Recommended use is like this:
+    //   AutoA<T> a; // will be nullptr
+    //   SomeInitFunction(a.getR()); // value is initialized
+    //
+    T** getR() { return &mT; }
+
+    // copy-constructing, or move/copy assignment is disallowed
+    AutoA(const AutoA&) = delete;
+    AutoA& operator=(const AutoA&) = delete;
+    AutoA& operator=(AutoA&&) = delete;
+
+    // move-constructing is okay
+    AutoA(AutoA&&) = default;
+
+private:
+    T* mT;
+};
+
+typedef AutoA<AParcel, AParcel_delete> AutoAParcel;
+typedef AutoA<AStatus, AStatus_delete> AutoAStatus;
+typedef AutoA<AIBinder_DeathRecipient, AIBinder_DeathRecipient_delete> AutoAIBinder_DeathRecipient;
+typedef AutoA<AIBinder_Weak, AIBinder_Weak_delete> AutoAIBinder_Weak;
+
+} // namespace android
+
+#endif // __cplusplus
