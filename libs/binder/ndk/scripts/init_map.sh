e06a788821e5cf9edd2db4b73efde84534bea469
Steven Moreland <smoreland@google.com>
2018-08-29 01:52:09 -0700

    Add libbinder_ndk to the NDK.
    This library is a C API over libbinder. Its intended to be used as a
    bakend of AIDL, but implemented services direclty in this protocol is
    also possible.
    
    Bug: 111445392
    Test: runtests.sh
    Change-Id: Iaf3adc0dce0646dcc04e78ae33cccdf0c29d613c

---
+++ libs/binder/ndk/scripts/init_map.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Simple helper for ease of development until this API is frozen.
+
+echo "LIBBINDER_NDK {"
+echo "  global:"
+{
+    grep -oP "AParcel_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_parcel.h | awk '{ print "    "$0";"; }';
+    grep -oP "AIBinder_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_ibinder.h | awk '{ print "    "$0";"; }';
+} | sort | uniq | awk '{ print $0 " # introduced=29"; }'
+echo "  local:"
+echo "    *;"
+echo "};"
