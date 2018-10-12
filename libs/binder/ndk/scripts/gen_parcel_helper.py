9b288df38b507b91b9026b317786cd439723e398
Steven Moreland <smoreland@google.com>
2018-10-12 11:53:03 -0700

    libbinder_ndk: read/write array for primitive types.
       type  | contiguous in NDK API | contiguous on the wire
      --------------------------------------------------------
       bool  | no*                   | no
       char  | yes                   | no**
       other | yes                   | yes
    
     *  std::vector<bool> as an implementation detail stores a packed byte
        array. This packed byte array is not exposed as an implementation
        detail, so even if we made bool contiguous on the wire, we couldn't
        ever make it contiguous in the NDK API.
    
     ** read/write Int32/Int is called repeatedly which writes each
        char16_t into a 32-bit wide section on the wire.
    
    Arrays in Java AIDL are also very annoying. Whereas in C++, their size
    can be changed, in Java, because of the way out-parameters work, the
    array size cannot be changed. We might consider adding a better error
    when this happens.
    
    Bug: 111445392
    Test: atest android.binder.cts
    
    Change-Id: I3934f7d5cd749810c4a512ac3fb57b63f9ae4ea5

--- libs/binder/ndk/scripts/gen_parcel_helper.py
+++ libs/binder/ndk/scripts/gen_parcel_helper.py
@@ -30,13 +30,15 @@ data_types = [
     ("Byte", "int8_t"),
 ]
 
-def replaceFileTags(path, content):
+non_contiguously_addressable = {"Bool"}
+
+def replaceFileTags(path, content, start_tag, end_tag):
     print("Updating", path)
     with open(path, "r+") as f:
         lines = f.readlines()
 
-        start = lines.index("// @START\n")
-        end = lines.index("// @END\n")
+        start = lines.index("// @" + start_tag + "\n")
+        end = lines.index("// @" + end_tag + "\n")
 
         if end <= start or start < 0 or end < 0:
             print("Failed to find tags in", path)
@@ -59,8 +61,10 @@ def main():
 
     print("Updating auto-generated code")
 
+    pre_header = ""
     header = ""
     source = ""
+    cpp_helper = ""
 
     for pretty, cpp in data_types:
         header += "/**\n"
@@ -82,8 +86,119 @@ def main():
         source += "    return PruneStatusT(status);\n"
         source += "}\n\n"
 
-    replaceFileTags(ROOT + "include_ndk/android/binder_parcel.h", header)
-    replaceFileTags(ROOT + "parcel.cpp", source)
+    for pretty, cpp in data_types:
+        nca = pretty in non_contiguously_addressable
+
+        arg_type = "const " + cpp + "* value"
+        if nca: arg_type = "const void* arrayData, AParcel_" + pretty.lower() + "_array_getter getter"
+        args = "value, length"
+        if nca: args = "arrayData, getter, length, &Parcel::write" + pretty
+
+        header += "/**\n"
+        header += " * Writes an array of " + cpp + " to the next location in a non-null parcel.\n"
+        header += " */\n"
+        header += "binder_status_t AParcel_write" + pretty + "Array(AParcel* parcel, " + arg_type + ", size_t length) __INTRODUCED_IN(29);\n\n"
+        source += "binder_status_t AParcel_write" + pretty + "Array(AParcel* parcel, " + arg_type + ", size_t length) {\n"
+        source += "    return WriteArray<" + cpp + ">(parcel, " + args + ");\n";
+        source += "}\n\n"
+
+    for pretty, cpp in data_types:
+        nca = pretty in non_contiguously_addressable
+
+        extra_getter_args = ""
+        if nca: extra_getter_args = ", size_t index"
+        getter_return = cpp + "*"
+        if nca: getter_return = cpp
+        getter_statement = "vec->data()"
+        if nca: getter_statement = "(*vec)[index]"
+        getter_comment_type = cpp + " array"
+        if nca: getter_comment_type = "value in a " + cpp + " array at index "
+        getter_array_data = "void* arrayData"
+        if nca: getter_array_data = "const void* arrayData"
+
+        getter_type = "AParcel_" + pretty.lower() + "_array_getter"
+        setter_type = "AParcel_" + pretty.lower() + "_array_setter"
+
+        pre_header += "/**\n"
+        pre_header += " * This is called to get the underlying data from an arrayData object.\n"
+        pre_header += " *\n"
+        pre_header += " * This will never be called for an empty array.\n"
+        pre_header += " */\n"
+        pre_header += "typedef " + getter_return + " (*" + getter_type + ")(" + getter_array_data + extra_getter_args + ");\n\n"
+
+        if nca:
+            pre_header += "/**\n"
+            pre_header += " * This is called to set an underlying value in an arrayData object at index.\n"
+            pre_header += " */\n"
+            pre_header += "typedef void (*" + setter_type + ")(void* arrayData, size_t index, " + cpp + " value);\n\n"
+
+        read_using = "getter"
+        if nca: read_using = "setter"
+        read_type = getter_type
+        if nca: read_type = setter_type
+
+        arguments = ["const AParcel* parcel"]
+        arguments += ["void** arrayData"]
+        arguments += ["AParcel_array_reallocator reallocator"]
+        arguments += [read_type + " " + read_using]
+        arguments = ", ".join(arguments)
+
+        header += "/**\n"
+        header += " * Reads an array of " + cpp + " from the next location in a non-null parcel.\n"
+        header += " */\n"
+        header += "binder_status_t AParcel_read" + pretty + "Array(" + arguments + ") __INTRODUCED_IN(29);\n\n"
+        source += "binder_status_t AParcel_read" + pretty + "Array(" + arguments + ") {\n"
+        additional_args = ""
+        if nca: additional_args = ", &Parcel::read" + pretty
+        source += "    return ReadArray<" + cpp + ">(parcel, arrayData, reallocator, " + read_using + additional_args + ");\n";
+        source += "}\n\n"
+
+        const_if_nca = ""
+        if nca: const_if_nca = "const "
+
+        cpp_helper += "/**\n"
+        cpp_helper += " * This retrieves the underlying " + getter_comment_type + " from a corresponding vectorData.\n"
+        cpp_helper += " */\n"
+        cpp_helper += "static inline " + getter_return + " AParcel_" + pretty.lower() + "_std_vector_getter(" + const_if_nca + "void* vectorData" + extra_getter_args + ") {\n"
+        cpp_helper += "    " + const_if_nca + "std::vector<" + cpp + ">* vec = static_cast<" + const_if_nca + "std::vector<" + cpp + ">*>(vectorData);\n"
+        cpp_helper += "    return " + getter_statement + ";\n"
+        cpp_helper += "}\n\n"
+
+        if nca:
+            cpp_helper += "/**\n"
+            cpp_helper += " * This sets the underlying " + cpp + " in a corresponding vectorData.\n"
+            cpp_helper += " */\n"
+            cpp_helper += "static inline void AParcel_" + pretty.lower() + "_std_vector_setter(void* vectorData, size_t index, " + cpp + " value) {\n"
+            cpp_helper += "    std::vector<" + cpp + ">* vec = static_cast<std::vector<" + cpp + ">*>(vectorData);\n"
+            cpp_helper += "    (*vec)[index] = value;\n"
+            cpp_helper += "}\n\n"
+
+        cpp_helper += "/**\n"
+        cpp_helper += " * Writes a vector of " + cpp + " to the next location in a non-null parcel.\n"
+        cpp_helper += " */\n"
+        cpp_helper += "static inline binder_status_t AParcel_write" + pretty + "Array(AParcel* parcel, const std::vector<" + cpp + ">& vec) {\n"
+        write_args = "vec.data()"
+        if nca: write_args = "static_cast<const void*>(&vec), AParcel_" + pretty.lower() + "_std_vector_getter"
+        cpp_helper += "    return AParcel_write" + pretty + "Array(parcel, " + write_args + ", vec.size());\n"
+        cpp_helper += "}\n\n"
+
+        cpp_helper += "/**\n"
+        cpp_helper += " * Reads a vector of " + cpp + " from the next location in a non-null parcel.\n"
+        cpp_helper += " */\n"
+        cpp_helper += "static inline binder_status_t AParcel_read" + pretty + "Array(const AParcel* parcel, std::vector<" + cpp + ">* vec) {\n"
+        cpp_helper += "    void* vectorData = static_cast<void*>(vec);\n"
+        read_args = []
+        read_args += ["parcel"]
+        read_args += ["&vectorData"]
+        read_args += ["&AParcel_std_vector_reallocator<" + cpp + ">"]
+        read_args += ["AParcel_" + pretty.lower() + "_std_vector_" + read_using]
+        cpp_helper += "    return AParcel_read" + pretty + "Array(" + ", ".join(read_args) + ");\n"
+        cpp_helper += "}\n\n"
+
+    replaceFileTags(ROOT + "include_ndk/android/binder_parcel.h", pre_header, "START-PRIMITIVE-VECTOR-GETTERS", "END-PRIMITIVE-VECTOR-GETTERS")
+    replaceFileTags(ROOT + "include_ndk/android/binder_parcel.h", header, "START-PRIMITIVE-READ-WRITE", "END-PRIMITIVE-READ-WRITE")
+    replaceFileTags(ROOT + "parcel.cpp", source, "START", "END")
+    replaceFileTags(ROOT + "include_ndk/android/binder_parcel_utils.h", cpp_helper, "START", "END")
 
     print("Updating DONE.")
 
