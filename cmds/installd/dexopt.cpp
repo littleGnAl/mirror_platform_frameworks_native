771f315f1687806ffba7c5bc50794b0a3129a3f9
Mathieu Chartier <mathieuc@google.com>
2018-11-01 12:07:26 -0700

    Make sure to return false for empty classpath
    Previously, the binder call was returning true causing the test to
    fail.
    
    Test: adb shell data/nativetest/installd_dexopt_test/installd_dexopt_test
    Bug: 118818285
    Change-Id: I1a4c3ee4a0e9ed614c15c95f88489b6bf4e66fa5

--- cmds/installd/dexopt.cpp
+++ cmds/installd/dexopt.cpp
@@ -2693,6 +2693,11 @@ static bool create_boot_image_profile_snapshot(const std::string& package_name,
         return false;
     }
 
+    // Return false for empty class path.
+    if (classpath.empty()) {
+        return false;
+    }
+
     // Open and create the snapshot profile.
     unique_fd snapshot_fd = open_spnashot_profile(AID_SYSTEM, package_name, profile_name);
 
