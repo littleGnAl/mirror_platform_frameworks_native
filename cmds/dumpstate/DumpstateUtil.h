438ba01a3937152efbf3c671bbdc0ab855f635b7
Nandana Dutt <nandana@google.com>
2018-11-02 16:17:05 +0000

    Added system property (dumpstate.unroot).
    This property allows overriding availability of "su root".
    This is useful to emulate the user build behavior of some
    commands on user debug builds.
    
    Fixes: 117980186
    Test: adb shell setprop dumpstate.unroot true && \
          adb shell cmd activity bug-report --progress
    Test: adb shell /data/nativetest64/dumpstate_test/dumpstate_test  --gtest_filter=DumpstateTest.RunCommandUnrootUserDebugBuild
    
    Change-Id: I8d8d2f0733a8d13754c7d2e6a0d323c73afcbbbb

--- cmds/dumpstate/DumpstateUtil.h
+++ cmds/dumpstate/DumpstateUtil.h
@@ -162,9 +162,17 @@ class PropertiesHelper {
      */
     static bool IsDryRun();
 
+    /**
+     * Checks whether root availability should be overridden.
+     *
+     * Useful to verify how dumpstate would work in a device with an user build.
+     */
+    static bool IsUnroot();
+
   private:
     static std::string build_type_;
     static int dry_run_;
+    static int unroot_;
 };
 
 /*
