ae8eb1abcd804c46c358026da7c0a753ae9dd622
Olivier Gaillard <gaillard@google.com>
2018-11-14 15:35:50 +0000

    Rename WorkSource methods on IPCThreadState.
    Test: unit tests
    Change-Id: I0ed38b0e3cfdeea1dbaf2562271f9390d1e992c1
    Merged-In: I0ed38b0e3cfdeea1dbaf2562271f9390d1e992c1

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -599,7 +599,7 @@ status_t Parcel::writeInterfaceToken(const String16& interface)
 {
     writeInt32(IPCThreadState::self()->getStrictModePolicy() |
                STRICT_MODE_PENALTY_GATHER);
-    writeInt32(IPCThreadState::self()->getWorkSource());
+    writeInt32(IPCThreadState::self()->getCallingWorkSourceUid());
     // currently the interface identification token is just its name as a string
     return writeString16(interface);
 }
@@ -629,7 +629,7 @@ bool Parcel::enforceInterface(const String16& interface,
     }
     // WorkSource.
     int32_t workSource = readInt32();
-    threadState->setWorkSource(workSource);
+    threadState->setCallingWorkSourceUid(workSource);
     // Interface descriptor.
     const String16 str(readString16());
     if (str == interface) {
