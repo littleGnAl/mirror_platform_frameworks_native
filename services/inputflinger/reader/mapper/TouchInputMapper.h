331b10fcb333dbe220401dee070d364ebbd6547b
Nathaniel R. Lewis <nathaniel.lewis@sony.com>
2019-08-21 04:46:29 +0000

    Add relative coordinates for touchpad events.
    By their nature, touchpad devices deal in absolute coordinates. It
    may prove useful for app developers to receive information about how
    touches have changed position since the last event.
    
    Test: connect a device that android classifies as a touchpad (such
          as a DualShock 4 in touchpad capture mode). MotionEvents
          received in an app should have valid content in AXIS_RELATIVE_X
          and AXIS_RELATIVE_Y fields.
    
    Bug: 38511270
    Change-Id: Ic96323464af6f11027fbf36ee00e5d226c1b8626

--- services/inputflinger/reader/mapper/TouchInputMapper.h
+++ services/inputflinger/reader/mapper/TouchInputMapper.h
@@ -102,7 +102,7 @@ struct CookedPointerData {
     uint32_t pointerCount;
     PointerProperties pointerProperties[MAX_POINTERS];
     PointerCoords pointerCoords[MAX_POINTERS];
-    BitSet32 hoveringIdBits, touchingIdBits;
+    BitSet32 hoveringIdBits, touchingIdBits, canceledIdBits, validIdBits;
     uint32_t idToIndex[MAX_POINTER_ID + 1];
 
     CookedPointerData();
@@ -128,6 +128,8 @@ struct CookedPointerData {
     inline bool isTouching(uint32_t pointerIndex) const {
         return touchingIdBits.hasBit(pointerProperties[pointerIndex].id);
     }
+
+    inline bool hasPointerCoordsForId(uint32_t id) const { return validIdBits.hasBit(id); }
 };
 
 class TouchInputMapper : public InputMapper {
