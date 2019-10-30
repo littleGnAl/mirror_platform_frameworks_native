610b440297a03f402b60d405768991b41a5588da
Daniel Norman <danielnorman@google.com>
2019-10-30 10:33:22 -0700

    Changes readByteVectorInternal to use readInplace.
    Bug: 143592429
    Test: system/tools/aidl/runtests.sh
    Change-Id: Ie22bb8f9016c3dd5b3b9531a1b0ee0cc8a8e59e1

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -1449,21 +1449,31 @@ restart_write:
     return err;
 }
 
-status_t Parcel::readByteVectorInternal(int8_t* data, size_t size) const {
-    if (size_t(size) > dataAvail()) {
-      return BAD_VALUE;
+const int8_t* Parcel::readByteVectorInternal(size_t size) const {
+    if (size > dataAvail()) {
+      return nullptr;
     }
-    return read(data, size);
+    return reinterpret_cast<const int8_t*>(readInplace(size));
 }
 
 status_t Parcel::readByteVector(std::vector<int8_t>* val) const {
     if (status_t status = resizeOutVector(val); status != OK) return status;
-    return readByteVectorInternal(val->data(), val->size());
+    const size_t size = val->size();
+    const int8_t* data = readByteVectorInternal(size);
+    if (!data) return BAD_VALUE;
+    val->clear();
+    val->insert(val->begin(), data, data+size);
+    return NO_ERROR;
 }
 
 status_t Parcel::readByteVector(std::vector<uint8_t>* val) const {
     if (status_t status = resizeOutVector(val); status != OK) return status;
-    return readByteVectorInternal(reinterpret_cast<int8_t*>(val->data()), val->size());
+    const size_t size = val->size();
+    const int8_t* data = readByteVectorInternal(size);
+    if (!data) return BAD_VALUE;
+    val->clear();
+    val->insert(val->begin(), data, data+size);
+    return NO_ERROR;
 }
 
 status_t Parcel::readByteVector(std::unique_ptr<std::vector<int8_t>>* val) const {
@@ -1473,7 +1483,12 @@ status_t Parcel::readByteVector(std::unique_ptr<std::vector<int8_t>>* val) const
         // This occurs when writing a null byte vector.
         return OK;
     }
-    return readByteVectorInternal((*val)->data(), (*val)->size());
+    const size_t size = (*val)->size();
+    const int8_t* data = readByteVectorInternal(size);
+    if (!data) return BAD_VALUE;
+    (*val)->clear();
+    (*val)->insert((*val)->begin(), data, data+size);
+    return NO_ERROR;
 }
 
 status_t Parcel::readByteVector(std::unique_ptr<std::vector<uint8_t>>* val) const {
@@ -1483,7 +1498,12 @@ status_t Parcel::readByteVector(std::unique_ptr<std::vector<uint8_t>>* val) cons
         // This occurs when writing a null byte vector.
         return OK;
     }
-    return readByteVectorInternal(reinterpret_cast<int8_t*>((*val)->data()), (*val)->size());
+    const size_t size = (*val)->size();
+    const int8_t* data = readByteVectorInternal(size);
+    if (!data) return BAD_VALUE;
+    (*val)->clear();
+    (*val)->insert((*val)->begin(), data, data+size);
+    return NO_ERROR;
 }
 
 status_t Parcel::readInt32Vector(std::unique_ptr<std::vector<int32_t>>* val) const {
