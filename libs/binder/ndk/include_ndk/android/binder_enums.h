04775bb4d5b914c4245b08c03350c4e8c0f82e5c
Jooyung Han <jooyung@google.com>
2019-12-13 05:27:37 +0900

    binder: Add enum_range<Enum>
    Native backends of AIDL enum types support a template variable for
    enumerating all values of the type.
    
    Bug: 143992652
    Test: system/tools/aidl/runtests.sh
          atest CtsNdkBinderTestCases
    
    Change-Id: I3c3d24a6f24c4c02ac5c5b59bb0831c26f695dd2

---
+++ libs/binder/ndk/include_ndk/android/binder_enums.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @addtogroup NdkBinder
+ * @{
+ */
+
+/**
+ * @file binder_enums.h
+ * @brief Helpers for AIDL enum types.
+ */
+
+#pragma once
+
+#include <iterator>
+#include <type_traits>
+
+namespace ndk {
+
+namespace details {
+/**
+ * Never instantiated. Used as a placeholder for template variables.
+ * @hide
+ */
+template <typename T>
+struct aidl_invalid_type;
+
+/**
+ * AIDL generates specializations of this for enums.
+ * @hide
+ */
+template <typename EnumType, typename = std::enable_if_t<std::is_enum<EnumType>::value>>
+constexpr aidl_invalid_type<EnumType> enum_values;
+}  // namespace details
+
+/**
+ * Iterable interface to enumerate all values of AIDL enum types.
+ */
+template <typename EnumType, typename = std::enable_if_t<std::is_enum<EnumType>::value>>
+struct enum_range {
+    /**
+     * Return an iterator pointing to the first enum value.
+     */
+    constexpr auto begin() const { return std::begin(details::enum_values<EnumType>); }
+    /**
+     * Return an iterator pointing to one past the last enum value.
+     */
+    constexpr auto end() const { return std::end(details::enum_values<EnumType>); }
+};
+
+}  // namespace ndk
+
+/** @} */
