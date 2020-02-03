597865938393e22a26b0f8e4d336fa9f85f1d12d
Sam Saccone <samccone@google.com>
2020-02-02 21:22:40 -0800

    Codelab change example.
    Test: manual atest
    Change-Id: I66ac158bfbe14d339903c39c93419b3277f4f6ee

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+
+// even better!!!
+
 //#define LOG_NDEBUG 0
 #define ATRACE_TAG ATRACE_TAG_GRAPHICS
 
@@ -1837,6 +1840,8 @@ void SurfaceFlinger::handleMessageRefresh() {
 
     postFrame();
     postComposition();
+    mClientColorMatrix = mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                              vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
 
     mHadClientComposition = false;
     mHadDeviceComposition = false;
