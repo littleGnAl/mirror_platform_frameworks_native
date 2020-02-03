bdc9964ad30713f034aca5af377a6df986ca7d6b
Martijn Coenen <maco@google.com>
2020-02-03 10:01:36 +0100

    Fix defaultServiceManager() locking and retry loop.
    Use a static instead of double-checked locking.
    
    Bug: 142838845
    Test: builds
    Change-Id: I0b42539cae70a70c6a23798c27f6b088bb5d3b40

--- libs/binder/IServiceManager.cpp
+++ libs/binder/IServiceManager.cpp
@@ -85,31 +85,29 @@ private:
     sp<AidlServiceManager> mTheRealServiceManager;
 };
 
-static Mutex gDefaultServiceManagerLock;
+static std::once_flag sm_once;
 static sp<IServiceManager> gDefaultServiceManager;
 
 sp<IServiceManager> defaultServiceManager()
 {
-
-    if (gDefaultServiceManager != nullptr) return gDefaultServiceManager;
-
-    {
-        AutoMutex _l(gDefaultServiceManagerLock);
-        while (gDefaultServiceManager == nullptr) {
-            gDefaultServiceManager = new ServiceManagerShim(
-                interface_cast<AidlServiceManager>(
-                    ProcessState::self()->getContextObject(nullptr)));
-            if (gDefaultServiceManager == nullptr)
+    std::call_once(sm_once, []() {
+        sp<AidlServiceManager> sm = nullptr;
+        while (sm == nullptr) {
+            sm = interface_cast<AidlServiceManager>(ProcessState::self()->getContextObject(nullptr));
+            if (sm == nullptr) {
                 sleep(1);
+            }
         }
-    }
+
+        gDefaultServiceManager = new ServiceManagerShim(sm);
+    });
 
     return gDefaultServiceManager;
 }
 
+// Note: this is only used for testing, and not thread-safe
 void setDefaultServiceManager(const sp<IServiceManager>& sm) {
-  AutoMutex _l(gDefaultServiceManagerLock);
-  gDefaultServiceManager = sm;
+    gDefaultServiceManager = sm;
 }
 
 #if !defined(__ANDROID_VNDK__) && defined(__ANDROID__)
