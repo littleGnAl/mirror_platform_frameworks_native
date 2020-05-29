17caa1cd72acb76b4c59ac663fb050ce878b9ce9
Steven Moreland <smoreland@google.com>
2020-05-29 21:44:41 +0000

    Allow AIBinder<->IBinder when sharing libbinder.
    Bug: N/A
    Test: libbinder_ndk_unit_test
    Change-Id: I4051e32e487e92ee70f85a733877cd709fc510eb

--- libs/binder/ndk/include_platform/android/binder_ibinder_platform.h
+++ libs/binder/ndk/include_platform/android/binder_ibinder_platform.h
@@ -18,6 +18,10 @@
 
 #include <android/binder_ibinder.h>
 
+#if !defined(__ANDROID_APEX__) && !defined(__ANDROID_VNDK__)
+#include <binder/IBinder.h>
+#endif
+
 __BEGIN_DECLS
 
 /**
@@ -44,3 +48,25 @@ void AIBinder_setRequestingSid(AIBinder* binder, bool requestingSid) __INTRODUCE
 __attribute__((warn_unused_result)) const char* AIBinder_getCallingSid() __INTRODUCED_IN(31);
 
 __END_DECLS
+
+#if !defined(__ANDROID_APEX__) && !defined(__ANDROID_VNDK__)
+
+/**
+ * Get libbinder version of binder from AIBinder.
+ *
+ * \param binder binder with ownership retained by the client
+ * \return platform binder object
+ */
+android::sp<android::IBinder> AIBinder_toPlatformBinder(AIBinder* binder);
+
+/**
+ * Get libbinder_ndk version of binder from platform binder.
+ *
+ * \param binder platform binder which may be from anywhere (doesn't have to be
+ * created with libbinder_ndK)
+ * \return binder with one reference count of ownership given to the client. See
+ * AIBinder_decStrong
+ */
+AIBinder* AIBinder_fromPlatformBinder(const android::sp<android::IBinder>& binder);
+
+#endif
