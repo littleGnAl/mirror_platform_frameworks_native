4840bfab506ff53f1d81f31260ebbc804e8fd0d3
Stephen Crane <sjc@immunant.com>
2020-06-16 17:48:35 -0700

    Initial Binder Rust crate
    This crate exposes an idiomatic Binder interface for Rust clients and
    services. This interface is primarily designed for use by the
    forthcoming Rust backend for the AIDL compiler. The crate links against
    the stable NDK version of the libbinder interface and therefore can
    potentially be used by APEX packages.
    
    Test: atest rustBinderTest_integration
    Test: libbinder_rs-internal_test_binder
    Change-Id: I2e9b800186f24217edeb1c3778f13902a3cca2fd

---
+++ libs/binder/rust/tests/integration.rs
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use binder::declare_binder_proxy;
+use binder::interfaces::service_manager::BpServiceManager;
+use binder::parcel::Parcel;
+use binder::{
+    binder_fn_get_class, Binder, IBinder, ProcessState, Remotable, SpIBinder, TransactionCode,
+};
+
+#[test]
+fn servicemanager_connect() {
+    let mut sm: BpServiceManager =
+        binder::get_interface("manager").expect("Did not get manager binder service");
+    assert!(sm.is_binder_alive());
+    assert!(sm.ping_binder().is_ok());
+}
+
+struct TestService;
+
+impl Remotable for TestService {
+    const DESCRIPTOR: &'static str = <Self as ITest>::DESCRIPTOR;
+
+    fn on_transact(
+        &self,
+        _code: TransactionCode,
+        _data: &Parcel,
+        reply: &mut Parcel,
+    ) -> binder::Result<()> {
+        reply.write(&self.test()?)?;
+        Ok(())
+    }
+
+    binder_fn_get_class!(Binder<Self>);
+}
+
+impl ITest for TestService {
+    fn test(&self) -> binder::Result<String> {
+        Ok("testing service".to_string())
+    }
+}
+
+pub trait ITest {
+    const DESCRIPTOR: &'static str = "android.os.ITest";
+
+    fn test(&self) -> binder::Result<String>;
+}
+
+declare_binder_proxy!(BpTest { x: i32 = 100 }: ITest, TestService);
+
+impl ITest for BpTest {
+    fn test(&self) -> binder::Result<String> {
+        let reply = self
+            .binder
+            .transact(SpIBinder::FIRST_CALL_TRANSACTION, 0, |_| Ok(()))?;
+        reply.read()
+    }
+}
+
+#[test]
+fn run_server() {
+    ProcessState::start_thread_pool();
+    let binder_native = Binder::new(TestService);
+    let res = binder::add_service("testing", binder_native.as_binder());
+    assert!(res.is_ok());
+
+    let test_client: BpTest =
+        binder::get_interface("testing").expect("Did not get manager binder service");
+    assert_eq!(test_client.test().unwrap(), "testing service");
+}
