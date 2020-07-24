bb10652af6028dffd701a35d531ca849b9434491
dakshesh <daksheshkumar@google.com>
2020-07-24 04:33:00 +0000

    Fuzzers for libinput
    Covers InputTransport.h, KeyCharacterMap.h and VelocityTracker.h
    
    Line Coverage:
    InputTransport.cpp: ~54%
    KeyCharacterMap.cpp: ~50% (parsing is fully covered)
    VelocityTracker.cpp: ~85%
    
    Bug: 161984404
    Test: Builds and runs on device(pixel 4)
    Change-Id: I96b556912dbd69266c2ad54efc56a6ff96b1ee94

---
+++ libs/input/tests/fuzzers/KeyCharacterMap_fuzzer.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fuzzer/FuzzedDataProvider.h>
+#include <input/KeyCharacterMap.h>
+
+static char TEMP_FILE_NAME[] = "/data/local/tmp/keycharactermapXXXXXXXXX";
+size_t TEMP_FILE_NAME_LEN = sizeof(TEMP_FILE_NAME);
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    FuzzedDataProvider fdp(data, size);
+
+    // Three different formats available.
+    android::KeyCharacterMap::Format format =
+            static_cast<android::KeyCharacterMap::Format>(fdp.ConsumeIntegralInRange(0, 3));
+
+    char filePath[TEMP_FILE_NAME_LEN];
+    memcpy(filePath, TEMP_FILE_NAME, TEMP_FILE_NAME_LEN);
+
+    int fd = mkstemp(filePath);
+
+    if (fd < 0) {
+        return -1;
+    }
+
+    write(fd, data, size);
+    close(fd);
+
+    android::sp<android::KeyCharacterMap> m;
+    android::KeyCharacterMap::load(filePath, format, &m);
+
+    remove(filePath);
+
+    return 0;
+}
