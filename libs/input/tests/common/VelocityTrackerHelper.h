eff3ea1276f51dae630a0d4cf5f57007f785e402
dakshesh <daksheshkumar@google.com>
2020-07-24 04:33:00 +0000

    Fuzzers for libinput
    It covers InputTransport.h, KeyCharacterMap.h and VelocityTracker.h. InputTransport and VelocityTracker Fuzzers were created by adapting existing test cases.
    
    A common header for VelocityTracker_fuzzer and VelocityTracker_test was
    added and code was pulled out from VelocityTracker_test.cpp.
    
    Line Coverage:
    InputTransport.cpp: ~60% cov and ~71% functions  ~15000 exec/s
    KeyCharacterMap.cpp: ~50% cov (parsing is fully covered) ~7500 exec/s
    VelocityTracker.cpp: ~85% cov  ~6500 exec/s
    
    To Build the fuzzers(for eg : VelocityTracker_fuzzer) :
    SANITZE_TARGET=hwaddress m VelocityTracker_fuzzer
    
    To run :
    adb shell data/fuzz/arm64/VelocityTracker_fuzzer/VelocityTracker_fuzzer
    
    Fuzzers are not host_supported, so they would only run on device.
    
    Bug: 161984404
    Test: Builds and runs on device(pixel 4)
    Change-Id: I96b556912dbd69266c2ad54efc56a6ff96b1ee94

---
+++ libs/input/tests/common/VelocityTrackerHelper.h
@@ -0,0 +1,168 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/stringprintf.h>
+#include <input/VelocityTracker.h>
+#include <math.h>
+
+#include <chrono>
+#include <optional>
+
+namespace android {
+
+constexpr int32_t DISPLAY_ID = ADISPLAY_ID_DEFAULT; // default display id
+
+constexpr uint32_t DEFAULT_POINTER_ID = 0; // pointer ID used for manually defined tests
+
+struct Position {
+    float x;
+    float y;
+
+    bool isValid() const { return !(isnan(x) && isnan(y)); }
+};
+
+struct MotionEventEntry {
+    std::chrono::nanoseconds eventTime;
+    std::vector<Position> positions;
+};
+
+static BitSet32 getValidPointers(const std::vector<Position>& positions) {
+    BitSet32 pointers;
+    for (size_t i = 0; i < positions.size(); i++) {
+        if (positions[i].isValid()) {
+            pointers.markBit(i);
+        }
+    }
+    return pointers;
+}
+
+static std::optional<uint32_t> getChangingPointerId(BitSet32 pointers, BitSet32 otherPointers) {
+    BitSet32 difference(pointers.value ^ otherPointers.value);
+    uint32_t pointerId = difference.clearFirstMarkedBit();
+    if (difference.value != 0U) {
+        ALOGE("Only 1 pointer can enter or leave at a time");
+        return std::nullopt;
+    }
+    return pointerId;
+}
+
+static std::optional<int32_t> resolveAction(const std::vector<Position>& lastPositions,
+                                            const std::vector<Position>& currentPositions,
+                                            const std::vector<Position>& nextPositions) {
+    BitSet32 pointers = getValidPointers(currentPositions);
+    const uint32_t pointerCount = pointers.count();
+
+    BitSet32 lastPointers = getValidPointers(lastPositions);
+    const uint32_t lastPointerCount = lastPointers.count();
+    if (lastPointerCount < pointerCount) {
+        // A new pointer is down
+        std::optional<uint32_t> pointerId = getChangingPointerId(pointers, lastPointers);
+        if (pointerId == std::nullopt) {
+            return std::nullopt;
+        }
+        return AMOTION_EVENT_ACTION_POINTER_DOWN |
+                (pointerId.value() << AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT);
+    }
+
+    BitSet32 nextPointers = getValidPointers(nextPositions);
+    const uint32_t nextPointerCount = nextPointers.count();
+    if (pointerCount > nextPointerCount) {
+        // An existing pointer is leaving
+        std::optional<uint32_t> pointerId = getChangingPointerId(pointers, nextPointers);
+        if (pointerId == std::nullopt) {
+            return std::nullopt;
+        }
+        return AMOTION_EVENT_ACTION_POINTER_UP |
+                (pointerId.value() << AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT);
+    }
+
+    return AMOTION_EVENT_ACTION_MOVE;
+}
+
+static std::optional<std::vector<MotionEvent>> createMotionEventStream(
+        const std::vector<MotionEventEntry>& motions) {
+    if (motions.empty()) {
+        ALOGE("Need at least 1 sample to create a MotionEvent. Received empty vector.");
+        return std::nullopt;
+    }
+
+    std::vector<MotionEvent> events;
+    for (size_t i = 0; i < motions.size(); i++) {
+        const MotionEventEntry& entry = motions[i];
+        BitSet32 pointers = getValidPointers(entry.positions);
+        const uint32_t pointerCount = pointers.count();
+
+        int32_t action;
+        if (i == 0) {
+            action = AMOTION_EVENT_ACTION_DOWN;
+            if (pointerCount != 1U) {
+                ALOGE("First event should only have 1 pointer");
+                return std::nullopt;
+            }
+        } else if (i == motions.size() - 1) {
+            if (pointerCount != 1U) {
+                ALOGE("Last event should only have 1 pointer");
+                return std::nullopt;
+            }
+            action = AMOTION_EVENT_ACTION_UP;
+        } else {
+            const MotionEventEntry& previousEntry = motions[i - 1];
+            const MotionEventEntry& nextEntry = motions[i + 1];
+
+            std::optional<uint32_t> taction =
+                    resolveAction(previousEntry.positions, entry.positions, nextEntry.positions);
+            if (taction == std::nullopt) {
+                return std::nullopt;
+            }
+            action = taction.value();
+        }
+
+        PointerCoords coords[pointerCount];
+        PointerProperties properties[pointerCount];
+        uint32_t pointerIndex = 0;
+        while (!pointers.isEmpty()) {
+            uint32_t pointerId = pointers.clearFirstMarkedBit();
+
+            coords[pointerIndex].clear();
+            // We are treating column positions as pointerId
+            if (!entry.positions[pointerId].isValid()) {
+                ALOGE("The entry at pointerId must be valid");
+                return std::nullopt;
+            }
+            coords[pointerIndex].setAxisValue(AMOTION_EVENT_AXIS_X, entry.positions[pointerId].x);
+            coords[pointerIndex].setAxisValue(AMOTION_EVENT_AXIS_Y, entry.positions[pointerId].y);
+
+            properties[pointerIndex].id = pointerId;
+            properties[pointerIndex].toolType = AMOTION_EVENT_TOOL_TYPE_FINGER;
+            pointerIndex++;
+        }
+        if (pointerIndex != pointerCount) {
+            ALOGE("pointerIndex and pointerCount does not match");
+            return std::nullopt;
+        }
+        MotionEvent event;
+        event.initialize(0 /*deviceId*/, AINPUT_SOURCE_TOUCHSCREEN, DISPLAY_ID, action,
+                         0 /*actionButton*/, 0 /*flags*/, AMOTION_EVENT_EDGE_FLAG_NONE, AMETA_NONE,
+                         0 /*buttonState*/, MotionClassification::NONE, 0 /*xOffset*/,
+                         0 /*yOffset*/, 0 /*xPrecision*/, 0 /*yPrecision*/, 0 /*downTime*/,
+                         entry.eventTime.count(), pointerCount, properties, coords);
+
+        events.push_back(event);
+    }
+
+    return events;
+}
+} // namespace android
---
+++ libs/input/tests/fuzzers/KeyCharacterMap_fuzzer.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fuzzer/FuzzedDataProvider.h>
+#include <input/KeyCharacterMap.h>
+
+#include <filesystem>
+#include <optional>
+#include <string>
+namespace fs = std::filesystem;
+
+static const std::string FILENAME_PREFIX = "/keycharactermapXXXXXXXXX";
+
+android::KeyCharacterMap::Format getFormat(uint8_t option) {
+    switch (option) {
+        case 0: {
+            return android::KeyCharacterMap::FORMAT_BASE;
+        }
+        case 1: {
+            return android::KeyCharacterMap::FORMAT_OVERLAY;
+        }
+        default: {
+            return android::KeyCharacterMap::FORMAT_ANY;
+        }
+    }
+}
+
+std::optional<std::string> generateRandomKeyCharacterMap(const uint8_t* data, size_t size) {
+    // Suitable path of directory for creation of temp files in the filesystem
+    std::string tempFileDir = fs::temp_directory_path();
+    std::string tempFilePath = tempFileDir + FILENAME_PREFIX;
+    size_t tempFilePathLength = tempFilePath.length();
+
+    char filePath[tempFilePathLength];
+
+    strncpy(filePath, tempFilePath.c_str(),
+            tempFilePathLength); // Copying into filePath, mkstemp requires non-const char array
+
+    int fd = mkstemp(filePath);
+
+    if (fd < 0) {
+        return std::nullopt;
+    }
+
+    write(fd, data, size);
+    close(fd);
+
+    return std::string(filePath);
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    FuzzedDataProvider fdp(data, size);
+
+    // 3 different formats available.
+    android::KeyCharacterMap::Format format = getFormat(fdp.ConsumeIntegralInRange<uint8_t>(0, 2));
+
+    std::optional<std::string> filePath = generateRandomKeyCharacterMap(data, size);
+
+    // if File Creation fails
+    if (filePath == std::nullopt) {
+        return 0;
+    }
+
+    android::sp<android::KeyCharacterMap> m;
+    android::KeyCharacterMap::load(filePath.value(), format, &m);
+
+    remove(filePath.value().c_str());
+
+    return 0;
+}
