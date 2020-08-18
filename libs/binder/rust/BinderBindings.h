9416a8d0cd8116cc58297431731fa20ec8024c5d
Janis Danisevskis <jdanis@google.com>
2020-08-18 08:35:38 -0700

    Expose binder security context to rust binder services
    This patch exposes AIBinder_setRequestingSid and AIBinder_getCallingSid
    to binder services written in Rust.
    
    Bug: 165070170
    Test: Not yet
    Change-Id: Ic7bf8a19cbc02eaf0e7f3a56d8b5cf6f4a75dfd0

--- libs/binder/rust/BinderBindings.h
+++ libs/binder/rust/BinderBindings.h
@@ -21,6 +21,33 @@
 #include <android/binder_shell.h>
 #include <android/binder_status.h>
 
+__BEGIN_DECLS
+
+/**
+ * Makes calls to AIBinder_getCallingSid work if the kernel supports it. This
+ * must be called on a local binder server before it is sent out to any other
+ * process. If this is a remote binder, it will abort. If the kernel doesn't
+ * support this feature, you'll always get null from AIBinder_getCallingSid.
+ *
+ * \param binder local server binder to request security contexts on
+ */
+void AIBinder_setRequestingSid(AIBinder* binder, bool requestingSid) __INTRODUCED_IN(31);
+
+/**
+ * Returns the selinux context of the callee.
+ *
+ * In order for this to work, the following conditions must be met:
+ * - The kernel must be new enough to support this feature.
+ * - The server must have called AIBinder_setRequestingSid.
+ * - The callee must be a remote process.
+ *
+ * \return security context or null if unavailable. The lifetime of this context
+ * is the lifetime of the transaction.
+ */
+__attribute__((warn_unused_result)) const char* AIBinder_getCallingSid() __INTRODUCED_IN(31);
+
+__END_DECLS
+
 namespace android {
 
 namespace c_interface {
