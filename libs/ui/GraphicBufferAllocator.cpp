5b45c3b9ae5229ddc8075f891ee4d4e896118789
Steven Moreland <smoreland@google.com>
2020-09-26 00:11:12 +0000

    libui: Avoid heap initialization for buffer alloc.
    When heap initialization is enabled, all allocations are zeroed. However,
    for big allocations, this extra work isn't as cache-friendly. Since
    hw allocations are sometimes simply memory on the heap for the CPU,
    disabling heap initialization in order to preserve performance.
    
    Bug: 131355925
    Test: hwuimacro hwbuffer related tests no longer regress noticeably in
        performance.
    Change-Id: I6efc9b361f3728cdbe480970f5f9e604bd0e799e

--- libs/ui/GraphicBufferAllocator.cpp
+++ libs/ui/GraphicBufferAllocator.cpp
@@ -137,8 +137,16 @@ status_t GraphicBufferAllocator::allocateHelper(uint32_t width, uint32_t height,
     // TODO(b/72323293, b/72703005): Remove these invalid bits from callers
     usage &= ~static_cast<uint64_t>((1 << 10) | (1 << 13));
 
-    status_t error = mAllocator->allocate(requestorName, width, height, format, layerCount, usage,
-                                          1, stride, handle, importBuffer);
+    status_t error;
+    {
+        // TODO(b/169449588) restore original behavior instead of always setting
+        //     flag back to zero
+        // TODO(b/158870657) assert error once all devices use scudo
+        (void)mallopt(M_THREAD_DISABLE_MEM_INIT, 1);
+        error = mAllocator->allocate(requestorName, width, height, format, layerCount, usage, 1, stride, handle, importBuffer);
+        (void)mallopt(M_THREAD_DISABLE_MEM_INIT, 0);
+    }
+
     if (error != NO_ERROR) {
         ALOGE("Failed to allocate (%u x %u) layerCount %u format %d "
               "usage %" PRIx64 ": %d",
