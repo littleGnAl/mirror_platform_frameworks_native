829bd8631e4624d749aaa2ad1dc7f448f60aa5de
Dieter Hsu <dieterhsu@google.com>
2020-09-29 15:23:33 +0800

    Streaming bugreport zip file via stdout 2/2
    - Stream compressed content via the socket from dumpstate
    - Remove do_zip_file, do_add_data vars in dumpstate
    - '-d' and '-z' options are now no-op
    
    Bug: 162910469
    Test: adb bugreport --stream > test.zip; 7z t test.zip
    Test: adb shell bugreportz -s > test.zip; 7z t test.zip
    Test: atest dumpstate_test
    Test: atest dumpstate_smoke_test
    
    Change-Id: Icf79bbc42a5616e85625bd604e543fbf973911da

--- cmds/dumpstate/dumpstate.h
+++ cmds/dumpstate/dumpstate.h
@@ -388,11 +388,9 @@ class Dumpstate {
      * Structure to hold options that determine the behavior of dumpstate.
      */
     struct DumpOptions {
-        bool do_add_date = false;
-        bool do_zip_file = false;
         bool do_vibrate = true;
-        // Writes bugreport content to a socket; only flatfile format is supported.
-        bool use_socket = false;
+        // Writes bugreport content to a socket.
+        bool do_stream_zip_file = false;
         bool use_control_socket = false;
         bool do_screenshot = false;
         bool is_screenshot_copied = false;
@@ -434,13 +432,6 @@ class Dumpstate {
         /* Returns true if the options set so far are consistent. */
         bool ValidateOptions() const;
 
-        /* Returns if options specified require writing bugreport to a file */
-        bool OutputToFile() const {
-            // If we are not writing to socket, we will write to a file. If bugreport_fd is
-            // specified, it is preferred. If not bugreport is written to /bugreports.
-            return !use_socket;
-        }
-
         /* Returns if options specified require writing to custom file location */
         bool OutputToCustomFile() {
             // Custom location is only honored in limited mode.
@@ -478,8 +469,8 @@ class Dumpstate {
     // `bugreport-BUILD_ID`.
     std::string base_name_;
 
-    // Name is the suffix part of the bugreport files - it's typically the date (when invoked with
-    // `-d`), but it could be changed by the user..
+    // Name is the suffix part of the bugreport files - it's typically the date,
+    // but it could be changed by the user..
     std::string name_;
 
     std::string bugreport_internal_dir_ = DUMPSTATE_DIRECTORY;
