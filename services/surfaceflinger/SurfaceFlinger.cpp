aeec5f5e72f1cdc55b064286fe26fcf0c397736b
Sergio Andres Rodriguez Orama <sorama@google.com>
2020-09-30 05:09:26 +0000

    Android codelab change Test: manual atest
    Change-Id: I13efdaeefbc189e5a5947d9387c8fa2d8120e54d

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2100,6 +2100,8 @@ void SurfaceFlinger::onMessageRefresh() {
 
     postFrame();
     postComposition();
+    mClientColorMatrix = mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                              vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
 
     const bool prevFrameHadDeviceComposition = mHadDeviceComposition;
 
