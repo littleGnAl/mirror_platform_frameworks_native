d8d3b67d09b3184cba0beadf1b15a8c60154dc1a
Dieter Hsu <dieterhsu@google.com>
2020-10-31 14:51:34 +0800

    bugreportz command request reports via binder call
    Requesting bugreport using bugreportz now will call bugreporting API via
    binder service. Output behavior have changed a little since the messages
    are written from listener callbacks of bugreportd service.
    
    Bug: 162910469
    Test: atest bugreportz_test
    Test: manually delete /data/user_de/0/com.android.shell/files/bugreportz_test
          and run adb bugreport
    Change-Id: Ib29526501bb104dbad2d75ef98d1f8363fe19dc4

---
+++ cmds/bugreportz/DumpstateClient.h
@@ -0,0 +1,77 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef FRAMEWORK_NATIVE_CMD_DUMPSTATE_CLIENT_H_
+#define FRAMEWORK_NATIVE_CMD_DUMPSTATE_CLIENT_H_
+
+#include <android-base/properties.h>
+#include <android/os/BnDumpstateListener.h>
+#include <android/os/IDumpstate.h>
+#include <android/os/IDumpstateListener.h>
+
+#include <future>
+
+namespace android::os::bugreportz {
+
+class DumpstateClient : public android::os::BnDumpstateListener,
+                        public android::IBinder::DeathRecipient {
+
+  public:
+    explicit DumpstateClient(int fd) noexcept;
+    // For testing
+    DumpstateClient(int fd, sp<android::os::IDumpstate> ds) noexcept;
+
+    binder::Status StartBugreport(bool show_progress);
+
+    void WaitForBugreport();
+
+    // Implements BnDumpstateListener.
+    binder::Status onProgress(int32_t /*progress*/) override;
+    binder::Status onError(int32_t /*error_code*/) override;
+    binder::Status onFinished() override;
+    binder::Status onScreenshotTaken(bool /*success*/) override;
+    binder::Status onUiIntensiveBugreportDumpsFinished(
+            const String16& /*calling_package*/) override;
+
+    // Implements IBinder::DeathRecipient.
+    void binderDied(const wp<IBinder>& /*who*/) override;
+
+  private:
+    // Write the progress to fd when listener callbacks.
+    void WriteProgress(const std::string& line) const;
+
+    std::string bugreport_path_;
+
+    // The Dumpstate service proxy to invoke binder call.
+    sp<IDumpstate> ds_;
+
+    // As a listener, client writes updates to the file descriptor.
+    int fd_;
+
+    // When not invoked with the -p option to show progress, it must skip BEGIN and PROGRESS lines
+    // otherwise it will break adb (which is expecting either OK or FAIL).
+    bool show_progress_ = true;
+
+    // A flag indicate that BEGIN message has sent.
+    bool begin_msg_sent_ = false;
+
+    // Wait until the value set in onFinish/onError of the listener before termination.
+    std::promise<void> promise_finish_;
+
+    std::mutex lock_;
+};
+
+} // namespace android::os::bugreportz
+
+#endif  // FRAMEWORK_NATIVE_CMD_DUMPSTATE_CLIENT_H_
--- cmds/bugreportz/bugreportz_test.cpp
+++ cmds/bugreportz/bugreportz_test.cpp
@@ -14,22 +14,54 @@
  * limitations under the License.
  */
 
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
+#include "bugreportz.h"
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <gmock/gmock.h>
+#include <gmock/gmock-matchers.h>
+#include <gtest/gtest.h>
 
 #include <string>
 
-#include "bugreportz.h"
-
+using ::testing::_;
+using ::testing::DoAll;
+using ::testing::Invoke;
+using ::testing::Return;
 using ::testing::StrEq;
+using ::testing::Test;
 using ::testing::internal::CaptureStdout;
 using ::testing::internal::GetCapturedStdout;
+using ::android::base::unique_fd;
+using ::android::binder::Status;
+using ::android::os::IDumpstateListener;
+using ::android::os::bugreportz::DumpstateClient;
 
-class BugreportzTest : public ::testing::Test {
+using namespace ::android;
+
+class MockBinder : public BBinder {
+  public:
+    MOCK_METHOD(status_t, linkToDeath,
+                (const sp<DeathRecipient>& recipient, void* cookie, uint32_t flags), (override));
+    MOCK_METHOD(status_t, unlinkToDeath,
+                (const wp<DeathRecipient>& recipient, void* cookie, uint32_t flags,
+                 wp<DeathRecipient>* outRecipient),
+                (override));
+    MOCK_METHOD(status_t, pingBinder, (), (override));
+};
+
+class MockDumpstate : public android::os::IDumpstate {
+  public:
+    MOCK_METHOD(Status, startBugreport,
+                ((int32_t callingUid), (const std::string& callingPackage),
+                (unique_fd bugreportFd), (unique_fd screenshotFd),
+                (int32_t bugreportMode),
+                (const sp<android::os::IDumpstateListener>& listener),
+                (bool isScreenshotRequested)),
+                (override));
+    MOCK_METHOD(Status, cancelBugreport, (), (override));
+    MOCK_METHOD(IBinder*, onAsBinder, (), (override));
+};
+
+class BugreportzTest : public Test {
   public:
     // Creates the pipe used to communicate with bugreportz()
     void SetUp() {
@@ -70,12 +102,12 @@ class BugreportzTest : public ::testing::Test {
     // Tests must call WriteToSocket() to set what's written prior to calling it, since the writing
     // end of the pipe will be closed before calling bugreportz() (otherwise that function would
     // hang).
-    void Bugreportz(bool show_progress) {
+    void Bugreportz(bool show_progress, sp<DumpstateClient> client) {
         close(write_fd_);
         write_fd_ = -1;
 
         CaptureStdout();
-        int status = bugreportz(read_fd_, show_progress);
+        int status = bugreportz(show_progress, client);
 
         close(read_fd_);
         read_fd_ = -1;
@@ -84,23 +116,49 @@ class BugreportzTest : public ::testing::Test {
         ASSERT_EQ(0, status) << "bugrepotz() call failed (stdout: " << stdout_ << ")";
     }
 
+    void BugreportzStream() {
+        close(write_fd_);
+        write_fd_ = -1;
+
+        CaptureStdout();
+        int status = bugreportz_stream(read_fd_);
+
+        close(read_fd_);
+        read_fd_ = -1;
+        stdout_ = GetCapturedStdout();
+
+        ASSERT_EQ(0, status) << "bugrepotz_stream() call failed (stdout: " << stdout_ << ")";
+    }
+
+    sp<DumpstateClient> CreateClient() {
+        binder_ = sp<MockBinder>::make();
+        ds_ = sp<MockDumpstate>::make();
+        client_ = sp<DumpstateClient>::make(write_fd_, ds_);
+        ON_CALL(*ds_, onAsBinder).WillByDefault(Return(binder_.get()));
+        return client_;
+    }
+
+  protected:
+    sp<MockBinder> binder_ = nullptr;
+    sp<MockDumpstate> ds_ = nullptr;
+    sp<DumpstateClient> client_ = nullptr;
+
   private:
     int read_fd_;
     int write_fd_;
     std::string stdout_;
 };
 
+// TODO: fix and verify BugreportzTest with mock client
+
 // Tests 'bugreportz', without any argument - it will ignore progress lines.
 TEST_F(BugreportzTest, NoArgument) {
-    WriteToSocket("BEGIN:THE IGNORED PATH WARS HAS!\n");  // Should be ommited.
-    WriteToSocket("What happens on 'dumpstate',");
-    WriteToSocket("stays on 'bugreportz'.\n");
-    WriteToSocket("PROGRESS:Y U NO OMITTED?\n");  // Should be ommited.
-    WriteToSocket("But ");
-    WriteToSocket("PROGRESS IN THE MIDDLE");  // Ok - not starting a line.
-    WriteToSocket(" is accepted\n");
+    client_ = CreateClient();
+    client_->onProgress(0);
+    client_->onProgress(42);
+    client_->onFinished();
 
-    Bugreportz(false);
+    Bugreportz(false, client_);
 
     AssertStdoutEquals(
         "What happens on 'dumpstate',stays on 'bugreportz'.\n"
@@ -109,15 +167,13 @@ TEST_F(BugreportzTest, NoArgument) {
 
 // Tests 'bugreportz -p' - it will just echo dumpstate's output to stdout
 TEST_F(BugreportzTest, WithProgress) {
-    WriteToSocket("BEGIN:I AM YOUR PATH\n");
-    WriteToSocket("What happens on 'dumpstate',");
-    WriteToSocket("stays on 'bugreportz'.\n");
-    WriteToSocket("PROGRESS:IS INEVITABLE\n");
-    WriteToSocket("PROG");
-    WriteToSocket("RESS:IS NOT AUTOMATIC\n");
-    WriteToSocket("Newline is optional");
+    client_ = CreateClient();
+    client_->onProgress(0);
+    client_->onProgress(0);
+    client_->onProgress(50);
+    client_->onFinished();
 
-    Bugreportz(true);
+    Bugreportz(true, client_);
 
     AssertStdoutEquals(
         "BEGIN:I AM YOUR PATH\n"
@@ -125,4 +181,48 @@ TEST_F(BugreportzTest, WithProgress) {
         "PROGRESS:IS INEVITABLE\n"
         "PROGRESS:IS NOT AUTOMATIC\n"
         "Newline is optional");
+}
+
+// Tests 'bugreportz -s' - just echo data
+TEST_F(BugreportzTest, WithStream) {
+    char emptyZip[] = {0x50, 0x4B, 0x05, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    std::string data(emptyZip);
+    WriteToSocket(data);
+
+    BugreportzStream();
+
+    AssertStdoutEquals(data);
+}
+
+class DumpstateClientTest : public Test {
+  public:
+    void SetUp() {
+        binder_ = sp<MockBinder>::make();
+        ds_ = sp<MockDumpstate>::make();
+        client_ = sp<DumpstateClient>::make(dup(STDOUT_FILENO), ds_);
+
+        ON_CALL(*ds_, onAsBinder).WillByDefault(Return(binder_.get()));
+    }
+  protected:
+    sp<MockBinder> binder_ = nullptr;
+    sp<MockDumpstate> ds_ = nullptr;
+    sp<DumpstateClient> client_ = nullptr;
+};
+
+TEST_F(DumpstateClientTest, StartBugreportHappy) {
+    EXPECT_CALL(*ds_, startBugreport(_,_,_,_,_,_,_)).WillRepeatedly(
+            DoAll(Invoke([&](){client_->onFinished();}),
+                  Return(Status::ok())));
+
+    ASSERT_EQ(bugreportz(/*show_progress=*/true, client_), EXIT_SUCCESS);
+}
+
+TEST_F(DumpstateClientTest, AnotherReportInProgress) {
+    const int err_code = IDumpstateListener::BUGREPORT_ERROR_ANOTHER_REPORT_IN_PROGRESS;
+    EXPECT_CALL(*ds_, startBugreport(_,_,_,_,_,_,_)).WillRepeatedly(
+            DoAll(Invoke([&](){client_->onError(err_code);}),
+                  Return(Status::fromExceptionCode(Status::EX_SERVICE_SPECIFIC))));
+
+    ASSERT_EQ(bugreportz(/*show_progress=*/true, client_), EXIT_FAILURE);
 }
