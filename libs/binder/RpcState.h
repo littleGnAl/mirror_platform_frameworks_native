fa8a36c272370ca910a8d4e8f1ff0ed97ed9b7b5
Steven Moreland <smoreland@google.com>
2020-11-11 02:14:45 +0000

    libbinder: support calls over sockets
    Upload is for Gerrit save only.
    
    FIXME: search all source code for FIXMEs - there are many
    FIXME: code formatting (in libbinder even?)
    FIXME: the next things to do:
     - fix RpcAddress lifetime, and clear mBinderToAddres map
     - support multiple simultaneous connections to the same server
     - linking to death/refcounts for process deaths
     - cleanup/optimize transaction code
     - add fuzzer
     - address HMAC
    
    Bug: 167966510
    Test: binderRpcTest
    
    Change-Id: I276c6e312f584b57f4e7a14389ea4a1d63cfa2f4

---
+++ libs/binder/RpcState.h
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <android-base/unique_fd.h>
+#include <binder/IBinder.h>
+#include <binder/Parcel.h>
+#include <binder/RpcConnection.h>
+
+#include <map>
+
+namespace android {
+
+struct RpcWireHeader;
+
+// FIXME: docs, justify global
+// FIXME: make a lot of these functions private
+// FIXME: document what this replyConnection thing is
+class RpcState {
+public:
+    // FIXME: combine with process state or build similarly?
+    static RpcState& self();
+
+    status_t transact(const base::unique_fd& fd,
+                      const RpcWireAddress* address,
+                      uint32_t code,
+                      const Parcel& data,
+                      const sp<RpcConnection>& replyConnection,
+                      Parcel* reply,
+                      uint32_t flags);
+    status_t waitForReply(const base::unique_fd& fd,
+                          const sp<RpcConnection>& connection,
+                          Parcel* reply);
+    status_t sendDecStrong(const base::unique_fd& fd, const RpcWireAddress* address);
+    status_t getAndExecuteCommand(const base::unique_fd& fd,
+                                  const sp<RpcConnection>& replyConnection);
+
+    /**
+     * FIXME: perhaps allow specifying an address/capability here so that
+     * whoever registers the root object can set the address, limiting who can
+     * see it?
+     */
+    void setRootObject(const sp<IBinder>& binder);
+
+    // FIXME: switch binder to be wp here, since we keep wp
+    // FIXME: document address lifetime, or switch address to be
+    // owned by the binder
+    /**
+     * Should be called on out-going binders. This comes with an explicit incRef
+     * of ownership, which is associated with sending a binder.
+     */
+    const RpcWireAddress* attachBinder(const sp<IBinder>& binder);
+    sp<IBinder> lookupOrCreateProxy(const sp<RpcConnection>& connection, RpcWireAddress&& address);
+
+    // FIXME: this needs to be better
+    // debugging dump of rpc state contents
+    void dump();
+
+private:
+    // FIXME: needing this function is a product of how we read packets
+    status_t processServerCommand(const base::unique_fd& fd,
+                                  const sp<RpcConnection>& replyConnection,
+                                  const RpcWireHeader& command);
+    status_t processTransact(const base::unique_fd& fd,
+                             const sp<RpcConnection>& replyConnection,
+                             const RpcWireHeader& command);
+    status_t processDecRef(const base::unique_fd& fd,
+                           const RpcWireHeader& command);
+
+    struct BinderNode {
+        wp<IBinder> binder;
+        // number of times we've sent this binder out of process
+        // (each time is one implicit strong ref)
+        size_t strong;
+    };
+    // binders known by both sides of a connection
+    // FIXME: synchronization
+    // FIXME: do not make copies of RpcWireAddress, since they will be large
+    //     maybe it's best to keep here, so that sizeof(BpBinder) isn't too
+    //     big
+    // FIXME: unordered hash?
+    // FIXME: clean this up when binders are deleted
+    // FIXME: should separate local vs remote !!!!
+    std::map<RpcWireAddress, BinderNode> mNodeForAddress;
+};
+
+} // namespace android
