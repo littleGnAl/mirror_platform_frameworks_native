003ce0e29de0c10f9c76706d86a85d6be4370917
Steven Moreland <smoreland@google.com>
2020-11-11 02:14:45 +0000

    libbinder: support calls over sockets
    Upload is for Gerrit save only.
    
    FIXME: convert test to gtest, add tests for various cases
    FIXME: search all source code for FIXMEs - there are many
    FIXME: code formatting (in libbinder even?)
    FIXME: the next things to do:
     - support sending binder objects
     - support nested transactions
     - do something with file descriptors
     - handle reference counts
     - cleanup/optimize transaction code
     - add fuzzer
    
    Change-Id: I276c6e312f584b57f4e7a14389ea4a1d63cfa2f4

---
+++ libs/binder/RpcState.cpp
@@ -0,0 +1,211 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "RpcState"
+
+#include <binder/RpcState.h>
+
+#include "RpcCommands.h"
+
+namespace android {
+
+void RpcState::attachBinder(const sp<IBinder>& binder) {
+    RpcAddress address = RpcAddress{(int)mBinders.size()};
+    mBinders[address] = binder;
+}
+
+std::string hexString(const void* bytes, size_t len) {
+    if (bytes == nullptr) return "<null>";
+
+    const uint8_t* bytes8 = static_cast<const uint8_t*>(bytes);
+    char chars[] = "0123456789abcdef";
+    std::string result;
+    result.resize(len * 2);
+
+    for (size_t i = 0; i < len; i++) {
+        result[2 * i] = chars[bytes8[i] >> 4];
+        result[2 * i + 1] = chars[bytes8[i] & 0xf];
+    }
+
+    return result;
+}
+
+// FIXME: write now, we are using this primitive too much, should switch to
+// recvmsg, scatter gather, and also potentially buffering calls here if we are
+// sending too many at once
+
+static inline bool rpcSend(const base::unique_fd& fd, const char* what, void* data, size_t size) {
+    ALOGI("About to send %s: %s", what, hexString(data, size).c_str());  // FIXME: spam
+
+    ssize_t sent = TEMP_FAILURE_RETRY(send(fd.get(), data, size, 0));
+
+    // FIXME: range check size
+    if (sent != (ssize_t) size) {
+        ALOGE("Failed to send %s (sent %zd of %zu bytes), error: %s",
+              what, sent, size, strerror(errno));
+        return false;
+    } else {
+        ALOGI("Send successful");
+    }
+
+    return true;
+}
+static inline bool rpcRec(const base::unique_fd& fd, const char* what, void* data, size_t size) {
+
+    ssize_t recd = TEMP_FAILURE_RETRY(recv(fd.get(), data, size, MSG_WAITALL));
+
+    // FIXME: range check this
+    if (recd != (ssize_t) size) {
+        ALOGE("Failed to read %s (received %zd of %zu bytes), error: %s",
+              what, recd, size, strerror(errno));
+        return false;
+    } else {
+       ALOGI("Received %s: %s", what, hexString(data, size).c_str());  // FIXME: spam
+    }
+
+    return true;
+}
+
+status_t RpcState::transact(const base::unique_fd& fd, const RpcAddress* address, uint32_t code, const Parcel& data, Parcel* reply, uint32_t flags) {
+    // FIXME: handle null address?
+    // FIXME: send w/o such a mess, w/o copies, scatter-gather
+    // FIXME: check that parcel does not have any objects
+
+    RpcTransaction transaction {
+        .address = address->address,
+        .code = code,
+        .flags = flags,  // FIXME prune
+    };
+
+    std::vector<uint8_t> transactionData(sizeof(RpcTransaction) + data.dataSize());
+    memcpy(transactionData.data() + 0, &transaction, sizeof(RpcTransaction));
+    memcpy(transactionData.data() + sizeof(RpcTransaction), data.data(), data.dataSize());
+
+    RpcCommand command {
+        .command = RPC_COMMAND_TRANSACT,
+        .bodySize = (uint32_t) transactionData.size(), // FIXME: range check
+    };
+
+    if (!rpcSend(fd, "transact header", &command, sizeof(command))) {
+        return UNKNOWN_ERROR;
+    }
+    if (!rpcSend(fd, "command body", transactionData.data(), transactionData.size())) {
+        return UNKNOWN_ERROR;
+    }
+
+    // FIXME: handle fake reply parcelable, like libbinder does, or fix callers?
+    LOG_ALWAYS_FATAL_IF(reply == nullptr, "NO!");
+
+    if (flags & IBinder::FLAG_ONEWAY) {
+        return OK;  // do not wait for result
+    }
+
+    return waitForReply(fd, reply);
+}
+
+status_t RpcState::getAndExecuteCommand(const base::unique_fd& fd) {
+    ALOGE("PROCESSING COMMAND in %d", getpid());
+
+    // FIXME: what's the best way to read from a socket?
+    // FIXME: switch to using Parcel to parse the data from the kernel, like
+    // IPCThreadState does?
+    // FIXME: clean this all up....
+
+    RpcCommand command;
+    if (!rpcRec(fd, "command header", &command, sizeof(command))) {
+        return NOT_ENOUGH_DATA;
+    }
+
+    // FIXME: error handling
+    LOG_ALWAYS_FATAL_IF(command.command != RPC_COMMAND_TRANSACT);
+
+    // FIXME: avoid allocating extra size here?
+    std::vector<uint8_t> transactionData(command.bodySize);
+    if (!rpcRec(fd, "transaction body", transactionData.data(), transactionData.size())) {
+        return NOT_ENOUGH_DATA;
+    }
+
+    // FIXME: check bodySize < sizeof(RpcTransaction)
+    RpcTransaction* transaction = reinterpret_cast<RpcTransaction*>(transactionData.data());
+    // FIXME: synchronization
+    // FIXME: different lookup method?
+    auto it = mBinders.find(RpcAddress{transaction->address});
+    if (it == mBinders.end()) {
+        ALOGE("Unknown binder address %d", transaction->address);
+        return UNKNOWN_ERROR;
+    }
+
+    Parcel data;
+    data.setData(transaction->data, command.bodySize - offsetof(RpcTransaction, data));
+
+    Parcel reply;
+    status_t status = it->second->transact(transaction->code, data, &reply, transaction->flags);
+
+    if (transaction->flags & IBinder::FLAG_ONEWAY) {
+        if (status != OK) {
+            ALOGW("Oneway call failed with error: %d", status);
+        }
+        return OK;
+    }
+
+    RpcReply rpcReply {
+        .status = status,
+    };
+
+    std::vector<uint8_t> replyData(sizeof(RpcReply) + data.dataSize());
+    memcpy(replyData.data() + 0, &rpcReply, sizeof(RpcReply));
+    memcpy(replyData.data() + sizeof(RpcReply), reply.data(), reply.dataSize());
+
+    RpcCommand cmdReply {
+        .command = RPC_COMMAND_REPLY,
+        .bodySize = (uint32_t) replyData.size(), // FIXME: range check
+    };
+
+    if (!rpcSend(fd, "reply header", &cmdReply, sizeof(RpcCommand))) {
+        return UNKNOWN_ERROR;
+    }
+    if (!rpcSend(fd, "reply body", replyData.data(), replyData.size())) {
+        return UNKNOWN_ERROR;
+    }
+    return OK;
+}
+
+status_t RpcState::waitForReply(const base::unique_fd& fd, Parcel* reply) {
+    RpcCommand command;
+    if (!rpcRec(fd, "command header", &command, sizeof(command))) {
+        return NOT_ENOUGH_DATA;
+    }
+
+    // FIXME: support nested commands
+    LOG_ALWAYS_FATAL_IF(command.command != RPC_COMMAND_REPLY);
+
+    std::vector<uint8_t> replyData(command.bodySize);
+    if (!rpcRec(fd, "reply body", replyData.data(), replyData.size())) {
+        return NOT_ENOUGH_DATA;
+    }
+    // FIXME: check bodySize < sizeof(RpcReply)
+    RpcReply* rpcReply = reinterpret_cast<RpcReply*>(replyData.data());
+
+    if (rpcReply->status != OK) return rpcReply->status;
+
+    // FIXME: use ipcSetDataReference, to avoid copy? or resize beforehand
+    // instead of reading into std::vector, depends on how we optimize this.
+    reply->setData(rpcReply->data, command.bodySize - offsetof(RpcReply, data));
+
+    return OK;
+}
+
+} // namespace android
---
+++ libs/binder/tests/binderRpcTest.cpp
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <unistd.h>
+#include <sys/prctl.h>
+
+#include <cstdlib>
+#include <iostream>
+
+#include <BnBinderRpcTest.h>
+#include <android-base/logging.h>
+#include <binder/Binder.h>
+#include <binder/BpBinder.h>
+#include <binder/RpcConnection.h>
+#include <binder/RpcServer.h>
+#include <binder/Stability.h>
+#include <gtest/gtest.h>
+
+using android::RpcAddress;
+using android::RpcConnection;
+using android::RpcServer;
+using android::binder::Status;
+using android::internal::Stability;
+using android::sp;
+using android::BpBinder;
+using android::IBinder;
+using android::interface_cast;
+using android::OK;
+
+// FIXME: only test socket-specific things, other things should be tested in
+// aidl_integration_test
+
+const char* kSock = "/dev/some_socket";
+
+#define EXPECT_OK(status) \
+  do { \
+    Status stat = (status); \
+    EXPECT_TRUE(stat.isOk()) << stat; \
+  } while(false)
+
+class MyBinderRpcTest : public BnBinderRpcTest {
+public:
+    Status sendString(const std::string& str) {
+        std::cout << "Child received string: " << str << std::endl;
+        return Status::ok();
+    }
+    Status doubleString(const std::string& str, std::string* strstr) {
+        std::cout << "Child received string: " << str << std::endl;
+        *strstr = str + str;
+        return Status::ok();
+    }
+};
+
+static sp<IBinder> getBinder() {
+    // FIXME: this connection should be automatically created in the background
+    // by whatever method we get ahold of a server. Currently, we just magically
+    // know the address (and the top-level binder is always registered with the
+    // first identifier, 0).
+    static sp<RpcConnection> connection = RpcConnection::connect(kSock);
+    if (connection == nullptr) {
+        std::cout << "NULL CONNECTION!!!" << std::endl;
+        abort();
+    }
+    // FIXME: very easy to forget to give ownership of this to BpBinder. We
+    // should limit access to BpBinder::create.
+    static const RpcAddress address {0}; // FIXME: magic
+    sp<IBinder> binder = BpBinder::create(connection, &address);
+
+    // FIXME: would normally be done when reading parcel from another process,
+    // but here we "magically" create a connection, so we don't get this
+    // information from a transaction.
+    Stability::markCompilationUnit(binder.get());
+
+    return binder;
+}
+static sp<IBinderRpcTest> getInterface() {
+    return interface_cast<IBinderRpcTest>(getBinder());
+}
+
+TEST(BinderRpc, Ping) {
+    sp<IBinder> binder = getBinder();
+    EXPECT_EQ(OK, binder->pingBinder());
+}
+
+TEST(BinderRpc, SendSomethingOneway) {
+    EXPECT_OK(getInterface()->sendString("asdf"));
+}
+
+TEST(BinderRpc, SendAndGetResultBack) {
+    std::string doubled;
+    EXPECT_OK(getInterface()->doubleString("cool ", &doubled));
+    EXPECT_EQ("cool cool ", doubled);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    android::base::InitLogging(argv, android::base::StderrLogger, android::base::DefaultAborter);
+
+    // - FIXME - stop using unix domain sockets, or implement some other
+    // mechanism to make sure it's not in use
+    unlink(kSock);
+
+    pid_t childPid = fork();
+    if (childPid == 0) {
+        prctl(PR_SET_PDEATHSIG, SIGHUP);  // technically racey
+        sp<RpcServer> server = RpcServer::makeUnixServer(kSock);
+        server->attachServedBinder(new MyBinderRpcTest());
+        server->join();
+        return EXIT_FAILURE;
+    }
+
+    usleep(100000); // give server time to create connection
+
+    std::cout << "This pid: " << getpid() << std::endl;
+    std::cout << "Child pid: " << childPid << std::endl;
+
+    return RUN_ALL_TESTS();
+}
+
