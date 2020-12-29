31a1a745907feaa7d03333d5a4bb94f428022623
Tianhua Sun <tianhua.sun@amlogic.com>
2020-12-29 07:20:55 -0500

    SurfaceFlinger: fix no small window displayed in PIP mode of DTVKIT [1/1]
    Bug:146758432
    PD#SWPL-39338
    
    Problem:
    Dtvkit, no small window displayed in pip mode
    
    Solution:
    porting from I82d81ded
    For surfaceview of tv sideband, there is no activeBuffer in bufferqueue.
    ClientCompostion layer if above it need plug holes for this case.
    
    Verify:
    ohm
    
    Change-Id: Id92c3856eb8eb5a14813336f723d30b7a17514b1
    Signed-off-by: Tianhua Sun <tianhua.sun@amlogic.com>

--- services/surfaceflinger/BufferLayer.cpp
+++ services/surfaceflinger/BufferLayer.cpp
@@ -176,7 +176,10 @@ std::optional<compositionengine::LayerFE::LayerSettings> BufferLayer::prepareCli
         if (!holes.isEmpty()) {
             targetSettings.clearRegion.orSelf(holes);
         }
-        return std::nullopt;
+
+        // For surfaceview of tv sideband, there is no activeBuffer in bufferqueue,
+        // we need return LayerSettings.
+        return result;
     }
     bool blackOutLayer = (isProtected() && !targetSettings.supportsProtectedContent) ||
             (isSecure() && !targetSettings.isSecure);
