906451d3185184976642e6db570f7d741bd5fa2a
Adhiti Sharma <adhiti.sharma@borqs.com>
2021-05-04 19:51:16 +0530

    Critical communication keys support (2/2)
    Android device vendors such as Kyocera,Ecom, Sonim, MSI, produces devices
    with dedicated Push-To-Talk (PTT) and SOS keys for critical
    communication needs. That keys bring Push-to-Talk over Cellular (PoC)
    application to the front, wake up the device's screen, and initiate PTT
    or Emergency critical call.Ecom has 2 extra dedicated keys like RSM_EXT_A
    and RSM_EXT_B for RSM headset support.
    
    Device vendors might use new key codes 289, 290, 291 and 292 for
    PTT, SOS, RSM_EXT_A and RSM_EXT_B dedicated keys.
    
    Bug: 185883106
    Test: manual - adb shell input keyevent 289
    Test: manual - adb shell input keyevent 290
    Test: manual - adb shell input keyevent 291
    Test: manual - adb shell input keyevent 292
    Test: make
    
    Change-Id: I6390ef9a5cbb869db19ad42947e23c61b6ad7335

--- include/android/keycodes.h
+++ include/android/keycodes.h
@@ -776,7 +776,15 @@ enum {
     AKEYCODE_THUMBS_DOWN = 287,
     /** Used to switch current account that is consuming content.
      * May be consumed by system to switch current viewer profile. */
-    AKEYCODE_PROFILE_SWITCH = 288
+    AKEYCODE_PROFILE_SWITCH = 288,
+    /** SOS */
+    AKEYCODE_SOS = 289,
+    /** PTT */
+    AKEYCODE_PTT = 290,
+    /** RSM_EXT_A */
+    AKEYCODE_RSM_EXT_A = 291,
+    /** RSM_EXT_B */
+    AKEYCODE_RSM_EXT_B = 292
 
     // NOTE: If you add a new keycode here you must also add it to several other files.
     //       Refer to frameworks/base/core/java/android/view/KeyEvent.java for the full list.
