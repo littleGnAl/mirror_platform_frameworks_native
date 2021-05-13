3992cbb2694190a46e2e49f30d582af2cc39941d
Yifan Hong <elsk@google.com>
2021-05-13 16:07:47 -0700

    Add RpcServer::shutdown.
    The function terminates any existing execution of join().
    
    After this CL, join() is only allowed to be called in one thread.
    
    Test: pass
    Change-Id: I5f1abbb39ee42a8f94b7394a702a152701537e7e

--- libs/binder/include/binder/RpcServer.h
+++ libs/binder/include/binder/RpcServer.h
@@ -119,9 +119,17 @@ public:
     /**
      * You must have at least one client session before calling this.
      *
-     * TODO(b/185167543): way to shut down?
+     * If a client needs to actively terminate join,  call shutdown() in a separate thread.
+     *
+     * At any given point, there can only be one thread calling join().
      */
     void join();
+
+    /**
+     * Shut down any existing join(). Return true if successfully shut down, false otherwise
+     * (e.g. no join() is running).
+     */
+    [[nodiscard]] bool shutdown();
 
     /**
      * Accept one connection on this server. You must have at least one client
@@ -142,11 +150,29 @@ public:
     void onSessionTerminating(const sp<RpcSession>& session);
 
 private:
+    /** This is not a pipe. */
+    struct FdTrigger {
+        static std::unique_ptr<FdTrigger> make();
+        /**
+         * poll() on this fd for POLLHUP to get notification when another thread calls shutdown().
+         */
+        base::borrowed_fd readFd() const { return mRead; }
+        /**
+         * Close the write end of the pipe so that the read end receives POLLHUP.
+         */
+        void shutdown();
+
+    private:
+        base::unique_fd mWrite;
+        base::unique_fd mRead;
+    };
+
     friend sp<RpcServer>;
     RpcServer();
 
     void establishConnection(sp<RpcServer>&& session, base::unique_fd clientFd);
     bool setupSocketServer(const RpcSocketAddress& address);
+    [[nodiscard]] bool acceptOneNoCheck();
 
     bool mAgreedExperimental = false;
     bool mStarted = false; // TODO(b/185167543): support dynamically added clients
@@ -159,6 +185,7 @@ private:
     wp<IBinder> mRootObjectWeak;
     std::map<int32_t, sp<RpcSession>> mSessions;
     int32_t mSessionIdCounter = 0;
+    std::unique_ptr<FdTrigger> mJoinShutdownTrigger;
 };
 
 } // namespace android
