9b8915f2a3a2118be2265c744f63e64a741f27ca
Yifan Hong <elsk@google.com>
2021-06-14 13:24:19 -0700

    Utils: Add execute()
    Add utility for executing commands.
    
    This function assumes that, when a given predicate
    |end| finishes, the child process does not emit any other
    messages.
    
    If this is not the case, caller to execute()
    must handle these I/O in the pipes in the returned
    CommandResult object. Otherwise the child program may
    hang on I/O.
    
    Test: binderUtilsTest
    Bug: 190233850
    
    Change-Id: Ib8be5db140af04a286ccbb2283cb032390aff5ac

--- libs/binder/Utils.h
+++ libs/binder/Utils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,93 @@
  * limitations under the License.
  */
 
-#include <cstdint>
+#pragma once
+
 #include <stddef.h>
+#include <cstdint>
+
+#include <optional>
+#include <ostream>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include <android-base/expected.h>
+#include <android-base/macros.h>
+#include <android-base/unique_fd.h>
 
 namespace android {
 
 // avoid optimizations
 void zeroMemory(uint8_t* data, size_t size);
 
-}   // namespace android
+struct ExecuteError {
+    int code; // errno
+    std::string message;
+
+    explicit ExecuteError(int codeArg, std::string msg) : code(codeArg), message(std::move(msg)) {}
+    [[nodiscard]] std::string toString() const;
+};
+std::ostream& operator<<(std::ostream& os, const ExecuteError& res);
+
+struct CommandResult {
+    std::optional<int32_t> exitCode;
+    std::optional<int32_t> signal;
+    std::optional<pid_t> pid;
+    std::string stdout;
+    std::string stderr;
+
+    android::base::unique_fd outPipe;
+    android::base::unique_fd errPipe;
+
+    CommandResult() = default;
+    CommandResult(CommandResult&& other) noexcept { (*this) = std::move(other); }
+    CommandResult& operator=(CommandResult&& other) noexcept {
+        std::swap(exitCode, other.exitCode);
+        std::swap(signal, other.signal);
+        std::swap(pid, other.pid);
+        std::swap(stdout, other.stdout);
+        std::swap(stderr, other.stderr);
+        return *this;
+    }
+    [[nodiscard]] std::string toString() const;
+    ~CommandResult();
+
+    [[nodiscard]] bool stdoutEndsWithNewLine() const {
+        return !stdout.empty() && stdout.back() == '\n';
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CommandResult);
+};
+
+std::ostream& operator<<(std::ostream& os, const CommandResult& res);
+
+using ExecuteResult = android::base::expected<CommandResult, ExecuteError>;
+std::ostream& operator<<(std::ostream& os, const ExecuteResult& res);
+
+// Execute a command using tokens specified in @a argStringVec.
+//
+// @a end is a predicate checked periodically when the command emits any output to stdout or
+// stderr. When it is evaluated to true, the function returns immediately even though
+// the child process has not been terminated. The function also assumes that, after @a end
+// is evaluated to true, the child process does not emit any other messages.
+// If this is not the case, caller to execute() must handle these I/O in the pipes in the returned
+// CommandResult object. Otherwise the child program may hang on I/O.
+//
+// If @a end is nullptr, it is equivalent to a predicate that always returns false. In this
+// case, execute() returns after the child process is terminated.
+//
+// If @a end is evaluated to true, and execute() returns with the child process running,
+// the returned CommandResult has pid, outPipe, and errPipe set. In this case, the caller is
+// responsible for holding the returned CommandResult. When the CommandResult object is destroyed,
+// the child process is killed.
+//
+// On the other hand, execute() returns with the child process terminated, either exitCode or signal
+// is set.
+//
+// If the parent process has encountered any errors for system calls, return ExecuteError with
+// the proper errno set.
+ExecuteResult execute(std::vector<std::string> argStringVec,
+                      const std::function<bool(const CommandResult&)>& end);
+} // namespace android
