1c3abbfdbe5cf603f3a1a228e131a6d9f53d639a
Yifan Hong <elsk@google.com>
2021-06-14 19:58:34 -0700

    DO NOT SUBMIT [SQUASH] bpservicemanager -> bnservicemanager
    Bug: 191102354
    Test: none
    
    Change-Id: I0fe853dfbdc801617c3acefe14a4f47502612830

--- libs/binder/servicedispatcher.cpp
+++ libs/binder/servicedispatcher.cpp
@@ -23,6 +23,7 @@
 #include <android-base/logging.h>
 #include <android-base/properties.h>
 #include <android-base/stringprintf.h>
+#include <android/os/BnServiceManager.h>
 #include <android/os/IServiceManager.h>
 #include <binder/IServiceManager.h>
 #include <binder/RpcServer.h>
@@ -100,6 +101,66 @@ int Dispatch(const char* name) {
     __builtin_unreachable();
 }
 
+// Wrapper that wraps a BpServiceManager as a BnServiceManager.
+class ServiceManagerProxyToNative : public android::os::BnServiceManager {
+public:
+    ServiceManagerProxyToNative(const sp<android::os::IServiceManager>& impl) : mImpl(impl) {}
+    android::binder::Status getService(const std::string& name,
+                                       android::sp<android::IBinder>* _aidl_return) override {
+        return mImpl->getService(name, _aidl_return);
+    }
+    android::binder::Status checkService(const std::string& name,
+                                         android::sp<android::IBinder>* _aidl_return) override {
+        return mImpl->checkService(name, _aidl_return);
+    }
+    android::binder::Status addService(const std::string& name,
+                                       const android::sp<android::IBinder>& service,
+                                       bool allowIsolated, int32_t dumpPriority) override {
+        return mImpl->addService(name, service, allowIsolated, dumpPriority);
+    }
+    android::binder::Status listServices(int32_t dumpPriority,
+                                         std::vector<std::string>* _aidl_return) override {
+        return mImpl->listServices(dumpPriority, _aidl_return);
+    }
+    android::binder::Status registerForNotifications(
+            const std::string& name,
+            const android::sp<android::os::IServiceCallback>& callback) override {
+        return mImpl->registerForNotifications(name, callback);
+    }
+    android::binder::Status unregisterForNotifications(
+            const std::string& name,
+            const android::sp<android::os::IServiceCallback>& callback) override {
+        return mImpl->unregisterForNotifications(name, callback);
+    }
+    android::binder::Status isDeclared(const std::string& name, bool* _aidl_return) override {
+        return mImpl->isDeclared(name, _aidl_return);
+    }
+    android::binder::Status getDeclaredInstances(const std::string& iface,
+                                                 std::vector<std::string>* _aidl_return) override {
+        return mImpl->getDeclaredInstances(iface, _aidl_return);
+    }
+    android::binder::Status updatableViaApex(const std::string& name,
+                                             std::optional<std::string>* _aidl_return) override {
+        return mImpl->updatableViaApex(name, _aidl_return);
+    }
+    android::binder::Status registerClientCallback(
+            const std::string& name, const android::sp<android::IBinder>& service,
+            const android::sp<android::os::IClientCallback>& callback) override {
+        return mImpl->registerClientCallback(name, service, callback);
+    }
+    android::binder::Status tryUnregisterService(
+            const std::string& name, const android::sp<android::IBinder>& service) override {
+        return mImpl->tryUnregisterService(name, service);
+    }
+    android::binder::Status getServiceDebugInfo(
+            std::vector<android::os::ServiceDebugInfo>* _aidl_return) override {
+        return mImpl->getServiceDebugInfo(_aidl_return);
+    }
+
+private:
+    sp<android::os::IServiceManager> mImpl;
+};
+
 int wrapServiceManager() {
     auto sm = defaultServiceManager();
     if (nullptr == sm) {
@@ -111,6 +172,15 @@ int wrapServiceManager() {
         LOG(ERROR) << "No service called `manager`";
         return EX_SOFTWARE;
     }
+    auto interface = android::os::IServiceManager::asInterface(service);
+    if (nullptr == interface) {
+        LOG(ERROR) << "Cannot cast service called `manager` to IServiceManager";
+        return EX_SOFTWARE;
+    }
+
+    // Work around restriction that doesn't allow us to send proxy over RPC.
+    interface = sp<ServiceManagerProxyToNative>::make(interface);
+    service = ServiceManagerProxyToNative::asBinder(interface);
 
     auto rpcServer = RpcServer::make();
     rpcServer->iUnderstandThisCodeIsExperimentalAndIWillNotUseItInProduction();
