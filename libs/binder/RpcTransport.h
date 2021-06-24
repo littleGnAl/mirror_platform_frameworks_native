8d9041e81b3eced6374bc46c4f3ee3611e807b3a
Yifan Hong <elsk@google.com>
2021-06-23 19:12:32 -0700

    binder: Add RpcTransport
    RpcTransport represents the transport layer. The transport
    layer may have security (TLS) enabled or not, depending on
    how it is configured.
    
    Test: TH
    Test: binderRpcTest (see follow up CL)
    Bug: 190868302
    
    Change-Id: I8f2445f08429581b703727a74ecef11bc44bb294

---
+++ libs/binder/RpcTransport.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Wraps the transport layer of RPC. Implementation may use plain sockets or TLS.
+
+#pragma once
+
+#include <memory>
+
+#include <android-base/unique_fd.h>
+
+namespace android {
+
+class RpcTransportCtx;
+class RpcTransport;
+
+// Represents a socket connection. Wrapper of SSL for libbinder usage.
+class RpcTransport {
+public:
+    virtual ~RpcTransport() = default;
+
+    // replacement of ::send(). errno may not be set if TLS is enabled.
+    virtual int send(const void *buf, int size) = 0;
+
+    // replacement of ::recv(). errno may not be set if TLS is enabled.
+    virtual int recv(void *buf, int size) = 0;
+
+    // replacement of ::recv(MSG_PEEK). errno may not be set if TLS is enabled.
+    //
+    // Implementation details:
+    // - For TLS, this may invoke syscalls and read data from the transport
+    // into an internal buffer in userspace. After that, pending() == true.
+    // - For raw sockets, this calls ::recv(MSG_PEEK), which leaves the data in the kernel buffer;
+    // pending() is always false.
+    virtual int peek(void *buf, int size) = 0;
+
+    // Returns true if there are data pending in a userspace buffer that RpcTransport holds.
+    //
+    // Implementation details:
+    // - For TLS, this does not invoke any syscalls or read any data from the
+    // transport. This only returns whether there are data pending in the internal buffer in
+    // userspace.
+    // - For raw sockets, this always returns false.
+    virtual bool pending() = 0;
+
+protected:
+    explicit RpcTransport(android::base::unique_fd socket) : mSocket(std::move(socket)) {}
+    [[nodiscard]] inline android::base::borrowed_fd socketFd() const { return mSocket; }
+
+private:
+    friend RpcTransportCtx;
+    android::base::unique_fd mSocket;
+};
+
+// Wrapper of SSL_CTX for libbinder usage.
+class RpcTransportCtx {
+public:
+    // Create a proper context with TLS enabled or not. Never null.
+    static std::unique_ptr<RpcTransportCtx> create(bool tls = false);
+    virtual ~RpcTransportCtx() = default;
+    // Called after ::accept4() to configure TLS.
+    virtual std::unique_ptr<RpcTransport> sslAccept(android::base::unique_fd acceptedFd) = 0;
+    // Called after ::connect() to configure TLS.
+    virtual std::unique_ptr<RpcTransport> sslConnect(android::base::unique_fd connectedFd) = 0;
+};
+
+} // namespace android
