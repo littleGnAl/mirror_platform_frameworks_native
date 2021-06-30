874f7642a10337740394e631d22e34d477bf2628
Yifan Hong <elsk@google.com>
2021-06-29 18:44:56 -0700

    RpcSession attaches/detaches JVM for Java thread
    If libutils thread hooks are set, run them before
    and after the while loop in join. This ensures the
    Java thread is attached / detached appropriately for
    binder threads that handles RPC calls.
    
    Test: run aservice on Java services
    Bug: 190450693
    Change-Id: I23f171dd1e08dfacfbbb46ecc9564cbf42d6f353

--- libs/binder/RpcSession.cpp
+++ libs/binder/RpcSession.cpp
@@ -20,6 +20,7 @@
 
 #include <inttypes.h>
 #include <poll.h>
+#include <pthread.h>
 #include <unistd.h>
 
 #include <string_view>
@@ -28,6 +29,7 @@
 #include <binder/Parcel.h>
 #include <binder/RpcServer.h>
 #include <binder/Stability.h>
+#include <utils/AndroidThreads.h>
 #include <utils/String8.h>
 
 #include "RpcSocketAddress.h"
@@ -274,10 +276,35 @@ RpcSession::PreJoinSetupResult RpcSession::preJoinSetup(base::unique_fd fd) {
     };
 }
 
+namespace {
+class HookExecuter {
+public:
+    HookExecuter() { runHook(true /* is pre */); }
+    ~HookExecuter() { runHook(false /* is pre */); }
+
+private:
+    void runHook(bool isPre) {
+        thread_hook_t hook = nullptr;
+        void* userdata = nullptr;
+        auto getter = isPre ? getThreadPreHook : getThreadPostHook;
+        getter(&hook, &userdata);
+        if (hook == nullptr) return;
+
+        char buf[16];
+        const char* threadName = "UnknownRpcSessionThread"; // default thread name
+        if (0 == pthread_getname_np(pthread_self(), buf, sizeof(buf))) {
+            threadName = buf;
+        }
+        hook(userdata, threadName);
+    }
+};
+} // namespace
+
 void RpcSession::join(sp<RpcSession>&& session, PreJoinSetupResult&& setupResult) {
     sp<RpcConnection>& connection = setupResult.connection;
 
     if (setupResult.status == OK) {
+        HookExecuter hookExecuter;
         while (true) {
             status_t status = session->state()->getAndExecuteCommand(connection, session,
                                                                      RpcState::CommandType::ANY);
