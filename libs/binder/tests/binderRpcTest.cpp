d91db46763f44728dfde60020e15df189c6219be
Steven Moreland <smoreland@google.com>
2021-07-14 23:19:25 +0000

    libbinder: send is non-blocking and interruptible
    FIXME: this causes several flakes if flake mode is turned on
    
    We actually don't use MSG_DONTBLOCK now, but we only read after
    receiving POLLOUT. However, by polling, send also now plugs into the
    shutdown triggers. As a side effect, this causes send calls to fail
    earlier.
    
    Bug: 186470974
    Test: binderRpcTest
    Change-Id: Ia13d0dc1749fa7c10340e07e221b4b682cc59cc8

--- libs/binder/tests/binderRpcTest.cpp
+++ libs/binder/tests/binderRpcTest.cpp
@@ -127,11 +127,6 @@ public:
         out->clear();
         for (auto session : spServer->listSessions()) {
             size_t count = session->state()->countBinders();
-            if (count != 1) {
-                // this is called when there is only one binder held remaining,
-                // so to aid debugging
-                session->state()->dump();
-            }
             out->push_back(count);
         }
         return Status::ok();
@@ -967,6 +962,12 @@ TEST_P(BinderRpc, OnewayCallExhaustion) {
 
     Status status = iface->sleepMsAsync(kTooLongMs);
     EXPECT_EQ(DEAD_OBJECT, status.transactionError()) << status;
+
+    // now that it has died, wait for the remote session to shutdown
+    std::vector<int32_t> remoteCounts;
+    do {
+        EXPECT_OK(proc.rootIface->countBinders(&remoteCounts));
+    } while (remoteCounts.size() == kNumClients);
 
     // the second session should be shutdown in the other process by the time we
     // are able to join above (it'll only be hung up once it finishes processing
