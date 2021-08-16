9bcd5d3f5dfff264922e9161ca18e04d7e4c970b
Yifan Hong <elsk@google.com>
2021-08-16 16:59:37 -0700

    binder: Support DER format certificates.
    Test: binderRpcTest
    Bug: 195166979
    
    Change-Id: I78b4d2b1207819a9cf9eadf099e2c44cfaac8096

--- libs/binder/tests/binderRpcTest.cpp
+++ libs/binder/tests/binderRpcTest.cpp
@@ -1725,6 +1725,7 @@ TEST_P(RpcTransportTest, MaliciousClient) {
 std::vector<CertificateFormat> testCertificateFormats() {
     return {
             CertificateFormat::PEM,
+            CertificateFormat::DER,
     };
 }
 
