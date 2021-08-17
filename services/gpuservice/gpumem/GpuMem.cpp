fd9fe83067f2ee7d83709908b174d826b1236bb3
Kalesh Singh <kaleshsingh@google.com>
2021-08-17 05:29:56 +0000

    Support tracking GPU imported memory from tracepoint
    Detect if gpu_mem_total supports imported memory and attach the
    gpu_mem bpf prog that tracks both total and imported gpu memory.
    
    Bug: N/A
    Test: adb shell dumpsys gpu
    Change-Id: I90587098117c8aac245d717bd2cc53f54f6f320d

--- services/gpuservice/gpumem/GpuMem.cpp
+++ services/gpuservice/gpumem/GpuMem.cpp
@@ -20,20 +20,65 @@
 
 #include "gpumem/GpuMem.h"
 
+#include <android-base/file.h>
 #include <android-base/stringprintf.h>
+#include <android-base/strings.h>
 #include <libbpf.h>
 #include <libbpf_android.h>
 #include <log/log.h>
 #include <unistd.h>
 #include <utils/Timers.h>
 #include <utils/Trace.h>
-
 #include <unordered_map>
 #include <vector>
 
 namespace android {
 
 using base::StringAppendF;
+
+class GpuMemProcInfo {
+public:
+    uint32_t pid;
+    uint64_t size;
+    uint64_t imported_size;
+
+    GpuMemProcInfo(uint32_t pid, uint64_t size, uint64_t imported_size)
+          : pid(pid), size(size), imported_size(imported_size) {}
+};
+
+static bool FtraceEventHasField(const char* subsystem, const char* event, const char* field) {
+    using ::android::base::ReadFileToString;
+    using ::android::base::Split;
+    using ::android::base::StartsWith;
+    using ::android::base::StringPrintf;
+    using ::android::base::Trim;
+
+    std::string path = StringPrintf("/sys/kernel/tracing/events/%s/%s/format", subsystem, event);
+    std::string debugfs_path =
+            StringPrintf("/sys/kernel/tracing/events/%s/%s/format", subsystem, event);
+
+    std::string format;
+    if (!ReadFileToString(path, &format) && !ReadFileToString(debugfs_path, &format)) {
+        ALOGE("Failed to read ftrace event format for %s/%s", subsystem, event);
+        return false;
+    }
+
+    auto lines = Split(format, "\n");
+    for (auto& line : lines) {
+        line = Trim(line);
+
+        if (!StartsWith(line, "field:")) {
+            continue;
+        }
+
+        auto field_name = Split(Trim(Split(line, ";")[0]), " ").back();
+        if (field_name == field) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 GpuMem::~GpuMem() {
     bpf_detach_tracepoint(kGpuMemTraceGroup, kGpuMemTotalTracepoint);
@@ -43,10 +88,16 @@ void GpuMem::initialize() {
     // Make sure bpf programs are loaded
     bpf::waitForProgsLoaded();
 
+    mImportedMemSupported = FtraceEventHasField(kGpuMemTraceGroup, kGpuMemTotalTracepoint,
+                                                kGpuMemImportedSizeField);
+
+    ALOGI("GPU imported memory tracking is %ssupported", mImportedMemSupported ? "" : "not ");
+
     errno = 0;
-    int fd = bpf::retrieveProgram(kGpuMemTotalProgPath);
+    const char* bpf_prog_path = (mImportedMemSupported) ? kGpuMemProgPath : kGpuMemTotalProgPath;
+    int fd = bpf::retrieveProgram(bpf_prog_path);
     if (fd < 0) {
-        ALOGE("Failed to retrieve pinned program from %s [%d(%s)]", kGpuMemTotalProgPath, errno,
+        ALOGE("Failed to retrieve pinned program from %s [%d(%s)]", bpf_prog_path, errno,
               strerror(errno));
         return;
     }
@@ -64,6 +115,9 @@ void GpuMem::initialize() {
         sleep(1);
     }
 
+    ALOGI("Attached bpf prog %s to tracepoint %s/%s", bpf_prog_path, kGpuMemTraceGroup,
+          kGpuMemTotalTracepoint);
+
     // Use the read-only wrapper BpfMapRO to properly retrieve the read-only map.
     errno = 0;
     auto map = bpf::BpfMapRO<uint64_t, uint64_t>(kGpuMemTotalMapPath);
@@ -74,11 +128,26 @@ void GpuMem::initialize() {
     }
     setGpuMemTotalMap(map);
 
+    if (mImportedMemSupported) {
+        errno = 0;
+        auto mem_imported_map = bpf::BpfMapRO<uint64_t, uint64_t>(kGpuMemImportedMapPath);
+        if (!mem_imported_map.isValid()) {
+            ALOGE("Failed to create bpf map from %s [%d(%s)]", kGpuMemImportedMapPath, errno,
+                  strerror(errno));
+            return;
+        }
+        setGpuMemImportedMap(mem_imported_map);
+    }
+
     mInitialized.store(true);
 }
 
 void GpuMem::setGpuMemTotalMap(bpf::BpfMap<uint64_t, uint64_t>& map) {
     mGpuMemTotalMap = std::move(map);
+}
+
+void GpuMem::setGpuMemImportedMap(bpf::BpfMap<uint64_t, uint64_t>& map) {
+    mGpuMemImportedMap = std::move(map);
 }
 
 // Dump the snapshots of global and per process memory usage on all gpus
@@ -90,14 +159,21 @@ void GpuMem::dump(const Vector<String16>& /* args */, std::string* result) {
         return;
     }
 
+    if (mImportedMemSupported && !mGpuMemImportedMap.isValid()) {
+        result->append("Failed to initialize GPU imported mem eBPF map\n");
+        return;
+    }
+
     auto res = mGpuMemTotalMap.getFirstKey();
     if (!res.ok()) {
         result->append("GPU memory total usage map is empty\n");
         return;
     }
+
+    // unordered_map<gpu_id, GpuMemProcInfo>
+    std::unordered_map<uint32_t, std::vector<GpuMemProcInfo>> dumpMap;
+
     uint64_t key = res.value();
-    // unordered_map<gpu_id, vector<pair<pid, size>>>
-    std::unordered_map<uint32_t, std::vector<std::pair<uint32_t, uint64_t>>> dumpMap;
     while (true) {
         uint32_t gpu_id = key >> 32;
         uint32_t pid = key;
@@ -106,7 +182,14 @@ void GpuMem::dump(const Vector<String16>& /* args */, std::string* result) {
         if (!res.ok()) break;
         uint64_t size = res.value();
 
-        dumpMap[gpu_id].emplace_back(pid, size);
+        uint64_t imported_size = 0;
+        if (mImportedMemSupported) {
+            if (res = mGpuMemImportedMap.readValue(key); res.ok()) {
+                imported_size = res.value();
+            }
+        }
+
+        dumpMap[gpu_id].emplace_back(pid, size, imported_size);
 
         res = mGpuMemTotalMap.getNextKey(key);
         if (!res.ok()) break;
@@ -118,18 +201,29 @@ void GpuMem::dump(const Vector<String16>& /* args */, std::string* result) {
         StringAppendF(result, "Memory snapshot for GPU %u:\n", gpu.first);
 
         std::sort(gpu.second.begin(), gpu.second.end(),
-                  [](auto& l, auto& r) { return l.first < r.first; });
+                  [](auto& l, auto& r) { return l.pid < r.pid; });
 
         int i = 0;
-        if (gpu.second[0].first != 0) {
-            StringAppendF(result, "Global total: N/A\n");
+        if (gpu.second[0].pid != 0) {
+            StringAppendF(result, "Global: total: N/A");
+            if (mImportedMemSupported) {
+                StringAppendF(result, ", imported: N/A");
+            }
+            StringAppendF(result, "\n");
         } else {
-            StringAppendF(result, "Global total: %" PRIu64 "\n", gpu.second[0].second);
+            StringAppendF(result, "Global: total: %" PRIu64, gpu.second[0].size);
+            if (mImportedMemSupported) {
+                StringAppendF(result, ", imported: %" PRIu64, gpu.second[0].imported_size);
+            }
+            StringAppendF(result, "\n");
             i++;
         }
         for (; i < gpu.second.size(); i++) {
-            StringAppendF(result, "Proc %u total: %" PRIu64 "\n", gpu.second[i].first,
-                          gpu.second[i].second);
+            StringAppendF(result, "Proc %u total: %" PRIu64, gpu.second[i].pid, gpu.second[i].size);
+            if (mImportedMemSupported) {
+                StringAppendF(result, ", imported: %" PRIu64, gpu.second[i].imported_size);
+            }
+            StringAppendF(result, "\n");
         }
     }
 }
