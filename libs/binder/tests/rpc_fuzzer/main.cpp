132005a8a456e073febaa672a9d9cd639e230c0e
Yifan Hong <elsk@google.com>
2021-09-24 11:56:44 -0700

    BUG
    Change-Id: Icd87a2fda8ebabdd35e8b4d8ca623ae568477db3

--- libs/binder/tests/rpc_fuzzer/main.cpp
+++ libs/binder/tests/rpc_fuzzer/main.cpp
@@ -92,7 +92,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     server->iUnderstandThisCodeIsExperimentalAndIWillNotUseItInProduction();
     CHECK_EQ(OK, server->setupUnixDomainServer(kSock.c_str()));
 
-    std::thread serverThread([=] { (void)server->join(); });
+    std::thread serverThread([=] {
+        pthread_setname_np(pthread_self(), "server");
+        (void)server->join();
+    });
 
     sockaddr_un addr{
             .sun_family = AF_UNIX,
@@ -100,28 +103,35 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     CHECK_LT(kSock.size(), sizeof(addr.sun_path));
     memcpy(&addr.sun_path, kSock.c_str(), kSock.size());
 
-    std::vector<base::unique_fd> connections;
+    std::vector<std::unique_ptr<RpcTransport>> connections;
 
     bool hangupBeforeShutdown = provider.ConsumeBool();
 
+    std::unique_ptr<RpcTransportCtx> clientCtx = makeTransportCtxFactory(&provider)->newClientCtx();
+    CHECK_NE(clientCtx, nullptr);
+    auto fdTrigger = FdTrigger::make();
+    CHECK_NE(fdTrigger, nullptr);
+
     while (provider.remaining_bytes() > 0) {
         if (connections.empty() || provider.ConsumeBool()) {
-            base::unique_fd fd(TEMP_FAILURE_RETRY(socket(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0)));
+            base::unique_fd fd(TEMP_FAILURE_RETRY(socket(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC | SOCK_NONBLOCK, 0)));
             CHECK_NE(fd.get(), -1);
             CHECK_EQ(0,
                      TEMP_FAILURE_RETRY(
                              connect(fd.get(), reinterpret_cast<sockaddr*>(&addr), sizeof(addr))))
                     << strerror(errno);
-            connections.push_back(std::move(fd));
+            auto transport = clientCtx->newTransport(std::move(fd), fdTrigger.get());
+            CHECK_NE(transport, nullptr);
+            connections.emplace_back(std::move(transport));
         } else {
             size_t idx = provider.ConsumeIntegralInRange<size_t>(0, connections.size() - 1);
 
             if (provider.ConsumeBool()) {
                 std::string writeData = provider.ConsumeRandomLengthString();
-                ssize_t size = TEMP_FAILURE_RETRY(send(connections.at(idx).get(), writeData.data(),
-                                                       writeData.size(), MSG_NOSIGNAL));
-                CHECK(errno == EPIPE || size == writeData.size())
-                        << size << " " << writeData.size() << " " << strerror(errno);
+                status_t status = connections.at(idx)->interruptableWriteFully(fdTrigger.get(),
+                                                                               writeData.data(),
+                                                                               writeData.size());
+                CHECK(status == OK || status == DEAD_OBJECT) << statusToString(status);
             } else {
                 connections.erase(connections.begin() + idx); // hang up
             }
