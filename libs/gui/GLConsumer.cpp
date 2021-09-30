d99ab6d93f39ec52cc438c1605ec1a08a022008c
SOOJONG HWANG <sjong.hwang@samsung.com>
2021-10-01 00:36:06 +0900

    GLConsumer: add HW_TEXTURE usage when create texture
    GPU expects external image to be aligned with its h/w requirement.
    GraphicBuffer which will be used as texture, should be created with HW_TEXTURE usage.
    
    Signed-off-by: SOOJONG HWANG <sjong.hwang@samsung.com>
    Change-Id: Icac81da95f3c98bb2fe8d0cad2cef81c62951726

--- libs/gui/GLConsumer.cpp
+++ libs/gui/GLConsumer.cpp
@@ -301,7 +301,7 @@ sp<GraphicBuffer> GLConsumer::getDebugTexImageBuffer() {
         // continues to use it.
         sp<GraphicBuffer> buffer = new GraphicBuffer(
                 kDebugData.width, kDebugData.height, PIXEL_FORMAT_RGBA_8888,
-                GraphicBuffer::USAGE_SW_WRITE_RARELY,
+                GraphicBuffer::USAGE_SW_WRITE_RARELY | GraphicBuffer::USAGE_HW_TEXTURE,
                 "[GLConsumer debug texture]");
         uint32_t* bits;
         buffer->lock(GraphicBuffer::USAGE_SW_WRITE_RARELY, reinterpret_cast<void**>(&bits));
