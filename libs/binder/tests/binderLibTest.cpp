4fe37ef5337ba2d0c52f8a93a31a61783dc0a400
Steven Moreland <smoreland@google.com>
2021-10-14 14:24:15 -0700

    libbinder: use after fork aborts
    This is a separate CL, since the warning is independently useful, even
    if this needs to be reverted.
    
    Fixes: 202289725
    Test: binderLibTest
    Change-Id: Ifb046f01cee047d1908a69c075a66ecb0bc68a78

--- libs/binder/tests/binderLibTest.cpp
+++ libs/binder/tests/binderLibTest.cpp
@@ -436,6 +436,11 @@ class TestDeathRecipient : public IBinder::DeathRecipient, public BinderLibTestE
         };
 };
 
+TEST_F(BinderLibTest, CannotUseBinderAfterFork) {
+    // EXPECT_DEATH works by forking the process
+    EXPECT_DEATH({ ProcessState::self(); }, "libbinder does not support being forked");
+}
+
 TEST_F(BinderLibTest, WasParceled) {
     auto binder = sp<BBinder>::make();
     EXPECT_FALSE(binder->wasParceled());
