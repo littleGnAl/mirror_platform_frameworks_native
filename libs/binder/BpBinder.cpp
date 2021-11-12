52e26643ee8589266a9eefbc2668551e0a8ea709
Steven Moreland <smoreland@google.com>
2021-11-12 22:54:53 +0000

    libbinder: add build option to disable the kernel IPC
    Add a new BINDER_NO_KERNEL_IPC to disable all code related
    to the kernel driver backend.
    
    Bug: 224644083
    Test: m
    Change-Id: I622757fcc0f9885dbf271c0ffa84c54938d50774

--- libs/binder/BpBinder.cpp
+++ libs/binder/BpBinder.cpp
@@ -19,14 +19,18 @@
 
 #include <binder/BpBinder.h>
 
-#include <binder/IPCThreadState.h>
 #include <binder/IResultReceiver.h>
+#include <binder/Parcel.h>
 #include <binder/RpcSession.h>
 #include <binder/Stability.h>
 #include <cutils/compiler.h>
 #include <utils/Log.h>
 
 #include <stdio.h>
+
+#ifndef BINDER_NO_KERNEL_IPC
+#include <binder/IPCThreadState.h>
+#endif
 
 //#undef ALOGV
 //#define ALOGV(...) fprintf(stderr, __VA_ARGS__)
@@ -115,6 +119,10 @@ void BpBinder::ObjectManager::kill()
 // ---------------------------------------------------------------------------
 
 sp<BpBinder> BpBinder::create(int32_t handle) {
+#ifdef BINDER_NO_KERNEL_IPC
+    (void)handle;
+    return nullptr;
+#else
     int32_t trackedUid = -1;
     if (sCountByUidEnabled) {
         trackedUid = IPCThreadState::self()->getCallingUid();
@@ -153,6 +161,7 @@ sp<BpBinder> BpBinder::create(int32_t handle) {
         sTrackingMap[trackedUid]++;
     }
     return sp<BpBinder>::make(BinderHandle{handle}, trackedUid);
+#endif
 }
 
 sp<BpBinder> BpBinder::create(const sp<RpcSession>& session, uint64_t address) {
@@ -179,9 +188,11 @@ BpBinder::BpBinder(Handle&& handle)
 BpBinder::BpBinder(BinderHandle&& handle, int32_t trackedUid) : BpBinder(Handle(handle)) {
     mTrackedUid = trackedUid;
 
+#ifndef BINDER_NO_KERNEL_IPC
     ALOGV("Creating BpBinder %p handle %d\n", this, this->binderHandle());
 
     IPCThreadState::self()->incWeakHandle(this->binderHandle(), this);
+#endif
 }
 
 BpBinder::BpBinder(RpcHandle&& handle) : BpBinder(Handle(handle)) {
@@ -257,6 +268,7 @@ status_t BpBinder::pingBinder()
     return transact(PING_TRANSACTION, data, &reply);
 }
 
+#ifndef BINDER_NO_KERNEL_IPC
 status_t BpBinder::dump(int fd, const Vector<String16>& args)
 {
     Parcel send;
@@ -270,6 +282,7 @@ status_t BpBinder::dump(int fd, const Vector<String16>& args)
     status_t err = transact(DUMP_TRANSACTION, send, &reply);
     return err;
 }
+#endif
 
 // NOLINTNEXTLINE(google-default-arguments)
 status_t BpBinder::transact(
@@ -279,7 +292,7 @@ status_t BpBinder::transact(
     if (mAlive) {
         bool privateVendor = flags & FLAG_PRIVATE_VENDOR;
         // don't send userspace flags to the kernel
-        flags = flags & ~FLAG_PRIVATE_VENDOR;
+        flags = flags & ~static_cast<uint32_t>(FLAG_PRIVATE_VENDOR);
 
         // user transactions require a given stability level
         if (code >= FIRST_CALL_TRANSACTION && code <= LAST_CALL_TRANSACTION) {
@@ -303,7 +316,11 @@ status_t BpBinder::transact(
             status = rpcSession()->transact(sp<IBinder>::fromExisting(this), code, data, reply,
                                             flags);
         } else {
+#ifdef BINDER_NO_KERNEL_IPC
+            status = UNKNOWN_ERROR;
+#else
             status = IPCThreadState::self()->transact(binderHandle(), code, data, reply, flags);
+#endif
         }
         if (data.dataSize() > LOG_TRANSACTIONS_OVER_SIZE) {
             Mutex::Autolock _l(mLock);
@@ -328,6 +345,11 @@ status_t BpBinder::linkToDeath(
 {
     if (isRpcBinder()) return UNKNOWN_TRANSACTION;
 
+#ifdef BINDER_NO_KERNEL_IPC
+    (void)recipient;
+    (void)cookie;
+    (void)flags;
+#else
     Obituary ob;
     ob.recipient = recipient;
     ob.cookie = cookie;
@@ -355,6 +377,7 @@ status_t BpBinder::linkToDeath(
             return res >= (ssize_t)NO_ERROR ? (status_t)NO_ERROR : res;
         }
     }
+#endif
 
     return DEAD_OBJECT;
 }
@@ -366,6 +389,12 @@ status_t BpBinder::unlinkToDeath(
 {
     if (isRpcBinder()) return UNKNOWN_TRANSACTION;
 
+#ifdef BINDER_NO_KERNEL_IPC
+    (void)recipient;
+    (void)cookie;
+    (void)flags;
+    (void)outRecipient;
+#else
     AutoMutex _l(mLock);
 
     if (mObitsSent) {
@@ -393,12 +422,14 @@ status_t BpBinder::unlinkToDeath(
             return NO_ERROR;
         }
     }
+#endif
 
     return NAME_NOT_FOUND;
 }
 
 void BpBinder::sendObituary()
 {
+#ifndef BINDER_NO_KERNEL_IPC
     LOG_ALWAYS_FATAL_IF(isRpcBinder(), "Cannot send obituary for remote binder.");
 
     ALOGV("Sending obituary for proxy %p handle %d, mObitsSent=%s\n", this, binderHandle(),
@@ -430,6 +461,7 @@ void BpBinder::sendObituary()
 
         delete obits;
     }
+#endif
 }
 
 void BpBinder::reportOneDeath(const Obituary& obit)
@@ -471,9 +503,10 @@ BpBinder* BpBinder::remoteBinder()
 
 BpBinder::~BpBinder()
 {
-    ALOGV("Destroying BpBinder %p handle %d\n", this, binderHandle());
-
     if (CC_UNLIKELY(isRpcBinder())) return;
+
+#ifndef BINDER_NO_KERNEL_IPC
+    ALOGV("Destroying BpBinder %p handle %d\n", this, binderHandle());
 
     IPCThreadState* ipc = IPCThreadState::self();
 
@@ -503,23 +536,29 @@ BpBinder::~BpBinder()
         ipc->expungeHandle(binderHandle(), this);
         ipc->decWeakHandle(binderHandle());
     }
+#endif
 }
 
 void BpBinder::onFirstRef()
 {
-    ALOGV("onFirstRef BpBinder %p handle %d\n", this, binderHandle());
     if (CC_UNLIKELY(isRpcBinder())) return;
+
+#ifndef BINDER_NO_KERNEL_IPC
+    ALOGV("onFirstRef BpBinder %p handle %d\n", this, binderHandle());
     IPCThreadState* ipc = IPCThreadState::self();
     if (ipc) ipc->incStrongHandle(binderHandle(), this);
+#endif
 }
 
 void BpBinder::onLastStrongRef(const void* /*id*/)
 {
-    ALOGV("onLastStrongRef BpBinder %p handle %d\n", this, binderHandle());
     if (CC_UNLIKELY(isRpcBinder())) {
         (void)rpcSession()->sendDecStrong(this);
         return;
     }
+
+#ifndef BINDER_NO_KERNEL_IPC
+    ALOGV("onLastStrongRef BpBinder %p handle %d\n", this, binderHandle());
     IF_ALOGV() {
         printRefs();
     }
@@ -545,6 +584,7 @@ void BpBinder::onLastStrongRef(const void* /*id*/)
         // are no longer linked?
         delete obits;
     }
+#endif
 }
 
 bool BpBinder::onIncStrongAttempted(uint32_t /*flags*/, const void* /*id*/)
@@ -552,9 +592,13 @@ bool BpBinder::onIncStrongAttempted(uint32_t /*flags*/, const void* /*id*/)
     // RPC binder doesn't currently support inc from weak binders
     if (CC_UNLIKELY(isRpcBinder())) return false;
 
+#ifdef BINDER_NO_KERNEL_IPC
+    return false;
+#else
     ALOGV("onIncStrongAttempted BpBinder %p handle %d\n", this, binderHandle());
     IPCThreadState* ipc = IPCThreadState::self();
     return ipc ? ipc->attemptIncStrongHandle(binderHandle()) == NO_ERROR : false;
+#endif
 }
 
 uint32_t BpBinder::getBinderProxyCount(uint32_t uid)
