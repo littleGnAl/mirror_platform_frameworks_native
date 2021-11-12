9c67904887905c954ce1ef082b6d4bb0b09e26cb
Steven Moreland <smoreland@google.com>
2021-11-12 22:54:53 +0000

    libbinder: add build option to disable the kernel IPC
    Add a new BINDER_NO_KERNEL_IPC to disable all code related
    to the kernel driver backend.
    
    Bug: 224644083
    Test: m
    Change-Id: I622757fcc0f9885dbf271c0ffa84c54938d50774

--- libs/binder/include/binder/BpBinder.h
+++ libs/binder/include/binder/BpBinder.h
@@ -48,7 +48,9 @@ public:
     virtual const String16&    getInterfaceDescriptor() const;
     virtual bool        isBinderAlive() const;
     virtual status_t    pingBinder();
+#ifndef BINDER_NO_KERNEL_IPC
     virtual status_t    dump(int fd, const Vector<String16>& args);
+#endif
 
     // NOLINTNEXTLINE(google-default-arguments)
     virtual status_t    transact(   uint32_t code,
