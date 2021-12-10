231bbfcd733a9368eeb352b5cadd6a049e49b2f6
Andrei Homescu <ahomescu@google.com>
2021-12-10 05:30:21 +0000

    WIP: Build libbinder on Trusty
    Bug: 224644083
    Test: build Trusty
    Change-Id: I25b97736d41489d20c2dd266e8e110764215378c

--- libs/binder/include/binder/RpcServer.h
+++ libs/binder/include/binder/RpcServer.h
@@ -29,6 +29,7 @@
 namespace android {
 
 class FdTrigger;
+class RpcServerTrusty;
 class RpcSocketAddress;
 
 /**
@@ -179,7 +180,8 @@ public:
 
     ~RpcServer();
 
-private:
+protected:
+    friend RpcServerTrusty;
     friend sp<RpcServer>;
     explicit RpcServer(std::shared_ptr<RpcTransportCtx> ctx);
 
@@ -187,8 +189,10 @@ private:
     void onSessionIncomingThreadEnded() override;
 
     static constexpr size_t kRpcAddressSize = 128;
-    static void establishConnection(sp<RpcServer>&& server, base::unique_fd clientFd,
-                                    std::array<uint8_t, kRpcAddressSize> addr, size_t addrLen);
+    static void establishConnection(
+            sp<RpcServer>&& server, base::unique_fd clientFd,
+            std::array<uint8_t, kRpcAddressSize> addr, size_t addrLen,
+            std::function<void(sp<RpcSession>&&, RpcSession::PreJoinSetupResult&&)>&& joinFn);
     [[nodiscard]] status_t setupSocketServer(const RpcSocketAddress& address);
 
     const std::shared_ptr<RpcTransportCtx> mCtx;
