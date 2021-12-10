307220c1177e861994c49b723ad218f57c5cf1b0
Andrei Homescu <ahomescu@google.com>
2021-12-10 05:30:21 +0000

    WIP: Build libbinder on Trusty
    Bug: 224644083
    Test: build Trusty
    Change-Id: I25b97736d41489d20c2dd266e8e110764215378c

--- libs/binder/include/binder/RpcSession.h
+++ libs/binder/include/binder/RpcSession.h
@@ -34,6 +34,7 @@ class RpcServer;
 class RpcSocketAddress;
 class RpcState;
 class RpcTransport;
+class RpcTrustyServer;
 class FdTrigger;
 
 constexpr uint32_t RPC_WIRE_PROTOCOL_VERSION_NEXT = 0;
@@ -189,6 +190,7 @@ private:
     friend sp<RpcSession>;
     friend RpcServer;
     friend RpcState;
+    friend RpcTrustyServer;
     explicit RpcSession(std::unique_ptr<RpcTransportCtx> ctx);
 
     // for 'target', see RpcState::sendDecStrongToTarget
