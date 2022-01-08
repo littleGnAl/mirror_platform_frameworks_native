6dfffb535c37c9823b1e87f446c54ceac2d70477
Palak Chaudhary <cryptic@google.com>
2022-01-08 15:09:09 +0000

    Android codelab change Test: manual atest
    Change-Id: I5ddba2cdc4c02c419a4aa247d9bbf62511295c61

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2097,10 +2097,6 @@ void SurfaceFlinger::onMessageRefresh() {
 
     refreshArgs.colorTransformMatrix =
             mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
-                 vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
-
-    refreshArgs.colorTransformMatrix =
-            mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
                 vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
 
     refreshArgs.devOptForceClientComposition = mDebugDisableHWC || mDebugRegion;
