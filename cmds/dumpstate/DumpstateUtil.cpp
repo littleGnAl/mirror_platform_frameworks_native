2226839d9070db0358be0ed9b47b072f24cfe54d
Tim Murray <timmurray@google.com>
2022-01-19 17:06:08 -0800

    dumpstate: improve performance
    Two changes:
    
    1. There were some seemingly-unnecessary fsyncs scattered throughout
    dumpstate. Remove those.
    
    2. Profiling revealed that a significant amount of dumpstate's CPU
    overhead was spent copying/destroying PTEs around fork/exec. Switch to
    vfork() to avoid the PTE copies.
    
    Test: bug reports work
    Bug: 215574756
    Change-Id: Ib202fd5c8d1f37548ee53bd64eaf4e114098a062

--- cmds/dumpstate/DumpstateUtil.cpp
+++ cmds/dumpstate/DumpstateUtil.cpp
@@ -232,7 +232,6 @@ int DumpFileToFd(int out_fd, const std::string& title, const std::string& path) 
             dprintf(out_fd, "*** Error dumping %s (%s): %s\n", path.c_str(), title.c_str(),
                     strerror(err));
         }
-        fsync(out_fd);
         return -1;
     }
     return DumpFileFromFdToFd(title, path, fd.get(), out_fd, PropertiesHelper::IsDryRun());
@@ -280,7 +279,6 @@ int RunCommandToFd(int fd, const std::string& title, const std::vector<std::stri
 
     if (!title.empty()) {
         dprintf(fd, "------ %s (%s) ------\n", title.c_str(), command);
-        fsync(fd);
     }
 
     const std::string& logging_message = options.LoggingMessage();
@@ -299,14 +297,13 @@ int RunCommandToFd(int fd, const std::string& title, const std::vector<std::stri
             // There is no title, but we should still print a dry-run message
             dprintf(fd, "%s: skipped on dry run\n", command_string.c_str());
         }
-        fsync(fd);
         return 0;
     }
 
     const char* path = args[0];
 
     uint64_t start = Nanotime();
-    pid_t pid = fork();
+    pid_t pid = vfork();
 
     /* handle error case */
     if (pid < 0) {
@@ -376,7 +373,6 @@ int RunCommandToFd(int fd, const std::string& title, const std::vector<std::stri
     /* handle parent case */
     int status;
     bool ret = waitpid_with_timeout(pid, options.TimeoutInMs(), &status);
-    fsync(fd);
 
     uint64_t elapsed = Nanotime() - start;
     if (!ret) {
