f46e8cbd8bf850d90c9a924d44c0b1ff0e7de173
Sam Saccone <samccone@google.com>
2022-02-04 17:37:42 +0000

    foo
    Test: manual
    
    Change-Id: I06466f474008eb129ea0eb3f4b86d1adea269500

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2126,6 +2126,8 @@ void SurfaceFlinger::onMessageRefresh() {
 
     postFrame();
     postComposition();
+    mClientColorMatrix = mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                              vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
 
     const bool prevFrameHadClientComposition = mHadClientComposition;
 
