03bb95a54b593e33a8aaad62d760dc3be78f3be0
Andrei Homescu <ahomescu@google.com>
2022-03-19 06:34:06 +0000

    libbinder: Refactor RpcTransportRaw for inheritance
    Move the declaration of RpcTransportRaw into the public
    header and refactor it for inheritance so more specialized
    transports can reuse parts of RpcTransportRaw to avoid
    code duplication.
    
    Add a new RpcTransportRawFile implementation that can be
    used with files, e.g., /dev/trusty-ipc-dev0.
    
    Bug: 224064243
    Test: atest binderRpcTest
    Change-Id: I92057f108fa1478f8ac2085b84e49a36edb00233

--- libs/binder/RpcTransportRaw.cpp
+++ libs/binder/RpcTransportRaw.cpp
@@ -26,147 +26,118 @@
 
 namespace android {
 
-namespace {
-
 // RpcTransport with TLS disabled.
-class RpcTransportRaw : public RpcTransport {
-public:
-    explicit RpcTransportRaw(android::base::unique_fd socket) : mSocket(std::move(socket)) {}
-    status_t pollRead(void) override {
-        pollfd pfd{.fd = mSocket.get(), .events = static_cast<int16_t>(POLLIN), .revents = 0};
-        ssize_t ret = TEMP_FAILURE_RETRY(::poll(&pfd, 1, 0));
-        if (ret < 0) {
+status_t RpcTransportRaw::pollRead(void) {
+    pollfd pfd{.fd = mSocket.get(), .events = static_cast<int16_t>(POLLIN), .revents = 0};
+    ssize_t ret = TEMP_FAILURE_RETRY(::poll(&pfd, 1, 0));
+    if (ret < 0) {
+        int savedErrno = errno;
+        if (savedErrno == EAGAIN || savedErrno == EWOULDBLOCK) {
+            return WOULD_BLOCK;
+        }
+
+        LOG_RPC_DETAIL("RpcTransport poll(): %s", strerror(savedErrno));
+        return -savedErrno;
+    }
+
+    if (pfd.revents & POLLNVAL) {
+        return BAD_VALUE;
+    }
+    if (pfd.revents & POLLERR) {
+        return DEAD_OBJECT;
+    }
+    if (pfd.revents & POLLHUP) {
+        return DEAD_OBJECT;
+    }
+    if (pfd.revents & POLLIN) {
+        return OK;
+    }
+
+    return WOULD_BLOCK;
+}
+
+status_t RpcTransportRaw::interruptableReadOrWrite(
+        FdTrigger* fdTrigger, iovec* iovs, int niovs,
+        const std::function<ssize_t(int, iovec*, int)>& sendOrReceiveFun, const char* funName,
+        int16_t event, const std::function<status_t()>& altPoll) {
+    MAYBE_WAIT_IN_FLAKE_MODE;
+
+    if (niovs < 0) {
+        return BAD_VALUE;
+    }
+
+    // Since we didn't poll, we need to manually check to see if it was triggered. Otherwise, we
+    // may never know we should be shutting down.
+    if (fdTrigger->isTriggered()) {
+        return DEAD_OBJECT;
+    }
+
+    // If iovs has one or more empty vectors at the end and
+    // we somehow advance past all the preceding vectors and
+    // pass some or all of the empty ones to sendmsg/recvmsg,
+    // the call will return processSize == 0. In that case
+    // we should be returning OK but instead return DEAD_OBJECT.
+    // To avoid this problem, we make sure here that the last
+    // vector at iovs[niovs - 1] has a non-zero length.
+    while (niovs > 0 && iovs[niovs - 1].iov_len == 0) {
+        niovs--;
+    }
+    if (niovs == 0) {
+        // The vectors are all empty, so we have nothing to send.
+        return OK;
+    }
+
+    bool havePolled = false;
+    while (true) {
+        ssize_t processSize = TEMP_FAILURE_RETRY(sendOrReceiveFun(mSocket.get(), iovs, niovs));
+
+        if (processSize < 0) {
             int savedErrno = errno;
-            if (savedErrno == EAGAIN || savedErrno == EWOULDBLOCK) {
-                return WOULD_BLOCK;
+
+            // Still return the error on later passes, since it would expose
+            // a problem with polling
+            if (havePolled || (savedErrno != EAGAIN && savedErrno != EWOULDBLOCK)) {
+                LOG_RPC_DETAIL("RpcTransport %s(): %s", funName, strerror(savedErrno));
+                return -savedErrno;
             }
-
-            LOG_RPC_DETAIL("RpcTransport poll(): %s", strerror(savedErrno));
-            return -savedErrno;
-        }
-
-        if (pfd.revents & POLLNVAL) {
-            return BAD_VALUE;
-        }
-        if (pfd.revents & POLLERR) {
+        } else if (processSize == 0) {
             return DEAD_OBJECT;
-        }
-        if (pfd.revents & POLLHUP) {
-            return DEAD_OBJECT;
-        }
-        if (pfd.revents & POLLIN) {
-            return OK;
-        }
-
-        return WOULD_BLOCK;
-    }
-
-    template <typename SendOrReceive>
-    status_t interruptableReadOrWrite(FdTrigger* fdTrigger, iovec* iovs, int niovs,
-                                      SendOrReceive sendOrReceiveFun, const char* funName,
-                                      int16_t event, const std::function<status_t()>& altPoll) {
-        MAYBE_WAIT_IN_FLAKE_MODE;
-
-        if (niovs < 0) {
-            return BAD_VALUE;
-        }
-
-        // Since we didn't poll, we need to manually check to see if it was triggered. Otherwise, we
-        // may never know we should be shutting down.
-        if (fdTrigger->isTriggered()) {
-            return DEAD_OBJECT;
-        }
-
-        // If iovs has one or more empty vectors at the end and
-        // we somehow advance past all the preceding vectors and
-        // pass some or all of the empty ones to sendmsg/recvmsg,
-        // the call will return processSize == 0. In that case
-        // we should be returning OK but instead return DEAD_OBJECT.
-        // To avoid this problem, we make sure here that the last
-        // vector at iovs[niovs - 1] has a non-zero length.
-        while (niovs > 0 && iovs[niovs - 1].iov_len == 0) {
-            niovs--;
-        }
-        if (niovs == 0) {
-            // The vectors are all empty, so we have nothing to send.
-            return OK;
-        }
-
-        bool havePolled = false;
-        while (true) {
-            msghdr msg{
-                    .msg_iov = iovs,
-                    // posix uses int, glibc uses size_t.  niovs is a
-                    // non-negative int and can be cast to either.
-                    .msg_iovlen = static_cast<decltype(msg.msg_iovlen)>(niovs),
-            };
-            ssize_t processSize =
-                    TEMP_FAILURE_RETRY(sendOrReceiveFun(mSocket.get(), &msg, MSG_NOSIGNAL));
-
-            if (processSize < 0) {
-                int savedErrno = errno;
-
-                // Still return the error on later passes, since it would expose
-                // a problem with polling
-                if (havePolled || (savedErrno != EAGAIN && savedErrno != EWOULDBLOCK)) {
-                    LOG_RPC_DETAIL("RpcTransport %s(): %s", funName, strerror(savedErrno));
-                    return -savedErrno;
+        } else {
+            while (processSize > 0 && niovs > 0) {
+                auto& iov = iovs[0];
+                if (static_cast<size_t>(processSize) < iov.iov_len) {
+                    // Advance the base of the current iovec
+                    iov.iov_base = reinterpret_cast<char*>(iov.iov_base) + processSize;
+                    iov.iov_len -= processSize;
+                    break;
                 }
-            } else if (processSize == 0) {
+
+                // The current iovec was fully written
+                processSize -= iov.iov_len;
+                iovs++;
+                niovs--;
+            }
+            if (niovs == 0) {
+                LOG_ALWAYS_FATAL_IF(processSize > 0,
+                                    "Reached the end of iovecs "
+                                    "with %zd bytes remaining",
+                                    processSize);
+                return OK;
+            }
+        }
+
+        if (altPoll) {
+            if (status_t status = altPoll(); status != OK) return status;
+            if (fdTrigger->isTriggered()) {
                 return DEAD_OBJECT;
-            } else {
-                while (processSize > 0 && niovs > 0) {
-                    auto& iov = iovs[0];
-                    if (static_cast<size_t>(processSize) < iov.iov_len) {
-                        // Advance the base of the current iovec
-                        iov.iov_base = reinterpret_cast<char*>(iov.iov_base) + processSize;
-                        iov.iov_len -= processSize;
-                        break;
-                    }
-
-                    // The current iovec was fully written
-                    processSize -= iov.iov_len;
-                    iovs++;
-                    niovs--;
-                }
-                if (niovs == 0) {
-                    LOG_ALWAYS_FATAL_IF(processSize > 0,
-                                        "Reached the end of iovecs "
-                                        "with %zd bytes remaining",
-                                        processSize);
-                    return OK;
-                }
             }
-
-            if (altPoll) {
-                if (status_t status = altPoll(); status != OK) return status;
-                if (fdTrigger->isTriggered()) {
-                    return DEAD_OBJECT;
-                }
-            } else {
-                if (status_t status = fdTrigger->triggerablePoll(mSocket.get(), event);
-                    status != OK)
-                    return status;
-                if (!havePolled) havePolled = true;
-            }
+        } else {
+            if (status_t status = fdTrigger->triggerablePoll(mSocket.get(), event); status != OK)
+                return status;
+            if (!havePolled) havePolled = true;
         }
     }
-
-    status_t interruptableWriteFully(FdTrigger* fdTrigger, iovec* iovs, int niovs,
-                                     const std::function<status_t()>& altPoll) override {
-        return interruptableReadOrWrite(fdTrigger, iovs, niovs, sendmsg, "sendmsg", POLLOUT,
-                                        altPoll);
-    }
-
-    status_t interruptableReadFully(FdTrigger* fdTrigger, iovec* iovs, int niovs,
-                                    const std::function<status_t()>& altPoll) override {
-        return interruptableReadOrWrite(fdTrigger, iovs, niovs, recvmsg, "recvmsg", POLLIN,
-                                        altPoll);
-    }
-
-private:
-    base::unique_fd mSocket;
-};
+}
 
 // RpcTransportCtx with TLS disabled.
 class RpcTransportCtxRaw : public RpcTransportCtx {
@@ -177,8 +148,6 @@ public:
     std::vector<uint8_t> getCertificate(RpcCertificateFormat) const override { return {}; }
 };
 
-} // namespace
-
 std::unique_ptr<RpcTransportCtx> RpcTransportCtxFactoryRaw::newServerCtx() const {
     return std::make_unique<RpcTransportCtxRaw>();
 }
@@ -187,12 +156,37 @@ std::unique_ptr<RpcTransportCtx> RpcTransportCtxFactoryRaw::newClientCtx() const
     return std::make_unique<RpcTransportCtxRaw>();
 }
 
-const char *RpcTransportCtxFactoryRaw::toCString() const {
+const char* RpcTransportCtxFactoryRaw::toCString() const {
     return "raw";
 }
 
 std::unique_ptr<RpcTransportCtxFactory> RpcTransportCtxFactoryRaw::make() {
     return std::unique_ptr<RpcTransportCtxFactoryRaw>(new RpcTransportCtxFactoryRaw());
+}
+
+// RpcTransportCtx for files.
+class RpcTransportCtxRawFile : public RpcTransportCtx {
+public:
+    std::unique_ptr<RpcTransport> newTransport(android::base::unique_fd fd, FdTrigger*) const {
+        return std::make_unique<RpcTransportRawFile>(std::move(fd));
+    }
+    std::vector<uint8_t> getCertificate(RpcCertificateFormat) const override { return {}; }
+};
+
+std::unique_ptr<RpcTransportCtx> RpcTransportCtxFactoryRawFile::newServerCtx() const {
+    return std::make_unique<RpcTransportCtxRawFile>();
+}
+
+std::unique_ptr<RpcTransportCtx> RpcTransportCtxFactoryRawFile::newClientCtx() const {
+    return std::make_unique<RpcTransportCtxRawFile>();
+}
+
+const char* RpcTransportCtxFactoryRawFile::toCString() const {
+    return "file";
+}
+
+std::unique_ptr<RpcTransportCtxFactory> RpcTransportCtxFactoryRawFile::make() {
+    return std::unique_ptr<RpcTransportCtxFactoryRawFile>(new RpcTransportCtxFactoryRawFile());
 }
 
 } // namespace android
