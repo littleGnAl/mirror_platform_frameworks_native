099c854c1ff9793835877f53771812647f228383
Andrei Homescu <ahomescu@google.com>
2022-03-19 06:34:06 +0000

    libbinder: Refactor RpcTransportRaw for inheritance
    Move the declaration of RpcTransportRaw into the public
    header and refactor it for inheritance so more specialized
    transports can reuse parts of RpcTransportRaw to avoid
    code duplication.
    
    Add a new RpcTransportRawFile implementation that can be
    used with files, e.g., /dev/trusty-ipc-dev0.
    
    Bug: 224064243
    Test: atest binderRpcTest
    Change-Id: I92057f108fa1478f8ac2085b84e49a36edb00233

--- libs/binder/include/binder/RpcTransportRaw.h
+++ libs/binder/include/binder/RpcTransportRaw.h
@@ -19,11 +19,79 @@
 
 #pragma once
 
+#include <poll.h>
 #include <memory>
 
 #include <binder/RpcTransport.h>
 
 namespace android {
+
+// RpcTransport with TLS disabled.
+class RpcTransportRaw : public RpcTransport {
+public:
+    explicit RpcTransportRaw(android::base::unique_fd socket) : mSocket(std::move(socket)) {}
+    status_t pollRead(void) override;
+
+    status_t interruptableWriteFully(FdTrigger* fdTrigger, iovec* iovs, int niovs,
+                                     const std::function<status_t()>& altPoll) override {
+        return interruptableReadOrWrite(fdTrigger, iovs, niovs, socketWriteVec, "sendmsg", POLLOUT,
+                                        altPoll);
+    }
+
+    status_t interruptableReadFully(FdTrigger* fdTrigger, iovec* iovs, int niovs,
+                                    const std::function<status_t()>& altPoll) override {
+        return interruptableReadOrWrite(fdTrigger, iovs, niovs, socketReadVec, "recvmsg", POLLIN,
+                                        altPoll);
+    }
+
+protected:
+    status_t interruptableReadOrWrite(
+            FdTrigger* fdTrigger, iovec* iovs, int niovs,
+            const std::function<ssize_t(int, iovec*, int)>& sendOrReceiveFun, const char* funName,
+            int16_t event, const std::function<status_t()>& altPoll);
+
+    // Wrapper around sendmsg with the signature of writev.
+    static ssize_t socketWriteVec(int fd, iovec* iovs, int niovs) {
+        msghdr msg{
+                .msg_iov = iovs,
+                // posix uses int, glibc uses size_t.  niovs is a
+                // non-negative int and can be cast to either.
+                .msg_iovlen = static_cast<decltype(msg.msg_iovlen)>(niovs),
+        };
+        return sendmsg(fd, &msg, MSG_NOSIGNAL);
+    }
+
+    // Wrapper around recvmsg with the signature of readv.
+    static ssize_t socketReadVec(int fd, iovec* iovs, int niovs) {
+        msghdr msg{
+                .msg_iov = iovs,
+                // posix uses int, glibc uses size_t.  niovs is a
+                // non-negative int and can be cast to either.
+                .msg_iovlen = static_cast<decltype(msg.msg_iovlen)>(niovs),
+        };
+        return recvmsg(fd, &msg, MSG_NOSIGNAL);
+    }
+
+    base::unique_fd mSocket;
+};
+
+// RpcTransportRaw for files.
+class RpcTransportRawFile : public RpcTransportRaw {
+public:
+    explicit RpcTransportRawFile(android::base::unique_fd socket) : RpcTransportRaw(std::move(socket)) {}
+
+    status_t interruptableWriteFully(FdTrigger* fdTrigger, iovec* iovs, int niovs,
+                                     const std::function<status_t()>& altPoll) override {
+        return interruptableReadOrWrite(fdTrigger, iovs, niovs, writev, "writev", POLLOUT,
+                                        altPoll);
+    }
+
+    status_t interruptableReadFully(FdTrigger* fdTrigger, iovec* iovs, int niovs,
+                                    const std::function<status_t()>& altPoll) override {
+        return interruptableReadOrWrite(fdTrigger, iovs, niovs, readv, "readv", POLLIN,
+                                        altPoll);
+    }
+};
 
 // RpcTransportCtxFactory with TLS disabled.
 class RpcTransportCtxFactoryRaw : public RpcTransportCtxFactory {
@@ -36,6 +104,18 @@ public:
 
 private:
     RpcTransportCtxFactoryRaw() = default;
+};
+
+class RpcTransportCtxFactoryRawFile : public RpcTransportCtxFactory {
+public:
+    static std::unique_ptr<RpcTransportCtxFactory> make();
+
+    std::unique_ptr<RpcTransportCtx> newServerCtx() const override;
+    std::unique_ptr<RpcTransportCtx> newClientCtx() const override;
+    const char* toCString() const override;
+
+private:
+    RpcTransportCtxFactoryRawFile() = default;
 };
 
 } // namespace android
