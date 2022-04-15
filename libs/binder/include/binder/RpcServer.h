94b2ca202780e2e2a1b4d474ebe075bb37ae43ec
Andrei Homescu <ahomescu@google.com>
2022-04-15 23:33:47 +0000

    libbinder: add RPC build option to disable the socket API
    Adds a new BINDER_RPC_NO_SOCKET_API to disable the socket
    interface in RpcSession and RpcServer on systems that do not
    support sockets, like Trusty.
    
    Test: m
    Bug: 224644083
    Change-Id: I96ae715c5720053eacd051c39df9052f89839ea5

--- libs/binder/include/binder/RpcServer.h
+++ libs/binder/include/binder/RpcServer.h
@@ -48,6 +48,7 @@ public:
     static sp<RpcServer> make(
             std::unique_ptr<RpcTransportCtxFactory> rpcTransportCtxFactory = nullptr);
 
+#ifndef BINDER_RPC_NO_SOCKET_API
     /**
      * This represents a session for responses, e.g.:
      *
@@ -79,6 +80,7 @@ public:
      */
     [[nodiscard]] status_t setupInetServer(const char* address, unsigned int port,
                                            unsigned int* assignedPort = nullptr);
+#endif
 
     /**
      * If setup*Server has been successful, return true. Otherwise return false.
@@ -180,7 +182,10 @@ private:
 
     static void establishConnection(sp<RpcServer>&& server, base::unique_fd clientFd,
                                     const sockaddr_storage addr, socklen_t addrLen);
+
+#ifndef BINDER_RPC_NO_SOCKET_API
     [[nodiscard]] status_t setupSocketServer(const RpcSocketAddress& address);
+#endif
 
     const std::unique_ptr<RpcTransportCtx> mCtx;
     size_t mMaxThreads = 1;
