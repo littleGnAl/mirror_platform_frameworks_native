c4d055952d76e504ff8310d406db59bb153c4754
Andrei Homescu <ahomescu@google.com>
2022-04-30 00:50:20 +0000

    libbinder: compile some Linux code conditionally
    Binder.cpp and Parcle.cpp contain some Linux-specific code
    that deals with scheduling and task priorities. This disables
    those headers and methods on non-Linux OSes.
    
    Bug: 224644083
    Test: m
    Change-Id: I5edf60a16ccb2d1abbc669f80b397238622112fe

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -20,7 +20,6 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
-#include <linux/sched.h>
 #include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -30,6 +29,10 @@
 #include <sys/types.h>
 #include <sys/resource.h>
 #include <unistd.h>
+
+#ifdef __linux__
+#include <linux/sched.h>
+#endif
 
 #include <binder/Binder.h>
 #include <binder/BpBinder.h>
