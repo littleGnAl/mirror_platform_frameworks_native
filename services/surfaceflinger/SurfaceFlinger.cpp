d7f48ea7abb14072c2626a5f600f33aa1d357f2f
Dan Moore <danmo@google.com>
2022-07-21 16:36:00 +0000

    Test: manual atest
    Change-Id: I9c2fc9eadd0a3f243fb9e40e61c0e04a7dcf40ad

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2129,7 +2129,7 @@ void SurfaceFlinger::onMessageRefresh() {
     postFrame();
     postComposition();
     mClientColorMatrix = mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
-                          vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
+                              vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
     updateColorMatrixLocked();
 
     const bool prevFrameHadClientComposition = mHadClientComposition;
