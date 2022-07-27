143b91cda382ff8824e1003e44953282e7f42040
Devin Moore <devinmoore@google.com>
2022-07-27 15:54:06 +0000

    Add lookupOrCreateWeak to ObjectManager
    This looks for an attached object on the binder, attempts to promote it,
    and returns the object if it could be promoted.
    Otherwise, it will create a new object and attach it to the binder.
    
    Test: atest binderUnitTest aidl_integration_test
    Bug: 220141324
    Change-Id: I2c8baa211300fa3ada50f2b2c93862f8c968be26

--- libs/binder/BpBinder.cpp
+++ libs/binder/BpBinder.cpp
@@ -100,6 +100,44 @@ void* BpBinder::ObjectManager::detach(const void* objectID) {
     return value;
 }
 
+namespace {
+struct Tag {
+    wp<IBinder> binder;
+};
+} // namespace
+
+static void cleanWeak(const void* /* id */, void* obj, void* /* cookie */) {
+    delete static_cast<Tag*>(obj);
+}
+
+sp<IBinder> BpBinder::ObjectManager::lookupOrCreateWeak(const void* objectID, object_make_func make,
+                                                        const void* makeArgs) {
+    auto iter = mObjects.find(objectID);
+    if (iter != mObjects.end()) {
+        if (auto attached = static_cast<Tag*>(iter->second.object)->binder.promote()) {
+            return attached;
+        }
+    }
+
+    sp<IBinder> newObj = make(makeArgs);
+
+    if (iter != mObjects.end()) {
+        // reuse existing tag that was unable to be promoted
+        static_cast<Tag*>(iter->second.object)->binder = newObj;
+    } else {
+        auto tag = new Tag;
+        LOG_ALWAYS_FATAL_IF(!tag, "no more memory");
+
+        tag->binder = newObj;
+        entry_t e({tag, nullptr, cleanWeak});
+
+        auto res = mObjects.insert({objectID, e});
+        LOG_ALWAYS_FATAL_IF(res.second == false, "Unexpected attached object.");
+    }
+
+    return newObj;
+}
+
 void BpBinder::ObjectManager::kill()
 {
     const size_t N = mObjects.size();
@@ -514,6 +552,12 @@ void* BpBinder::detachObject(const void* objectID) {
 void BpBinder::withLock(const std::function<void()>& doWithLock) {
     AutoMutex _l(mLock);
     doWithLock();
+}
+
+sp<IBinder> BpBinder::lookupOrCreateWeak(const void* objectID, object_make_func make,
+                                         const void* makeArgs) {
+    AutoMutex _l(mLock);
+    return mObjects.lookupOrCreateWeak(objectID, make, makeArgs);
 }
 
 BpBinder* BpBinder::remoteBinder()
