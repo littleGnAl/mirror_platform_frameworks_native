d9a33ea0561da1b47ebc06aec855d9bceb1b4a26
Andrei Homescu <ahomescu@google.com>
2022-08-02 01:25:15 +0000

    libbinder: Add Trusty IPC support to binderRpcTest
    Build a new binderRpcTestTrusty binary on
    qemu_trusty_arm64 that runs a subset of the
    binderRpcTest tests against a Trusty version of
    binderRpcTestService.
    
    Bug: 230135749
    Test: atest binderRpcTestTrusty on qemu_trusty_arm64
    Change-Id: I10c77bab586e6f6a44117c3b376d591ed5e0b1fb

--- libs/binder/tests/binderRpcTestCommon.h
+++ libs/binder/tests/binderRpcTestCommon.h
@@ -77,6 +77,7 @@ enum class SocketType {
     UNIX_RAW,
     VSOCK,
     INET,
+    TIPC,
 };
 
 static inline std::string PrintToString(SocketType socketType) {
@@ -93,6 +94,8 @@ static inline std::string PrintToString(SocketType socketType) {
             return "vm_socket";
         case SocketType::INET:
             return "inet_socket";
+        case SocketType::TIPC:
+            return "trusty_ipc";
         default:
             LOG_ALWAYS_FATAL("Unknown socket type");
             return "";
