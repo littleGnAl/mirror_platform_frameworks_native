dc6e3a396de792c6b508e6ac545029354f8cbc9b
Andrei Homescu <ahomescu@google.com>
2022-08-02 04:38:30 +0000

    libbinder: Add binderRpcTestService for Trusty
    Add Trusty code and build files for binderRpcTestService
    so we can run the binderRpcTest tests on Android against
    a Trusty service.
    
    Bug: 230135749
    Test: build Trusty
    Change-Id: Id4dbe453229c1f1b93da2811cec265c0e331edfe

---
+++ libs/binder/trusty/binderRpcTestService/rules.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+LIBBINDER_TESTS_DIR := frameworks/native/libs/binder/tests
+
+MANIFEST := $(LOCAL_DIR)/manifest.json
+
+MODULE_SRCS := \
+	$(LOCAL_DIR)/binderRpcTestService.cpp \
+	$(LIBBINDER_TESTS_DIR)/binderRpcTestCommon.cpp \
+
+MODULE_INCLUDES := \
+	$(LIBBINDER_TESTS_DIR) \
+
+MODULE_LIBRARY_DEPS := \
+	trusty/user/base/lib/libc-trusty \
+	trusty/user/base/lib/libstdc++-trusty \
+	trusty/user/base/lib/tipc \
+	frameworks/native/libs/binder/trusty \
+	$(LOCAL_DIR)/aidl \
+
+include make/trusted_app.mk
