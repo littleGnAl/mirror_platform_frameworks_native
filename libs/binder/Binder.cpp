4ee9af37d90d0507a9049aeac2428c835d152450
Ayrton Munoz <ayrton@google.com>
2022-08-03 17:13:25 -0400

    WIP: libbinder: conditionally compile some code
    This conditionally avoids compiling some code when
    `__TRUSTY__` is defined due to trusty's limited support for
    file descriptors in binder.
    
    Bug: 230134581
    Test: build Trusty
    Change-Id: I60e8ddb0e0a1cff3c3c4b05f743caf469ff236a5

--- libs/binder/Binder.cpp
+++ libs/binder/Binder.cpp
@@ -168,6 +168,7 @@ status_t IBinder::getDebugPid(pid_t* out) {
     return OK;
 }
 
+#if !defined(__TRUSTY__)
 status_t IBinder::setRpcClientDebug(android::base::unique_fd socketFd,
                                     const sp<IBinder>& keepAliveBinder) {
     if (!kEnableRpcDevServers) {
@@ -199,6 +200,7 @@ status_t IBinder::setRpcClientDebug(android::base::unique_fd socketFd,
     if (status = data.writeStrongBinder(keepAliveBinder); status != OK) return status;
     return transact(SET_RPC_CLIENT_TRANSACTION, data, &reply);
 }
+#endif // !defined(__TRUSTY__)
 
 void IBinder::withLock(const std::function<void()>& doWithLock) {
     BBinder* local = localBinder();
@@ -651,6 +653,7 @@ status_t BBinder::setRpcClientDebug(const Parcel& data) {
     return setRpcClientDebug(std::move(clientFd), keepAliveBinder);
 }
 
+#if !defined(__TRUSTY__)
 status_t BBinder::setRpcClientDebug(android::base::unique_fd socketFd,
                                     const sp<IBinder>& keepAliveBinder) {
     if (!kEnableRpcDevServers) {
@@ -708,6 +711,7 @@ status_t BBinder::setRpcClientDebug(android::base::unique_fd socketFd,
     LOG_RPC_DETAIL("%s(fd=%d) successful", __PRETTY_FUNCTION__, socketFdForPrint);
     return OK;
 }
+#endif // !defined(__TRUSTY__)
 
 void BBinder::removeRpcServerLink(const sp<RpcServerLink>& link) {
     Extras* e = mExtras.load(std::memory_order_acquire);
@@ -753,6 +757,7 @@ status_t BBinder::onTransact(
             reply->writeString16(getInterfaceDescriptor());
             return NO_ERROR;
 
+#if !defined(__TRUSTY__)
         case DUMP_TRANSACTION: {
             int fd = data.readFileDescriptor();
             int argc = data.readInt32();
@@ -790,6 +795,7 @@ status_t BBinder::onTransact(
 
             return NO_ERROR;
         }
+#endif // !defined(__TRUSTY__)
 
         case SYSPROPS_TRANSACTION: {
             report_sysprop_change();
