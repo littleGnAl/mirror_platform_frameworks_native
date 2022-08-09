e22bb37a9eec533afc0761b70b780ef4dc952fd4
Shunkai Yao <yaoshunkai@google.com>
2022-08-09 19:13:00 +0000

    Add copy assignment operator for ParcelableHolder
    Bug: b/241907959
    Test: atest CtsNdkBinderTestCases
    Change-Id: I593f6fe5c50f2a2cc71651fb87766568dffcfc6d

--- libs/binder/ndk/include_cpp/android/binder_parcelable_utils.h
+++ libs/binder/ndk/include_cpp/android/binder_parcelable_utils.h
@@ -201,6 +201,22 @@ class AParcelableHolder {
     inline bool operator==(const AParcelableHolder& rhs) const { return this == &rhs; }
     inline bool operator>(const AParcelableHolder& rhs) const { return this > &rhs; }
     inline bool operator>=(const AParcelableHolder& rhs) const { return this >= &rhs; }
+#if __ANDROID_API__ >= 31
+    inline AParcelableHolder& operator=(const AParcelableHolder& rhs) {
+        // AParcelableHolder has been introduced in 31.
+#ifdef __ANDROID_UNAVAILABLE_SYMBOLS_ARE_WEAK__
+        if (__builtin_available(android 31, *)) {
+#else
+        if (__ANDROID_API__ >= 31) {
+#endif
+            this->reset();
+            this->mStability = rhs.mStability;
+            AParcel_appendFrom(rhs.mParcel.get(), this->mParcel.get(), 0,
+                               AParcel_getDataSize(rhs.mParcel.get()));
+        }
+        return *this;
+    }
+#endif
 
    private:
     mutable ndk::ScopedAParcel mParcel;
