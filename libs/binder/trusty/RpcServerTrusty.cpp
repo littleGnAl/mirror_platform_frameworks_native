7a011e0634d36e16e67003c95c36245582074c47
Andrei Homescu <ahomescu@google.com>
2022-08-17 04:59:01 +0000

    libbinder: add ancillaryFd support in RpcTransportTipcTrusty
    TBD
    
    Bug: 242940548
    Test: Trusty tests
    Change-Id: Ic5602bbf9f239f65489e0f411f89b10907741be3

--- libs/binder/trusty/RpcServerTrusty.cpp
+++ libs/binder/trusty/RpcServerTrusty.cpp
@@ -89,6 +89,10 @@ RpcServerTrusty::RpcServerTrusty(std::unique_ptr<RpcTransportCtx> ctx, std::stri
     } else {
         mTipcPort.acl = nullptr;
     }
+
+    mRpcServer->setSupportedFileDescriptorTransportModes(
+            {RpcSession::FileDescriptorTransportMode::NONE,
+             RpcSession::FileDescriptorTransportMode::TRUSTY});
 }
 
 int RpcServerTrusty::handleConnect(const tipc_port* port, handle_t chan, const uuid* peer,
