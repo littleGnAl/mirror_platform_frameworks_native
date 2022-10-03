41b384b02a3c3758a8d8bc89e55343a37ee607a3
David Brazdil <dbrazdil@google.com>
2022-10-03 16:29:27 +0100

    Create Rust bindings for UnixBootstrap RpcBinder
    Add bindings for running a UnixDomainSocketBootstrap server/client to
    libbinder_rpc_unstable and the librpcbinder Rust wrapper.
    
    Bug: 250685929
    Test: builds
    Change-Id: I6a38f3fb055401dfacdd67c843b22f3738938a20

--- libs/binder/rust/rpcbinder/src/client.rs
+++ libs/binder/rust/rpcbinder/src/client.rs
@@ -17,7 +17,7 @@
 use binder::{unstable_api::new_spibinder, FromIBinder, SpIBinder, StatusCode, Strong};
 use std::os::{
     raw::{c_int, c_void},
-    unix::io::RawFd,
+    unix::io::{IntoRawFd, OwnedFd, RawFd},
 };
 
 /// Connects to an RPC Binder server over vsock.
@@ -55,6 +55,19 @@ pub fn get_preconnected_rpc_service(
     }
 }
 
+/// Connects to an RPC Binder server, using the given Unix bootstrap socket.
+/// It takes ownership of the socket file descriptor.
+pub fn get_unix_bootstrap_rpc_service(bootstrap_fd: OwnedFd) -> Option<SpIBinder> {
+    // SAFETY: AIBinder returned by RpcUnixBootstrapClient has correct reference
+    // count, and the ownership can be safely taken by new_spibinder.
+    // RpcUnixBootstrapClient takes ownership of bootstrap_fd.
+    unsafe {
+        new_spibinder(binder_rpc_unstable_bindgen::RpcUnixBootstrapClient(
+            bootstrap_fd.into_raw_fd(),
+        ))
+    }
+}
+
 type RequestFd<'a> = &'a mut dyn FnMut() -> Option<RawFd>;
 
 unsafe extern "C" fn request_fd_wrapper(param: *mut c_void) -> c_int {
@@ -76,6 +89,14 @@ pub fn get_preconnected_rpc_interface<T: FromIBinder + ?Sized>(
     request_fd: impl FnMut() -> Option<RawFd>,
 ) -> Result<Strong<T>, StatusCode> {
     interface_cast(get_preconnected_rpc_service(request_fd))
+}
+
+/// Connects to an RPC Binder server for a particular interface, using the given
+/// Unix bootstrap socket. It takes ownership of the socket file descriptor.
+pub fn get_unix_bootstrap_rpc_interface<T: FromIBinder + ?Sized>(
+    bootstrap_fd: OwnedFd,
+) -> Result<Strong<T>, StatusCode> {
+    interface_cast(get_unix_bootstrap_rpc_service(bootstrap_fd))
 }
 
 fn interface_cast<T: FromIBinder + ?Sized>(
