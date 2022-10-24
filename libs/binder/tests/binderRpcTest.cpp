03146dc01a00058e17b7541c6216731eddabe8dd
Alice Wang <aliceywang@google.com>
2022-10-24 10:44:09 +0000

    [rpc_binder] Add rust API to set up RPC binder over named Unix socket
    This cl adds the rust API to set up RPC binder over named Unix sockets.
    A previous cl aosp/2229557 sets up the binder over anonymous Unix sockets.
    
    Test: atest MicrodroidTests ComposHostTestCases
    Bug: 222479468
    Change-Id: I0c7c38f4792c4536f5f88eb7e035091505f782f7

--- libs/binder/tests/binderRpcTest.cpp
+++ libs/binder/tests/binderRpcTest.cpp
@@ -406,6 +406,7 @@ public:
                         return connectTo(UnixSocketAddress(serverConfig.addr.c_str()));
                     });
                     break;
+                case SocketType::UNIX_INIT:
                 case SocketType::UNIX:
                     status = session->setupUnixDomainClient(serverConfig.addr.c_str());
                     break;
@@ -480,7 +481,7 @@ TEST_P(BinderRpc, SeparateRootObject) {
 
     SocketType type = std::get<0>(GetParam());
     if (type == SocketType::PRECONNECTED || type == SocketType::UNIX ||
-        type == SocketType::UNIX_BOOTSTRAP) {
+        type == SocketType::UNIX_BOOTSTRAP || type == SocketType::UNIX_INIT) {
         // we can't get port numbers for unix sockets
         return;
     }
@@ -1616,7 +1617,8 @@ static bool testSupportVsockLoopback() {
 }
 
 static std::vector<SocketType> testSocketTypes(bool hasPreconnected = true) {
-    std::vector<SocketType> ret = {SocketType::UNIX, SocketType::UNIX_BOOTSTRAP, SocketType::INET};
+    std::vector<SocketType> ret = {SocketType::UNIX, SocketType::UNIX_BOOTSTRAP, SocketType::INET,
+                                   SocketType::UNIX_INIT};
 
     if (hasPreconnected) ret.push_back(SocketType::PRECONNECTED);
 
@@ -1857,6 +1859,29 @@ public:
                     mBootstrapSocket = RpcTransportFd(std::move(bootstrapFdClient));
                     mAcceptConnection = &Server::recvmsgServerConnection;
                     mConnectToServer = [this] { return connectToUnixBootstrap(mBootstrapSocket); };
+                } break;
+                case SocketType::UNIX_INIT: {
+                    auto addr = allocateSocketAddress();
+                    auto socket_addr = UnixSocketAddress(addr.c_str());
+                    auto fd = base::unique_fd(TEMP_FAILURE_RETRY(
+                            socket(socket_addr.addr()->sa_family,
+                                   SOCK_STREAM | SOCK_CLOEXEC | SOCK_NONBLOCK, 0)));
+                    if (fd.get() < 0) {
+                        return AssertionFailure() << "setupInitSocketServer: cannot create socket";
+                    }
+                    if (0 !=
+                        TEMP_FAILURE_RETRY(
+                                bind(fd.get(), socket_addr.addr(), socket_addr.addrSize()))) {
+                        return AssertionFailure() << "setupInitSocketServer: cannot bind socket";
+                    }
+                    auto status = rpcServer->setupInitSocketServer(std::move(fd));
+                    if (status != OK) {
+                        return AssertionFailure()
+                                << "setupInitSocketServer: " << statusToString(status);
+                    }
+                    mConnectToServer = [addr] {
+                        return connectTo(UnixSocketAddress(addr.c_str()));
+                    };
                 } break;
                 case SocketType::VSOCK: {
                     auto port = allocateVsockPort();
