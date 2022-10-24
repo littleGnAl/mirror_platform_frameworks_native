cd1deeecbb5faae39a53f0f50b8a96e4c8343d71
Alice Wang <aliceywang@google.com>
2022-10-24 10:44:09 +0000

    [rpc_binder] Add rust API to set up RPC binder over named Unix socket
    This cl adds the rust API to set up RPC binder over named Unix sockets.
    A previous cl aosp/2229557 sets up the binder over anonymous Unix sockets.
    
    Test: atest MicrodroidTests ComposHostTestCases
    Bug: 222479468
    Change-Id: I0c7c38f4792c4536f5f88eb7e035091505f782f7

--- libs/binder/rust/rpcbinder/src/client.rs
+++ libs/binder/rust/rpcbinder/src/client.rs
@@ -15,10 +15,12 @@
  */
 
 use binder::{unstable_api::new_spibinder, FromIBinder, SpIBinder, StatusCode, Strong};
+use std::ffi::CString;
 use std::os::{
     raw::{c_int, c_void},
     unix::io::RawFd,
 };
+use std::path::Path;
 
 /// Connects to an RPC Binder server over vsock.
 pub fn get_vsock_rpc_service(cid: u32, port: u32) -> Option<SpIBinder> {
@@ -33,6 +35,28 @@ pub fn get_vsock_rpc_interface<T: FromIBinder + ?Sized>(
     port: u32,
 ) -> Result<Strong<T>, StatusCode> {
     interface_cast(get_vsock_rpc_service(cid, port))
+}
+
+/// Connects to an RPC Binder server over Unix domain socket.
+pub fn get_unix_domain_rpc_service<P: AsRef<Path>>(socket_path: P) -> Option<SpIBinder> {
+    let socket_path = socket_path.as_ref();
+    let socket_path = match socket_path.to_str().and_then(|p| CString::new(p).ok()) {
+        Some(path) => path,
+        None => {
+            log::error!("Cannot convert {:?} to CString", socket_path);
+            return None;
+        }
+    };
+    // SAFETY: AIBinder returned by UnixDomainRpcClient has correct reference count,
+    // and the ownership can safely be taken by new_spibinder.
+    unsafe { new_spibinder(binder_rpc_unstable_bindgen::UnixDomainRpcClient(socket_path.as_ptr())) }
+}
+
+/// Connects to an RPC Binder server for a particular interface over Unix domain socket.
+pub fn get_unix_domain_rpc_interface<T: FromIBinder + ?Sized, P: AsRef<Path>>(
+    socket_path: P,
+) -> Result<Strong<T>, StatusCode> {
+    interface_cast(get_unix_domain_rpc_service(socket_path))
 }
 
 /// Connects to an RPC Binder server, using the given callback to get (and take ownership of)
