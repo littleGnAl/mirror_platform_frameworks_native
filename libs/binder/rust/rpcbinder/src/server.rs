ed584c8de879222bf68443fcf164d72490e7893c
David Brazdil <dbrazdil@google.com>
2022-11-16 11:47:00 +0000

    rpc: Refactor bindings to give more control over server lifetime
    The RpcServer C/Rust bindings have been written such that the calling
    thread blocks until the server has returned. This is technically not
    necessary because the server runs in a different thread anyway, and it
    required a callback function to notify the caller that the server is
    ready. This construct complicated the caller code.
    
    Another limitation of this construct is that it is impossible for the
    caller to shutdown the server when it's not needed anymore. The only way
    of killing it is to kill either the thread or the process, neither of
    which does proper cleanup.
    
    To address this, refactor the C bindings to return an opaque handle for
    the created RpcServer, and only destroy the object when
    ShutdownRpcServer(handle) is called. The caller can also invoke
    JoinRpcServer(handle) to join the server thread.
    
    The Rust bindings are modified accordingly - the handle is wrapped in
    a RpcServer struct.
    
    RunVsockRpcServerWithFactory and the corresponding Rust wrapper are left
    unchanged. The lifetime of the factory reference is quite complex and
    the only user of this function will be removed soon. When that happens,
    we will remove RunVsockRpcServerWithFactory as well.
    
    Test: atest -p packages/modules/Virtualization:avf-presubmit
    Change-Id: I98fa0a97eb169de95ef8649c43f37a9c7c4cd96e

--- libs/binder/rust/rpcbinder/src/server.rs
+++ libs/binder/rust/rpcbinder/src/server.rs
@@ -18,114 +18,72 @@ use binder::{
     unstable_api::{AIBinder, AsNative},
     SpIBinder,
 };
+use binder_rpc_unstable_bindgen::RpcServerHandle;
+use std::io::{Error, ErrorKind};
+use std::sync::Mutex;
 use std::{ffi::CString, os::raw, ptr::null_mut};
 
-/// Runs a binder RPC server, serving the supplied binder service implementation on the given vsock
-/// port.
-///
-/// If and when the server is ready for connections (it is listening on the port), `on_ready` is
-/// called to allow appropriate action to be taken - e.g. to notify clients that they may now
-/// attempt to connect.
-///
-/// The current thread is joined to the binder thread pool to handle incoming messages.
-///
-/// Returns true if the server has shutdown normally, false if it failed in some way.
-pub fn run_vsock_rpc_server<F>(service: SpIBinder, port: u32, on_ready: F) -> bool
-where
-    F: FnOnce(),
-{
-    let mut ready_notifier = ReadyNotifier(Some(on_ready));
-    ready_notifier.run_vsock_server(service, port)
+/// Represents a running RpcServer instance.
+#[derive(Debug)]
+pub struct RpcServer {
+    handle: Mutex<RpcServerHandle>,
 }
 
-/// Runs a binder RPC server, serving the supplied binder service implementation on the given
-/// socket file name. The socket should be initialized in init.rc with the same name.
-///
-/// If and when the server is ready for connections, `on_ready` is called to allow appropriate
-/// action to be taken - e.g. to notify clients that they may now attempt to connect.
-///
-/// The current thread is joined to the binder thread pool to handle incoming messages.
-///
-/// Returns true if the server has shutdown normally, false if it failed in some way.
-pub fn run_init_unix_domain_rpc_server<F>(
-    service: SpIBinder,
-    socket_name: &str,
-    on_ready: F,
-) -> bool
-where
-    F: FnOnce(),
-{
-    let mut ready_notifier = ReadyNotifier(Some(on_ready));
-    ready_notifier.run_init_unix_domain_server(service, socket_name)
-}
+impl RpcServer {
+    fn from_handle(handle: RpcServerHandle) -> Result<RpcServer, Error> {
+        if handle == RpcServerHandle::MAX {
+            return Err(Error::new(ErrorKind::Other, "Failed to start server"));
+        }
+        Ok(RpcServer { handle: Mutex::new(handle) })
+    }
 
-struct ReadyNotifier<F>(Option<F>)
-where
-    F: FnOnce();
-
-impl<F> ReadyNotifier<F>
-where
-    F: FnOnce(),
-{
-    fn run_vsock_server(&mut self, mut service: SpIBinder, port: u32) -> bool {
+    /// Creates a binder RPC server, serving the supplied binder service implementation on the given
+    /// vsock port.
+    pub fn new_vsock(mut service: SpIBinder, port: u32) -> Result<RpcServer, Error> {
         let service = service.as_native_mut();
-        let param = self.as_void_ptr();
 
         // SAFETY: Service ownership is transferring to the server and won't be valid afterward.
         // Plus the binder objects are threadsafe.
-        // RunVsockRpcServerCallback does not retain a reference to `ready_callback` or `param`; it only
-        // uses them before it returns, which is during the lifetime of `self`.
-        unsafe {
-            binder_rpc_unstable_bindgen::RunVsockRpcServerCallback(
-                service,
-                port,
-                Some(Self::ready_callback),
-                param,
-            )
-        }
+        let handle = unsafe { binder_rpc_unstable_bindgen::VsockRpcServer(service, port) };
+        Self::from_handle(handle)
     }
 
-    fn run_init_unix_domain_server(&mut self, mut service: SpIBinder, socket_name: &str) -> bool {
+    /// Creates a binder RPC server, serving the supplied binder service implementation on the given
+    /// socket file name. The socket should be initialized in init.rc with the same name.
+    pub fn new_init_unix_domain(
+        mut service: SpIBinder,
+        socket_name: &str,
+    ) -> Result<RpcServer, Error> {
         let socket_name = match CString::new(socket_name) {
             Ok(s) => s,
             Err(e) => {
                 log::error!("Cannot convert {} to CString. Error: {:?}", socket_name, e);
-                return false;
+                return Err(Error::from(ErrorKind::InvalidInput));
             }
         };
         let service = service.as_native_mut();
-        let param = self.as_void_ptr();
 
         // SAFETY: Service ownership is transferring to the server and won't be valid afterward.
         // Plus the binder objects are threadsafe.
         // RunInitUnixDomainRpcServer does not retain a reference to `ready_callback` or `param`;
         // it only uses them before it returns, which is during the lifetime of `self`.
-        unsafe {
-            binder_rpc_unstable_bindgen::RunInitUnixDomainRpcServer(
-                service,
-                socket_name.as_ptr(),
-                Some(Self::ready_callback),
-                param,
-            )
-        }
+        let handle = unsafe {
+            binder_rpc_unstable_bindgen::InitUnixDomainRpcServer(service, socket_name.as_ptr())
+        };
+        Self::from_handle(handle)
     }
 
-    fn as_void_ptr(&mut self) -> *mut raw::c_void {
-        self as *mut _ as *mut raw::c_void
+    /// Joins the RpcServer thread.
+    pub fn join(&mut self) {
+        let handle = self.handle.lock().unwrap();
+        unsafe { binder_rpc_unstable_bindgen::JoinRpcServer(*handle) };
     }
+}
 
-    unsafe extern "C" fn ready_callback(param: *mut raw::c_void) {
-        // SAFETY: This is only ever called by `RunVsockRpcServerCallback`, within the lifetime of the
-        // `ReadyNotifier`, with `param` taking the value returned by `as_void_ptr` (so a properly
-        // aligned non-null pointer to an initialized instance).
-        let ready_notifier = param as *mut Self;
-        ready_notifier.as_mut().unwrap().notify()
-    }
-
-    fn notify(&mut self) {
-        if let Some(on_ready) = self.0.take() {
-            on_ready();
-        }
+impl Drop for RpcServer {
+    fn drop(&mut self) {
+        let handle = self.handle.lock().unwrap();
+        unsafe { binder_rpc_unstable_bindgen::ShutdownRpcServer(*handle) };
     }
 }
 
