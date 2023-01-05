9ca37d797a9083379615a22c39fb59865b74fc8e
Pawan Wagh <waghpawan@google.com>
2023-01-05 22:13:46 +0000

    Fuzz AParcel_marshal and AParcel_unmarshal APIs
    Adding both APIs to binder_parcel_fuzzer
    
    Test: m binder_parcel_fuzzer &&
    	out/host/linux-x86/fuzz/x86_64/binder_parcel_fuzzer/binder_parcel_fuzzer
    Bug: 264550130
    Change-Id: I107525f168c5c446adc758498905c1b4ceaaff9b

--- libs/binder/tests/parcel_fuzzer/binder_ndk.cpp
+++ libs/binder/tests/parcel_fuzzer/binder_ndk.cpp
@@ -199,5 +199,22 @@ std::vector<ParcelRead<NdkParcelAdapter>> BINDER_NDK_PARCEL_READ_FUNCTIONS{
             binder_status_t status = genericDataParcelable.readFromParcel(p.aParcel());
             FUZZ_LOG() << "status: " << status;
         },
+        [](const NdkParcelAdapter& p, FuzzedDataProvider& provider) {
+            FUZZ_LOG() << "about to marshal AParcel";
+            size_t start = provider.ConsumeIntegral<size_t>();
+            size_t len = provider.ConsumeIntegral<size_t>();
+            uint8_t buffer[len];
+            binder_status_t status = AParcel_marshal(p.aParcel(), buffer, start, len);
+            FUZZ_LOG() << "status: " << status;
+        },
+        [](const NdkParcelAdapter& /*p*/, FuzzedDataProvider& provider) {
+            FUZZ_LOG() << "about to unmarshal AParcel";
+            size_t len = provider.ConsumeIntegralInRange<size_t>(0, provider.remaining_bytes());
+            std::vector<uint8_t> parcelData = provider.ConsumeBytes<uint8_t>(len);
+            const uint8_t* buffer = parcelData.data();
+            binder_status_t status = AParcel_unmarshal(AParcel_create(), buffer, len);
+            FUZZ_LOG() << "status: " << status;
+        },
+
 };
 // clang-format on
