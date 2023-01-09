2b0ff6bf62dcc6aa37f5775e5947e447ad9a425f
Pawan Wagh <waghpawan@google.com>
2023-01-09 23:43:06 +0000

    Check for null buffer in AParcel_unmarshal
    Test: m binder_parcel_fuzzer &&
    out/host/linux-x86/fuzz/x86_64/binder_parcel_fuzzer/binder_parcel_fuzzer
    Bug: 264746958
    
    Change-Id: I379d04b0e25d5f9f475352abb9a078542cb0eb5d

--- libs/binder/ndk/parcel.cpp
+++ libs/binder/ndk/parcel.cpp
@@ -715,6 +715,11 @@ binder_status_t AParcel_unmarshal(AParcel* parcel, const uint8_t* buffer, size_t
     if (raw == nullptr) {
         return STATUS_NO_MEMORY;
     }
+
+    if (buffer == nullptr) {
+        return STATUS_UNEXPECTED_NULL;
+    }
+
     memcpy(raw, buffer, len);
     return STATUS_OK;
 }
