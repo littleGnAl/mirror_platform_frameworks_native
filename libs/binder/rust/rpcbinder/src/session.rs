272e2bec92ec46d1ec54d5969a231ac4bbebbe2e
Steven Moreland <smoreland@google.com>
2023-03-01 01:25:33 +0000

    RPC Binder: setMaxOutgoing{Threads,Connections}
    This API is updated to emphasize that it is placing a limit
    on the number of connections that will be started corresponding
    to threads in a remote pool. These connections won't actually
    correspond to threads in the process that this API is called.
    
    Documentation has also been updated to clarify a few questions
    from an internal discussion forum.
    
    Fixes: 270374393
    Test: binderRpcTest
    Change-Id: Ia0d9f0d0f42f58a2c63bf506476b33985f091a34

--- libs/binder/rust/rpcbinder/src/session.rs
+++ libs/binder/rust/rpcbinder/src/session.rs
@@ -76,10 +76,13 @@ impl RpcSessionRef {
     }
 
     /// Sets the maximum number of outgoing threads.
-    pub fn set_max_outgoing_threads(&self, threads: usize) {
+    pub fn set_max_outgoing_connections(&self, threads: usize) {
         // SAFETY - Only passes the 'self' pointer as an opaque handle.
         unsafe {
-            binder_rpc_unstable_bindgen::ARpcSession_setMaxOutgoingThreads(self.as_ptr(), threads)
+            binder_rpc_unstable_bindgen::ARpcSession_setMaxOutgoingConnections(
+                self.as_ptr(),
+                threads,
+            )
         };
     }
 
