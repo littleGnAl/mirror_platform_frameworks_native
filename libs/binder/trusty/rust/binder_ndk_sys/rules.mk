4fa6717c4027b70b190d0279d69ae656789cfcb1
Andrei Homescu <ahomescu@google.com>
2023-05-08 23:44:58 +0000

    libbinder_rs: Build libbinder_ndk_sys on Trusty
    Adds a rules.mk build file to build the C wrapper
    crate on Trusty with bindgen.
    
    Bug: 242243245
    Test: build binder_ndk_sys as a dependency of skel_rust
    Change-Id: I54af010a49763bda785230261d56485ffd15e98f

---
+++ libs/binder/trusty/rust/binder_ndk_sys/rules.mk
@@ -0,0 +1,45 @@
+# Copyright (C) 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+BINDER_RUST_DIR := frameworks/native/libs/binder/rust
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS := $(BINDER_RUST_DIR)/sys/lib.rs
+
+MODULE_CRATE_NAME := binder_ndk_sys
+
+MODULE_LIBRARY_DEPS += \
+	frameworks/native/libs/binder/trusty \
+	frameworks/native/libs/binder/trusty/ndk \
+	trusty/user/base/lib/trusty-sys \
+
+MODULE_BINDGEN_SRC_HEADER := $(BINDER_RUST_DIR)/sys/BinderBindings.hpp
+
+MODULE_BINDGEN_FLAGS += \
+	--default-enum-style="rust_non_exhaustive" \
+	--constified-enum="android::c_interface::consts::.*" \
+	--allowlist-type="android::c_interface::.*" \
+	--allowlist-type="AStatus" \
+	--allowlist-type="AIBinder_Class" \
+	--allowlist-type="AIBinder" \
+	--allowlist-type="AIBinder_Weak" \
+	--allowlist-type="AIBinder_DeathRecipient" \
+	--allowlist-type="AParcel" \
+	--allowlist-type="binder_status_t" \
+	--allowlist-function=".*" \
+
+include make/library.mk
