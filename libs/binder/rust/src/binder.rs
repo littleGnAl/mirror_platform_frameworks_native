4c888b5b5a894b776afefa0d557990213066f642
Andrei Homescu <ahomescu@google.com>
2023-05-09 02:50:37 +0000

    libbinder_rs: Build on Trusty
    Add rules.mk build file for Trusty and make some
    small changes to libbinder_rs so it builds:
    * Disable some OS-specific code related to file descriptors
    * Provide separate implementation of on_dump
    * Disable ProcessState and ThreadState permanently
    * Add separate ParcelFileDescriptor for Trusty
      (currently unimplemented)
    
    Bug: 242243245
    Test: build on Trusty
    Change-Id: I8110be2397322acbc6a971f246b074c8811592bf

--- libs/binder/rust/src/binder.rs
+++ libs/binder/rust/src/binder.rs
@@ -29,8 +29,9 @@ use std::fmt;
 use std::fs::File;
 use std::marker::PhantomData;
 use std::ops::Deref;
+#[cfg(not(target_os = "trusty"))]
+use std::os::fd::AsRawFd;
 use std::os::raw::c_char;
-use std::os::unix::io::AsRawFd;
 use std::ptr;
 
 /// Binder action to perform.
@@ -197,6 +198,7 @@ pub trait IBinderInternal: IBinder {
     fn set_requesting_sid(&mut self, enable: bool);
 
     /// Dump this object to the given file handle
+    #[cfg(not(target_os = "trusty"))]
     fn dump<F: AsRawFd>(&mut self, fp: &F, args: &[&str]) -> Result<()>;
 
     /// Get a new interface that exposes additional extension functionality, if
