af4e74a2581a31a5457aa6445174a4797e3587d4
Andrei Homescu <ahomescu@google.com>
2023-05-10 00:37:52 +0000

    binderRpcTest: Build on Trusty
    Add rules.mk to build binderRpcTest in Rust on Trusty.
    
    Bug: 242243245
    Test: build binderRpcTest on Trusty
    Change-Id: I66262b5885db52ef7adc6e58e731f52dc06773bf

---
+++ libs/binder/trusty/binderRpcTest/rust/rules.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS := $(LOCAL_DIR)/binderRpcTest.rs
+
+MODULE_CRATE_NAME := binder_rpc_test
+
+MODULE_LIBRARY_DEPS += \
+	frameworks/native/libs/binder/trusty/binderRpcTest/rust/aidl \
+	frameworks/native/libs/binder/trusty/rust \
+	frameworks/native/libs/binder/trusty/rust/rpcbinder \
+	trusty/user/base/lib/trusty-sys \
+	trusty/user/base/lib/tipc/rust \
+	trusty/user/base/lib/log-rust \
+	trusty/user/base/lib/trusty-std \
+
+MODULE_RUST_TESTS := true
+
+MANIFEST := $(LOCAL_DIR)/manifest.json
+
+include make/library.mk
