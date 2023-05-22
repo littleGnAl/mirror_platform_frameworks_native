3df35d96884b7e8c42e96ce724f295f64e903ffd
Michael Crimando <mcriman1@ford.com>
2023-05-22 11:01:17 -0400

    Bind an input device via descriptor
    These changes solve a problem for systems that have multiple displays
    as well as 2 or more input devices with the exact same name. (e.g. 2
    Xbox Series X controllers will report the exact same device name).
    
    Currently the default behavior for overall Android is that input
    device inputs go towards the last screen that the user touched.
    On systems where a single computer is used for displaying content to
    multiple people, this can be a problem. A user may be trying to play a
    game with a controller on screen 1, while a second user tries to
    browse the internet on screen 2. If the user on screen 2 touches the
    screen, it'll keep sending the controller input events to screen 2
    instead of the game on screen 1.
    
    To fix this, the system can use a function to bind controllers to a
    display. The system can just find an input device by device name
    and make sure all inputs from that controller go to the correct
    display. That ensures that no matter what, the controller inputs go to
    the desired game on screen 1.
    
    Now consider a single system with 2 screens and 2 different users
    trying to play controller games. User A will want their controller
    inputs to go to screen 1 and User B will want their controller inputs
    to go to screen 2. This is possible with the current functionality.
    That is, unless the controllers have the exact same device name. In
    that case the controllers can only be locked to one screen or another.
    
    Device names are not unique. However, descriptors are unique per
    device. This would allow the system to uniquely identify each input
    device and bind them to the correct screen.
    
    Descriptors also cover certain scenarios. Such as a keyboard with a
    track pad built into it. It'll be 2 different input devices with 2
    different device names but the same descriptor.
    
    Test: Built and run on hardware
    Signed-off-by: Michael Crimando <mcriman1@ford.com>
    Change-Id: I27a7268eedc6be9f5423b4b8ca40b8b9728fe18c

--- services/inputflinger/reader/include/InputDevice.h
+++ services/inputflinger/reader/include/InputDevice.h
@@ -63,8 +63,11 @@ public:
     inline std::optional<uint8_t> getAssociatedDisplayPort() const {
         return mAssociatedDisplayPort;
     }
-    inline std::optional<std::string> getAssociatedDisplayUniqueId() const {
-        return mAssociatedDisplayUniqueId;
+    inline std::optional<std::string> getAssociatedDisplayUniqueIdByPort() const {
+        return mAssociatedDisplayUniqueIdByPort;
+    }
+    inline std::optional<std::string> getAssociatedDisplayUniqueIdByDescriptor() const {
+        return mAssociatedDisplayUniqueIdByDescriptor;
     }
     inline std::optional<std::string> getDeviceTypeAssociation() const {
         return mAssociatedDeviceType;
@@ -193,7 +196,8 @@ private:
     uint32_t mSources;
     bool mIsExternal;
     std::optional<uint8_t> mAssociatedDisplayPort;
-    std::optional<std::string> mAssociatedDisplayUniqueId;
+    std::optional<std::string> mAssociatedDisplayUniqueIdByPort;
+    std::optional<std::string> mAssociatedDisplayUniqueIdByDescriptor;
     std::optional<std::string> mAssociatedDeviceType;
     std::optional<DisplayViewport> mAssociatedViewport;
     bool mHasMic;
@@ -421,8 +425,11 @@ public:
     inline std::optional<uint8_t> getAssociatedDisplayPort() const {
         return mDevice.getAssociatedDisplayPort();
     }
-    inline std::optional<std::string> getAssociatedDisplayUniqueId() const {
-        return mDevice.getAssociatedDisplayUniqueId();
+    inline std::optional<std::string> getAssociatedDisplayUniqueIdByPort() const {
+        return mDevice.getAssociatedDisplayUniqueIdByPort();
+    }
+    inline std::optional<std::string> getAssociatedDisplayUniqueIdByDescriptor() const {
+        return mDevice.getAssociatedDisplayUniqueIdByDescriptor();
     }
     inline std::optional<std::string> getDeviceTypeAssociation() const {
         return mDevice.getDeviceTypeAssociation();
