b90c8c662afa849b26c74bbbfe349f2798db7b7a
Tomasz Wasilczyk <twasilczyk@google.com>
2023-06-20 13:31:42 -0700

    Add build script to clarify Linux build steps.
    Bug: 285204695
    Test: ./build.sh
    Change-Id: Ic7a56be27389fcd1554f33d5cfd2e2d2761f51b5

---
+++ libs/binder/linux/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -eu
+
+if [ ! -f $ANDROID_BUILD_TOP/external/boringssl/CMakeLists.txt ]; then
+    cd "$ANDROID_BUILD_TOP/external/boringssl"
+    python3 src/util/generate_build_files.py cmake
+fi
+
+cd "$ANDROID_BUILD_TOP/frameworks/native/libs/binder/linux"
+
+CC=clang CXX=clang++ cmake \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DANDROID_BUILD_TOP=$ANDROID_BUILD_TOP \
+    -DAIDL_BIN=$ANDROID_HOST_OUT/bin/aidl \
+    -B build
+
+cmake --build build
