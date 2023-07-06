2d197566449180223ba2b6d97896fd1b4bf774b8
Tomasz Wasilczyk <twasilczyk@google.com>
2023-07-06 15:45:16 -0700

    Build aidl with cmake
    Bug: TODO
    Test: ./build.sh
    Change-Id: I01ccefb067c9c3d080567ed0974789b249f4763f

--- libs/binder/linux/build.sh
+++ libs/binder/linux/build.sh
@@ -1,23 +1,10 @@
 #!/bin/sh
 
-PREBUILT_HOST_OUT=`pwd`/../../../../../prebuilts/host/linux-x86
-if [ -f "$PREBUILT_HOST_OUT/bin/aidl" ]; then
-    export PATH="$PREBUILT_HOST_OUT/bin:$PATH"
-    if [ -z $ANDROID_BUILD_TOP ]; then
-        ANDROID_BUILD_TOP=`pwd`/../../../../..
-    fi
-    if [ -z $ANDROID_HOST_OUT ]; then
-        ANDROID_HOST_OUT=$PREBUILT_HOST_OUT
-    fi
+if [ -z $ANDROID_BUILD_TOP ]; then
+    export ANDROID_BUILD_TOP=`pwd`/../../../../..
 fi
 
 set -eu
-
-if [ ! -f $ANDROID_HOST_OUT/bin/aidl ]; then
-    echo "Please run:"
-    echo "m -j aidl"
-    exit
-fi
 
 if [ ! -f $ANDROID_BUILD_TOP/external/boringssl/CMakeLists.txt ]; then
     cd "$ANDROID_BUILD_TOP/external/boringssl"
@@ -26,11 +13,13 @@ fi
 
 cd "$ANDROID_BUILD_TOP/frameworks/native/libs/binder/linux"
 
+mkdir -p aidl/gen
+aidl/copy-aidl-gen.sh aidl/gen
+
 CC=gcc CXX=g++ cmake \
     -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DANDROID_BUILD_TOP=$ANDROID_BUILD_TOP \
-    -DAIDL_BIN=$ANDROID_HOST_OUT/bin/aidl \
     -B build
 
 cmake --build build
