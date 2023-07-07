0e277d1d6cd0c91421c9665062cfdabe7a614a5c
Andrei Homescu <ahomescu@google.com>
2023-07-07 05:10:43 +0000

    libbinder_rs: Use a custom bindgen script
    Use a custom shell script to wrap the bindgen invocation
    for libbinder_ndk_sys so that both Android and Trusty can
    run bindgen with the same command line options.
    
    Bug: 242243245
    Test: presubmit
    Change-Id: Id579628660d05eaddb05b5ac7d7fd12e7d75c4d9

---
+++ libs/binder/rust/libbinder_ndk_bindgen.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright (C) 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# On Android, use the bindgen from the host output directory
+# (where this script is also located)
+BINDGEN="`dirname $0`/bindgen"
+
+LIBBINDER_NDK_BINDGEN_ARGS=(
+    # Unfortunately the only way to specify the rust_non_exhaustive enum
+    # style for a type is to make it the default
+    --default-enum-style="rust_non_exhaustive"
+    # and then specify constified enums for the enums we don't want
+    # rustified
+    --constified-enum="android::c_interface::consts::.*"
+
+    --allowlist-type="android::c_interface::.*"
+    --allowlist-type="AStatus"
+    --allowlist-type="AIBinder_Class"
+    --allowlist-type="AIBinder"
+    --allowlist-type="AIBinder_Weak"
+    --allowlist-type="AIBinder_DeathRecipient"
+    --allowlist-type="AParcel"
+    --allowlist-type="binder_status_t"
+    --allowlist-function=".*"
+  )
+
+exec $BINDGEN ${LIBBINDER_NDK_BINDGEN_ARGS[@]} "$@"
