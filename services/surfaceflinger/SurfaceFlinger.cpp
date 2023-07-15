2a9f9a685a09ce5c6260f555eaeca17dbba9e7f8
Mike <mikeyuewang@google.com>
2023-07-15 05:46:46 +0000

    Android codelab change
    Test: manual atest
    
    Change-Id: I30589b11c3211dd2ded5c74f40cd083e66b2b370

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2276,6 +2276,9 @@ void SurfaceFlinger::composite(nsecs_t frameTime, int64_t vsyncId)
 
     postFrame();
     postComposition();
+    mClientColorMatrix = mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                              vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
+    updateColorMatrixLocked();
 
     const bool prevFrameHadClientComposition = mHadClientComposition;
 
