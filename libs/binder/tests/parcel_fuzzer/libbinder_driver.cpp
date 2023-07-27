303dc907a783935e047561b82a31fc55bab3cd08
Pawan Wagh <waghpawan@google.com>
2023-07-27 16:53:29 +0000

    Read reserved bytes and use setData in fuzzService
    Updating fuzzService to read the reserved bytes and call parcel
    setData in fillRandomParcel so that recorded parcel data can be
    used directly
    
    Test: m libbinder_random_parcel
    Bug: 278975837
    Change-Id: Ice0703e168b904ad959da0b41033e1056cdb6307

--- libs/binder/tests/parcel_fuzzer/libbinder_driver.cpp
+++ libs/binder/tests/parcel_fuzzer/libbinder_driver.cpp
@@ -35,6 +35,11 @@ void fuzzService(const std::vector<sp<IBinder>>& binders, FuzzedDataProvider&& p
             .extraFds = {},
     };
 
+    // Reserved bytes so that we don't have to change fuzzers and seed corpus if
+    // we introduce anything new in fuzzService.
+    std::vector<uint8_t> reservedBytes = provider.ConsumeBytes<uint8_t>(8);
+    (void)reservedBytes;
+
     // always refresh the calling identity, because we sometimes set it below, but also,
     // the code we're fuzzing might reset it
     IPCThreadState::self()->clearCallingIdentity();
@@ -82,7 +87,6 @@ void fuzzService(const std::vector<sp<IBinder>>& binders, FuzzedDataProvider&& p
         reply.setEnforceNoDataAvail(false);
         reply.setServiceFuzzing();
         (void)target->transact(code, data, &reply, flags);
-
         // feed back in binders and fds that are returned from the service, so that
         // we can fuzz those binders, and use the fds and binders to feed back into
         // the binders
