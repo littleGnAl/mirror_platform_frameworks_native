8b17b2ccea3466713c8d88b7acdec0cf44f85bf9
Devin Moore <devinmoore@google.com>
2023-08-30 18:12:34 +0000

    Wrap AHardwareBuffer read/writes in __builtin_available
    It is only available in 34+.
    
    Test: m
    Bug: 251850069
    Change-Id: I55f2a8558529920eedef7ce5e618b1bd551ad5f2

--- libs/nativewindow/include/android/hardware_buffer_aidl.h
+++ libs/nativewindow/include/android/hardware_buffer_aidl.h
@@ -95,14 +95,22 @@ public:
 
     binder_status_t readFromParcel(const AParcel* _Nonnull parcel) {
         reset();
-        return AHardwareBuffer_readFromParcel(parcel, &mBuffer);
+        if (__builtin_available(android __ANDROID_API_U__, *)) {
+            return AHardwareBuffer_readFromParcel(parcel, &mBuffer);
+        } else {
+            return STATUS_UNKNOWN_TRANSACTION;
+        }
     }
 
     binder_status_t writeToParcel(AParcel* _Nonnull parcel) const {
         if (!mBuffer) {
             return STATUS_BAD_VALUE;
         }
-        return AHardwareBuffer_writeToParcel(mBuffer, parcel);
+        if (__builtin_available(android __ANDROID_API_U__, *)) {
+            return AHardwareBuffer_writeToParcel(mBuffer, parcel);
+        } else {
+            return STATUS_UNKNOWN_TRANSACTION;
+        }
     }
 
     /**
