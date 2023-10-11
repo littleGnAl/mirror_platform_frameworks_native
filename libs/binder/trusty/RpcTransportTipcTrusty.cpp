c1ae5ada1152beddfa4a9a32ccf8d686a4325af8
Tomasz Wasilczyk <twasilczyk@google.com>
2023-10-11 18:47:46 +0000

    Use std::function instead of base::function_ref
    Bug: 302723053
    Test: mma
    Change-Id: I2a437363135a26dd8459e643ecf87a274da3d26e

--- libs/binder/trusty/RpcTransportTipcTrusty.cpp
+++ libs/binder/trusty/RpcTransportTipcTrusty.cpp
@@ -45,7 +45,7 @@ public:
 
     status_t interruptableWriteFully(
             FdTrigger* /*fdTrigger*/, iovec* iovs, int niovs,
-            const std::optional<android::base::function_ref<status_t()>>& /*altPoll*/,
+            const std::optional<SmallFunction<status_t()>>& /*altPoll*/,
             const std::vector<std::variant<base::unique_fd, base::borrowed_fd>>* ancillaryFds)
             override {
         if (niovs < 0) {
@@ -115,7 +115,7 @@ public:
 
     status_t interruptableReadFully(
             FdTrigger* /*fdTrigger*/, iovec* iovs, int niovs,
-            const std::optional<android::base::function_ref<status_t()>>& /*altPoll*/,
+            const std::optional<SmallFunction<status_t()>>& /*altPoll*/,
             std::vector<std::variant<base::unique_fd, base::borrowed_fd>>* ancillaryFds) override {
         if (niovs < 0) {
             return BAD_VALUE;
