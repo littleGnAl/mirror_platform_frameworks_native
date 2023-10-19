b15a491eebac388a51aa0ee37656b01908f02d60
Becky Siegel <beckysiegel@google.com>
2023-10-19 21:15:09 +0000

    TEST
    Change-Id: I56c87e90fb392ee5fb8e9db16b3c95ff0e45160e

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2630,6 +2630,11 @@ void SurfaceFlinger::composite(TimePoint frameTime, VsyncId vsyncId)
         refreshArgs.colorTransformMatrix = mDrawingState.colorMatrix;
         mDrawingState.colorMatrixChanged = false;
     }
+    
+    refreshArgs.colorTransformMatrix =
+            mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                 vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
+
 
     refreshArgs.devOptForceClientComposition = mDebugDisableHWC;
 
